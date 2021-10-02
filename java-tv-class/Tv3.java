d3e8f4a215671709aea4c6de816c1e857aa62003
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-10-02 04:37:34 +0000

    refactor: add a couple of methods

--- java-tv-class/Tv3.java
+++ java-tv-class/Tv3.java
@@ -1,9 +1,28 @@
 public class Tv3 {
     int channel = 40;
     int volume = 4;
+
+    // Change channel
+    public void channelDown() {
+        // channel = 35;
+        channel = channel - 5;
+    }
+
+    // Change volume
+    public void volumeUp() {
+        // volume = 6;
+        volume = volume + 2;
+    }
     public static void main (String[] args) {
         Tv3 myTv3 = new Tv3();
+        
         System.out.println("Current TV channel : " + myTv3.channel);
         System.out.println("Current TV volume : " + myTv3.volume);
+
+        myTv3.channelDown();
+        myTv3.volumeUp();
+
+        System.out.println("Last TV channel : " + myTv3.channel);
+        System.out.println("Last TV volume : " + myTv3.volume);
     }
 }
