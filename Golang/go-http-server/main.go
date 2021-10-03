b9f14729c2495e96f9cda0857a8595626c697dc8
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-10-03 08:52:53 +0000

    refactor: initialize a handler function

--- Golang/go-http-server/main.go
+++ Golang/go-http-server/main.go
@@ -1,10 +1,15 @@
 package main
 
-import "net/http";
+import (
+	"net/http"
+	"github.com/gowebexamples/http-server/api"
+);
 
 func main()  {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to Golang server !"))
-	});
-	http.ListenAndServe(":8080", nil);
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("Welcome to Golang server !"))
+	// });
+
+	srv := api.NewServer();
+	http.ListenAndServe(":8080", srv);
 }
