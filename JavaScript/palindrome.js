f920c2c2c563f0bff86a1faf822c64ad9834b9fa
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-10-12 07:41:20 +0000

    refactor: add array manipulation

---
+++ JavaScript/palindrome.js
@@ -0,0 +1,14 @@
+let word = "tomato";
+let reversed_words;
+
+// Count characters in the word
+console.log("The word length is :", word.length, "\n");
+
+// Convert the word into an array consisting of each character
+word_arr = word.split("");
+console.log("Now, let's convert the word into an array :", word_arr);
+
+
+
+console.log("\n[ RESULT ]");
+word === reversed_words ? console.log("The word is palindrome !") : console.log("The word is NOT palindrome...");
