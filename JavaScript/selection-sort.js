a744017001be21bc94b9f9f86967a5fad6a672c6
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-10-16 15:41:37 +0000

    feat(sorting): add selection sort

---
+++ JavaScript/selection-sort.js
@@ -0,0 +1,21 @@
+const numbers = [5, 66, 3, 2, 456, 123];
+
+function selectionSort(array) {
+    const arr = array.slice();
+    for (let i = 0; i < arr.length - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+    return arr;
+}
+
+const sorted_result = selectionSort(numbers);
+
+console.log("=== Selection Sort ===\n");
+console.log("Before sorted : ", numbers);
+console.log("After sorted : ", sorted_result);
