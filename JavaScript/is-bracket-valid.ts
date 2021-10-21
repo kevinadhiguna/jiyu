80732f8f44a6f5159b862122486005ae834a5007
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-10-21 18:05:07 +0000

    style: format code using Prettier

--- JavaScript/is-bracket-valid.ts
+++ JavaScript/is-bracket-valid.ts
@@ -1,16 +1,18 @@
 function isBracketValid(s: string) {
     let total: number = 0;
     for (let i = 0; i < s.length; i++) {
-        (s[i] == "(") ? total++ : total--;
+        s[i] == "(" ? total++ : total--;
         if (total < 0) {
-            return false
+            return false;
         }
     }
-    return total==0;
+    return total == 0;
 }
 
 console.log("\n=== Are these brackets valid ? ===\n");
-console.log("GitHub repository : https://github.com/kevinadhiguna/jiyu/blob/master/JavaScript/is-bracket-valid.ts\n");
+console.log(
+    "GitHub repository : https://github.com/kevinadhiguna/jiyu/blob/master/JavaScript/is-bracket-valid.ts\n"
+);
 
 console.log("Case '()' :", isBracketValid("()"));
 console.log("Case '()()' :", isBracketValid("()()"));
