f39d29ebd3fa98a65a9e7b4a7da8cebe10ed75d4
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-10-28 02:06:47 +0000

    feat(javascript): add even numbers program

---
+++ javascript/is-even-number.js
@@ -0,0 +1,15 @@
+const numbers = [5, 16, 7, 19, 198, 1037, 1887];
+
+function isEvenNumber(array) {
+    const evenNumbersArray = [];
+    for (let i = 0; i < array.length; i++) {
+        array[i] % 2 == 0 ? evenNumbersArray.push(array[i]) : null;
+    }
+    return evenNumbersArray;
+}
+
+// Execute the function
+const result = isEvenNumber(numbers);
+
+// Show results
+console.log("Among these numbers :\n", numbers, "\nEven Numbers are :\n", result);
