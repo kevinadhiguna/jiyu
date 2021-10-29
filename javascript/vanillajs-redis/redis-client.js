9563ac766506ac845f96122da4d5f14a6fbc5da2
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-10-29 04:18:32 +0000

    fix: close redis connection

--- javascript/vanillajs-redis/redis-client.js
+++ javascript/vanillajs-redis/redis-client.js
@@ -2,31 +2,31 @@ const { createHandyClient } = require("handy-redis"); // <- 'createHandyClient' 
 const { v4: uuidv4 } = require("uuid");
 require("dotenv").config();
 
+// Initialize Redis client
 const jsRedisClient = createHandyClient({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
 });
 
-// Async/Await
 async function redisSetMerchant(merchantId, merchantBrand, merchantName) {
+    // Set key-value
     await jsRedisClient.set("merchantId", merchantId);
     await jsRedisClient.set("merchantBrand", merchantBrand);
     await jsRedisClient.set("merchantName", merchantName);
 
+    // Get key-value
     const redisMerchantId = await jsRedisClient.get("merchantId");
     const redisMerchantBrand = await jsRedisClient.get("merchantBrand");
     const redisMerchantName = await jsRedisClient.get("merchantName");
 
+    // Show values
     console.log("Merchant ID is :", redisMerchantId);
     console.log("Merchant Brand is :", redisMerchantBrand);
     console.log("Merchant Name is :", redisMerchantName);
+
+    // Close Redis connection
+    await jsRedisClient.quit();
 }
 
 const id = uuidv4();
 redisSetMerchant(id, "Nike", "Air Jordan");
-
-// Promise
-// jsRedisClient
-//     .set("Full Name", "Kai Doe Alcaraz")
-//     .then(() => jsRedisClient.get("username"))
-//     .then((x) => console.log("Username is :", x));
