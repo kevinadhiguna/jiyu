9eda578fb66a820253310fad42c39c61c097e97a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-10-30 09:17:53 +0000

    feat(javascript): add object comparison program

---
+++ javascript/are-objects-same.js
@@ -0,0 +1,22 @@
+// This method is rapid but limited. In addition, the order of property matters. For more : https://stackoverflow.com/a/1144249.
+function areObjectsSame(obj1, obj2) {
+    return JSON.stringify(obj1) === JSON.stringify(obj2) ? "same" : "different";
+}
+
+const henderson = {
+    id: "5gf44",
+    name: "Liam Henderson",
+};
+
+const hader = {
+    id: "6gf46",
+    name: "Josh Hader",
+};
+
+console.log("Stringified object 1 :", JSON.stringify(henderson));
+console.log("Stringified object 2 :", JSON.stringify(hader));
+console.log(
+    "Object 1 and Object 2 are ",
+    areObjectsSame(henderson, hader),
+    "objects."
+);
