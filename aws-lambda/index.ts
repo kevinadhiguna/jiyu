5e04a19c8cd676aa1ff2b54c0d0d6195bb195fac
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-10-30 10:57:11 +0000

    refactor: update script

--- aws-lambda/index.ts
+++ aws-lambda/index.ts
@@ -1,15 +1,10 @@
-interface ReqEvent {
-    key1: string;
-    key2: string;
-    key3: string;
-    key4: string;
-}
+import { Context, Handler } from "aws-lambda";
+import { IReqEvent } from "./IReqEvent";
+import { IRes } from "./IRes";
 
-interface Res {
-    statusCode: number,
-    body: string,
-}
-
-const handler = () => {
-    console.log("Hello to AWS Lambda from TypeScript!");
-}
+export const handler: Handler<IReqEvent, IRes> = async (
+    event: IReqEvent,
+    context: Context
+): Promise => {
+    console.log("Hello from AWS Lambda powered by TypeScript!");
+};
