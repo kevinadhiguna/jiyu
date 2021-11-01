33e4358f13a70a4c9db10b274f088f00dc6d6c96
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-11-01 09:04:45 +0000

    refactor: update php ci apache

--- php/php-ci-apache/startScript.sh
+++ php/php-ci-apache/startScript.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-mv /codeigniter4 /var/www/html
+
+# Move current folder to '/var/www/html'
+# mv . /var/www/html
+
+# apache2ctl blocks the progress of your entrypoint script until apachectl exits. For more : https://stackoverflow.com/questions/49721591/docker-container-custom-index-html-on-startup.
 /usr/sbin/apache2ctl -D FOREGROUND
