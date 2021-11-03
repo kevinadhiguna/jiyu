8618ae1357fa77e8ce85ba49c454f295cecc0707
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-11-03 02:51:26 +0000

    fix: apply minus if the number is negative

--- javascript/reverse-integer.js
+++ javascript/reverse-integer.js
@@ -1,5 +1,5 @@
 /**
- * Source : https://javascript.plainenglish.io/reverse-integer-in-javascript-8ec48721ccfb
+ * Reference : https://javascript.plainenglish.io/reverse-integer-in-javascript-8ec48721ccfb
  * Reverse an integer : 739 -> 937
  *
  * Steps to reverse an integer :
@@ -10,17 +10,20 @@
  * (5) "937" -> Join all elements of the array into a single string. Good thing there is the '.join("")' method that helps one to do this.
  * (6) 937 -> Parse the string into an integer since type of "937" is string, NOT an integer... ('parseInt()' is the right method for this).
  *
+ * Note : Multiply minus one if the original number is negative.
+ *
  * Last but not least, do not forget to return the variable :)
  */
 
 function reverseInteger(n) {
+    const tempCopiedNumber = n;
     let reversedNumber = n
         .toString() // "xyz"
         .split("") // ["x", "y", "z"]
         .reverse() // ["z", "y", "x"]
         .join(""); // "zyx"
     let parsedInt = parseInt(reversedNumber); // zyx
-    return parsedInt;
+    return tempCopiedNumber < 0 ? parsedInt * -1 : parsedInt; // Add minus, if the original number is negative. Otherwise, just return the reversed number..
 }
 
 const integer = 9762938;
