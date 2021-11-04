c6463c82f1dbefe2cec2eaa7e6375584b58b9f3c
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-11-04 08:21:15 +0000

    feat(javascript): add browser navigator program

---
+++ javascript/browser-navigator.js
@@ -0,0 +1,20 @@
+/**
+ * The window.navigator object contains information about the visitor's browser (source: https://www.w3schools.com/js/js_window_navigator.asp).
+ * 
+ * These code are meant to be executed in a browser.
+ */
+
+// Detect if cookie is enabled in user's browser
+navigator.cookieEnabled;
+
+// Detect if user's browser is online
+navigator.onLine;
+
+// Detect if user's browser is online
+navigator.offline;
+
+// Detect user's platform (OS)
+navigator.platform;
+
+// Detect a language in a browser
+navigator.language;
