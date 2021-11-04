696af6609f9e050c9a003b6adfe9ab2eae38a58c
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-11-04 01:55:15 +0000

    feat(jaavscript): add array copywithin method

---
+++ javascript/array-copy-within-method.js
@@ -0,0 +1,11 @@
+const array = ["Burnes", "Harper", "Tatiis", "Mickey", "Donald"];
+
+console.log(array.copyWithin());
+
+console.log(array.copyWithin(0));
+console.log(array.copyWithin(1));
+console.log(array.copyWithin(2));
+console.log(array.copyWithin(3));
+console.log(array.copyWithin(4));
+
+console.log(array.copyWithin(1, 2));
