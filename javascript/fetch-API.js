d651e74d96ec802e381979b4c3b2cc342a8559af
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-11-05 03:02:46 +0000

    feat(javascript): add fetch API program

---
+++ javascript/fetch-API.js
@@ -0,0 +1,24 @@
+// Note : Make sure your package.json includes a line for “type”: “module”, Without this line node assumes you want to use Common JS modules rather than ESM.
+import fetch from "node-fetch"; // <- The fetch API is not implemented in NodeJS. Thus, one is required to use an external module called 'node-fetch' (Source: https://stackoverflow.com/questions/48433783/referenceerror-fetch-is-not-defined).
+
+/**
+ * JSONPlaceholder comes with a set of 6 common resources:
+ *
+ * /posts 	100 posts
+ * /comments 	500 comments
+ * /albums 	100 albums
+ * /photos 	5000 photos
+ * /todos 	200 todos
+ * /users 	10 users
+ *
+ * Resources : https://jsonplaceholder.typicode.com/
+ * Guide : https://jsonplaceholder.typicode.com/guide/
+ */
+
+async function fetchUsers() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users/");
+    const data = await res.json();
+    console.log(data);
+}
+
+fetchUsers();
