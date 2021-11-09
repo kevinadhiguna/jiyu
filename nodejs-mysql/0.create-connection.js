d8be3d0e62ec078d57b66e9e24e9baab640c7fb9
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-11-09 01:27:31 +0000

    feat: connect to mysql from a nodejs client

---
+++ nodejs-mysql/0.create-connection.js
@@ -0,0 +1,18 @@
+import mysql from "mysql";
+
+// Import 'dotenv' in ES6 (reference: https://github.com/motdotla/dotenv/issues/89#issuecomment-139372079)
+import dotenv from "dotenv";
+dotenv.config();
+
+// MySQL connection config
+const mysqlConnection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+});
+
+// Connect to MySQL
+mysqlConnection.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to MySQL !");
+});
