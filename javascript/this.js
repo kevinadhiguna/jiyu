bf00f6dacf2e399809788ed8a99b18927ddea4bd
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-11-10 07:39:38 +0000

    feat(javascript): add this keyword explanation and example

---
+++ javascript/this.js
@@ -0,0 +1,17 @@
+// Reference : https://www.w3schools.com/js/js_this.asp
+
+// "this" keyword refers to an object it belongs to.
+const angels_anaheim = {
+    manager: "Joe Maddon",
+    best_player: "Shohei Ohtani",
+    quote: function () {
+        return (
+            "Angels manager is " +
+            this.manager +
+            " and Angels' best player is " +
+            this.best_player
+        );
+    },
+};
+
+console.log("Currently", angels_anaheim.quote());
