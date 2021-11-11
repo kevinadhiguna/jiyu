405cba33d0c53534a05b3e65453d900c48935777
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-11-11 04:43:08 +0000

    refactor: log messages

--- javascript/string/string-templates.js
+++ javascript/string/string-templates.js
@@ -2,23 +2,28 @@
 
 // ### Back-tics syntax ###
 let redsox_fielders = `Devers, Hernandez, Bogaerts`;
+console.log("Redsox fielders :", redsox_fielders);
 
 // ### Quotes inside a string ###
 let angels_sp = `They're Cobb, "Shotime", and Detmers`;
+console.log("Angels starting pitchers :", angels_sp);
 
 // ### Multiline strings ###
 let mets_roster = `
 Lindor,
 Bayez,
 DeGrom
-`; 
+`;
+console.log("Mets roster :", mets_roster);
 
 // ### Template literals & Variable subtitution ###
 let manager_firstname = "Gabe";
 let manager_lastname = "Kepler";
 let manager_fullname = `SF Giants' manager is ${manager_firstname} ${manager_lastname}`;
+console.log(manager_fullname);
 
 // ### Expression Subtitution ###
 let total_team_wage = 200000000;
 let luxury_tax = 0.35;
 let payroll = `Team Payroll : ${price} + (${price} * ${luxury_tax})`;
+console.log(payroll);
