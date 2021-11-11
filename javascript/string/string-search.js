a3f794724eb7ba85d3400a8969149db982e00b0e
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-11-11 03:06:53 +0000

    chore: add a newline for each method

--- javascript/string/string-search.js
+++ javascript/string/string-search.js
@@ -14,14 +14,14 @@ let the_yankees = yankees.slice();
 
 // ### indexOf() ###
 // returns the index of the first occurence of a specified text in a string
-console.log("Index of (Aaron) Judge :", the_yankees.indexOf("Judge")); // 10
+console.log("Index of (Aaron) Judge :", the_yankees.indexOf("Judge"), "\n"); // 10
 
 // ### lastIndexOf() ###
 let the_yankees_2 = yankees.slice().concat(", Judge");
 console.log("The yankees 2 :", the_yankees_2);
 // returns the index of the last occurence of a specified text
-console.log("Last index of (Aaron) Judge :", the_yankees_2.lastIndexOf("Judge")); // 26
+console.log("Last index of (Aaron) Judge :", the_yankees_2.lastIndexOf("Judge"), "\n"); // 26
 
 // NOTE : Both indexOf() and lastIndexOf() return -1 if the text is not found.
 console.log("Index of (Shohei) Ohtani :", the_yankees.indexOf("Ohtani"));
-console.log("Index of (Carlos) Correa :", the_yankees.lastIndexOf("Correa"));
+console.log("Index of (Carlos) Correa :", the_yankees.lastIndexOf("Correa"), "\n");
