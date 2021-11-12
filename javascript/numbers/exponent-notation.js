a07224c847ad9a13af1a04e4ae4a52a9bf56b98a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-11-12 01:40:58 +0000

    feat(javascript): add exponent notation

---
+++ javascript/numbers/exponent-notation.js
@@ -0,0 +1,9 @@
+// Reference : https://www.w3schools.com/j2s/js_num2bers.asp
+
+let num = 345;
+
+// Extra small numbers can be written in exponent notation
+let num2 = 345e-3;
+
+console.log("num :", num); // 345
+console.log("num2 :", num2); // 0.345
