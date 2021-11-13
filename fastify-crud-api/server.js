9b28c3f6e6415efb772d97556d7dc9b54d34dd5c
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-11-13 00:46:15 +0000

    refactor: add start server function

---
+++ fastify-crud-api/server.js
@@ -0,0 +1,17 @@
+const fastify = require("fastify")({ logger: true });
+require('dotenv').config();
+
+const port = process.env.PORT;
+
+// Start server
+const start = async () => {
+    try {
+        await fastify.listen(port);
+    } catch (error) {
+        fastify.log.error(error);
+        console.error("Error occured.. exiting..");
+        process.exit(1);
+    }
+}
+
+start();
