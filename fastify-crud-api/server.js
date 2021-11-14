96c1ab53dada63c163e1a834f88ac1c8cd5ef3a5
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-11-14 04:41:34 +0000

    refactor: add an items route

--- fastify-crud-api/server.js
+++ fastify-crud-api/server.js
@@ -3,6 +3,11 @@ require('dotenv').config();
 
 const port = process.env.PORT;
 
+// Send a reply (response) if one hits '/items' (http://localhost:5000/items) route. 
+fastify.get('/items', (req, reply) => {
+    reply.send({ test: 'hello' });
+});
+
 // Start server
 const start = async () => {
     try {
