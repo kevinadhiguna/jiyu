1c413189c4b67a5723550bec87d11460cb5dcdcf
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-11-14 05:19:33 +0000

    refactor: import items and get items

--- fastify-crud-api/server.js
+++ fastify-crud-api/server.js
@@ -3,9 +3,12 @@ require('dotenv').config();
 
 const port = process.env.PORT;
 
+const items = require('./items');
+
 // Send a reply (response) if one hits '/items' (http://localhost:5000/items) route. 
 fastify.get('/items', (req, reply) => {
-    reply.send({ test: 'hello' });
+    // reply.send({ test: 'hello' });
+    reply.send(items);
 });
 
 // Start server
