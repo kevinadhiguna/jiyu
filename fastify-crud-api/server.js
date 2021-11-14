d10b504af65b45cf180be3807dd7f734aa4c3824
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-11-14 13:12:02 +0000

    refactor: register fastify swagger

--- fastify-crud-api/server.js
+++ fastify-crud-api/server.js
@@ -1,8 +1,19 @@
 const fastify = require("fastify")({ logger: true });
 require("dotenv").config();
 
+// Register fastify swagger
+fastify.register(require("fastify-swagger"), {
+    exposeRoute: true,
+    routePrefix: "/docs",
+    swagger: {
+        info: {
+            title: "fastify crud api",
+        },
+    },
+});
+
 // Register a route
-fastify.register(require('./routes/items'));
+fastify.register(require("./routes/items"));
 
 const port = process.env.PORT;
 
