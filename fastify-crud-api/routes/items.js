b7865a6884c80b86d82a84403172df628bf128b8
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-11-14 13:00:53 +0000

    refactor: add schema and handler

--- fastify-crud-api/routes/items.js
+++ fastify-crud-api/routes/items.js
@@ -1,20 +1,27 @@
 const items = require("../items");
 
+// Item schema
+const Item = {
+    type: "object",
+    properties: {
+        id: { type: "string" },
+        name: { type: "string" },
+    },
+};
+
 // Options for get all items (more like a response format)
 const getItemsOpts = {
     schema: {
         response: {
             200: {
                 type: "array",
-                items: {
-                    type: "object",
-                    properties: {
-                        id: { type: "string" },
-                        name: { type: "string" },
-                    },
-                },
+                items: Item,
             },
         },
+    },
+    handler: function (req, reply) {
+        // Send a reply (response) if one hits '/items' (http://localhost:5000/items) route.
+        reply.send(items);
     },
 };
 
@@ -22,32 +29,24 @@ const getItemsOpts = {
 const getItemOpts = {
     schema: {
         response: {
-            200: {
-                type: "object",
-                properties: {
-                    id: { type: "string" },
-                    name: { type: "string" },
-                },
-            },
+            200: Item,
         },
     },
-};
-
-function itemRoutes(fastify, options, done) {
-    // Get all items
-    fastify.get("/items", getItemsOpts, (req, reply) => {
-        // Send a reply (response) if one hits '/items' (http://localhost:5000/items) route.
-        reply.send(items);
-    });
-
-    // Get a specific item
-    fastify.get("/item/:id", getItemOpts, (req, reply) => {
+    handler: function (req, reply) {
         const { id } = req.params;
         const item = items.find((item) => {
             item.id === id;
         });
         reply.send(item);
-    });
+    },
+};
+
+function itemRoutes(fastify, options, done) {
+    // Get all items
+    fastify.get("/items", getItemsOpts);
+
+    // Get a specific item
+    fastify.get("/item/:id", getItemOpts);
 
     done();
 }
