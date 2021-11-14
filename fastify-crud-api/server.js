30732852416bc89232a2f970e6e7a6aeba787672
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-11-14 12:32:07 +0000

    refactor: move items route to another folder

--- fastify-crud-api/server.js
+++ fastify-crud-api/server.js
@@ -3,23 +3,6 @@ require("dotenv").config();
 
 const port = process.env.PORT;
 
-const items = require("./items");
-
-// Get all items
-fastify.get("/items", (req, reply) => {
-    // Send a reply (response) if one hits '/items' (http://localhost:5000/items) route.
-    reply.send(items);
-});
-
-// Get a specific item
-fastify.get("/item/:id", (req, reply) => {
-    const { id } = req.params;
-    const item = items.find((item) => {
-        item.id === id;
-    });
-    reply.send(item);
-});
-
 // Start server
 const start = async () => {
     try {
