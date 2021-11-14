0839de77a4f2983427018f2aa4f6039e78a6baa7
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-11-14 05:18:59 +0000

    chore: add mock items

---
+++ fastify-crud-api/items.js
@@ -0,0 +1,16 @@
+let items = [
+    {
+        id: 1,
+        name: "Cricket",
+    },
+    {
+        id: 2,
+        name: "Football",
+    },
+    {
+        id: 3,
+        name: "Baseball",
+    },
+];
+
+module.exports = items;
