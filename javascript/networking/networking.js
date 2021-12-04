521c3282c58abb6bb2a7e73a0e3c342402ac0362
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-12-04 02:16:17 +0000

    feat: add networking

---
+++ javascript/networking/networking.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+    res.writeHead(200, { "Content-type": "text/plain" });
+    res.end(`Hi, you came from ${ip}\n`);
+});
+
+const hostname = process.argv[2] || null;
+
+const port = process.env.PORT || 8050;
+
+server.listen(port, hostname, (err) => {
+    if (err) {
+        return console.log("Error occured :", err);
+    }
+
+    console.log("Listening to", hostname);
+});
