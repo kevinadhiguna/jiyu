e9cff8254b20f778ff44986828a170dfa1697c9a
kevinadhiguna <43397636+kevinadhiguna@users.noreply.github.com>
2021-12-22 10:19:06 +0700

    chore(data): add hasBeenMVP attribute

--- mount-vuejs/index.js
+++ mount-vuejs/index.js
@@ -9,9 +9,9 @@ const app = Vue.createApp({
             hasManager: true,
             managerName: "Joe Maddon",
             rosters: [
-                { name: 'Mike Trout', position: 'CF', img: "https://www.pngarts.com/files/5/Mike-Trout-PNG-Free-Download.png" },
-                { name: 'Anthony Rendon', position: '3B', img: "https://sportshub.cbsistatic.com/i/r/2020/08/24/6f2baabd-df27-45d6-acae-792c338d6d6d/thumbnail/1200x675/d3efc6b52fb5862456f74a74c1f780cc/anthony-rendon.png" },
-                { name: 'Noah Syndergaard', position: 'SP', img: "https://images.daznservices.com/di/library/sporting_news/5e/f6/noah-syndergaard-41619-ftr_1xae9ljnym1vd1usagyhhdj7of.png?t=711309146&quality=100&w=1280&h=720" },
+                { name: 'Mike Trout', position: 'CF', hasBeenMVP: true, img: "https://www.pngarts.com/files/5/Mike-Trout-PNG-Free-Download.png" },
+                { name: 'Anthony Rendon', position: '3B', hasBeenMVP: false, img: "https://sportshub.cbsistatic.com/i/r/2020/08/24/6f2baabd-df27-45d6-acae-792c338d6d6d/thumbnail/1200x675/d3efc6b52fb5862456f74a74c1f780cc/anthony-rendon.png" },
+                { name: 'Noah Syndergaard', position: 'SP', hasBeenMVP: false, img: "https://images.daznservices.com/di/library/sporting_news/5e/f6/noah-syndergaard-41619-ftr_1xae9ljnym1vd1usagyhhdj7of.png?t=711309146&quality=100&w=1280&h=720" },
             ]
             x: 0,
             y: 0
