35641f57f9dcbeda3095e5f8acc2bd0cbbb2c991
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-01-13 01:34:25 +0000

    refactor: add a little bit of change

--- str4pi/config/database.js
+++ str4pi/config/database.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     client: 'mysql',
     connection: {
       host: env('DATABASE_HOST', ''),
-      port: env.int('DATABASE_PORT', 3306),
+      port: env.int('DATABASE_PORT'),
       database: env('DATABASE_NAME', 'strapi'),
       user: env('DATABASE_USERNAME', 'strapi'),
       password: env('DATABASE_PASSWORD', 'strapi'),
