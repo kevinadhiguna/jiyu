ee09a3959ff96d4b1f4d91ed7a3db8099186f5dd
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-01-13 01:33:36 +0000

    chore: set default value to database host

--- str4pi/config/database.js
+++ str4pi/config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '0.0.0.0'),
+      host: env('DATABASE_HOST', ''),
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'strapi'),
       user: env('DATABASE_USERNAME', 'strapi'),
