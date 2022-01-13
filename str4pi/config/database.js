093222ad6ce6583e6e5de040f36c0aeb21050ffb
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-01-13 01:35:17 +0000

    chore: set default value to database username

--- str4pi/config/database.js
+++ str4pi/config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       host: env('DATABASE_HOST', ''),
       port: env.int('DATABASE_PORT'),
       database: env('DATABASE_NAME', ''),
-      user: env('DATABASE_USERNAME', 'strapi'),
+      user: env('DATABASE_USERNAME', ''),
       password: env('DATABASE_PASSWORD', 'strapi'),
       ssl: env.bool('DATABASE_SSL', false),
     },
