ae75dad4a7182674b77bf4caa9ed6d2835185e39
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-08 08:34:25 +0000

    refactor: change route and add goalRoutes

--- mern.io/server/server.js
+++ mern.io/server/server.js
@@ -5,13 +5,7 @@ require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({
-        "message": "Get initial route"
-    });
-});
+app.use("/api/goals", require("./routes/goalRoutes"));
 
 app.listen(port, () => {
   console.log(`The app has been launched on port ${port}.`);
