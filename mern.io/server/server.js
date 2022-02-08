aac71bafbd43dd0a99914174196ecb49a6562cee
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-08 07:41:18 +0000

    refactor: initialize express

--- mern.io/server/server.js
+++ mern.io/server/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const app = express();
 
 console.log("Hello World!");
 
-express.listen(5000, () => {
+app.listen(5000, () => {
     console.log("The app has been launched on port 5000.");
 })
