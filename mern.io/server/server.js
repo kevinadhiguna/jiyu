be6fc9aa1258c5d737f6847cc6f22f331b52b7d4
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-08 07:44:23 +0000

    refactor: create a route

--- mern.io/server/server.js
+++ mern.io/server/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 
-console.log("Hello World!");
+app.get('/', (req, res) => {
+    res.send("Hello World !");
+});
 
 app.listen(5000, () => {
     console.log("The app has been launched on port 5000.");
