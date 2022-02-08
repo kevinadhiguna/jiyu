6a7e0a5046cc957cfae8a8061b540c8ed76aed33
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-08 08:07:50 +0000

    refactor: add a status code

--- mern.io/server/server.js
+++ mern.io/server/server.js
@@ -6,11 +6,13 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.json({
+  res
+    .status(200)
+    .json({
         "message": "Get initial route"
     });
 });
 
 app.listen(port, () => {
-    console.log(`The app has been launched on port ${port}.`);
+  console.log(`The app has been launched on port ${port}.`);
 });
