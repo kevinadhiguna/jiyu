3af8a79c9558303ce18ed07fb03af5a3298f7dd4
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-08 08:28:34 +0000

    refactor: initialize router

--- mern.io/server/routes/goalRoutes.js
+++ mern.io/server/routes/goalRoutes.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const router = express.Router();
+
+router.get("/", (req, res) => {
+    res
+      .status(200)
+      .json({
+        "message": "Get initial routes"
+      });
+});
