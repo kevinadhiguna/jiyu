09617454c67a871648de77c5bbf6b33f9e28344a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-09 03:25:56 +0000

    refactor: create post method

--- mern.io/server/routes/goalRoutes.js
+++ mern.io/server/routes/goalRoutes.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
+// GET
 router.get("/", (req, res) => {
-    res
-      .status(200)
-      .json({
-        "message": "Get initial routes"
-      });
+  res
+    .status(200)
+    .json({
+      "message": "Get initial routes"
+    });
+});
+
+// POST
+router.post("/", (req, res) => {
+  res
+    .status(200)
+    .json({
+      "message": "Create a goal"
+    });
 });
 
 module.exports = router;
