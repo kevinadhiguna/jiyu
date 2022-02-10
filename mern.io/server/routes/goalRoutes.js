d5c965c7d52363472ace7e04fc8c6f111eb5ff87
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-10 01:52:53 +0000

    style: format code

--- mern.io/server/routes/goalRoutes.js
+++ mern.io/server/routes/goalRoutes.js
@@ -8,29 +8,23 @@ router.get("/", getGoals);
 
 // POST
 router.post("/", (req, res) => {
-  res
-    .status(200)
-    .json({
-      "message": "Create a goal"
-    });
+  res.status(200).json({
+    message: "Create a goal",
+  });
 });
 
 // PUT
 router.put("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({
-      "message": `Change goal ${req.params.id}`
-    });
+  res.status(200).json({
+    message: `Change goal ${req.params.id}`,
+  });
 });
 
 // DELETE
 router.delete("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({
-      "message": `Delete goal ${req.params.id}`
-    });
+  res.status(200).json({
+    message: `Delete goal ${req.params.id}`,
+  });
 });
 
 module.exports = router;
