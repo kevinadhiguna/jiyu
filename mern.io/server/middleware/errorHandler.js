ee2a8e62abc84d5e38874c12037bde03a8175981
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-12 03:42:23 +0000

    refactor: send status code

--- mern.io/server/middleware/errorHandler.js
+++ mern.io/server/middleware/errorHandler.js
@@ -1,3 +1,4 @@
 const errorHandler = (err, req, res, next)=> {
-    const statusCode = res.statusCode ? res.statusCode : 500;   
+    const statusCode = res.statusCode ? res.statusCode : 500;
+    res.status(statusCode);
 }
