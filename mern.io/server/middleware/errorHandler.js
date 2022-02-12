1528f7a55f451df54b04e703558ad8a71d008165
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-12 03:44:18 +0000

    refactor: only send stack when not in production environment

--- mern.io/server/middleware/errorHandler.js
+++ mern.io/server/middleware/errorHandler.js
@@ -3,6 +3,6 @@ const errorHandler = (err, req, res, next)=> {
     res.status(statusCode);
     res.json({
         message: err.message,
-        stack: err.stack
+        stack: process.env.NODE_ENV == "production" ? null : err.stack,
     });
 }
