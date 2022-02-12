eab2483534122ad55758edef7b9ddc760d4aa1c0
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-12 03:42:58 +0000

    refactor: send error message in json

--- mern.io/server/middleware/errorHandler.js
+++ mern.io/server/middleware/errorHandler.js
@@ -1,4 +1,7 @@
 const errorHandler = (err, req, res, next)=> {
     const statusCode = res.statusCode ? res.statusCode : 500;
     res.status(statusCode);
+    res.json({
+        message: err.message,
+    });
 }
