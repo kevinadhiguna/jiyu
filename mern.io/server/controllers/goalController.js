42594342c488252b4f94c0188e838eff4157ecd0
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-13 04:12:35 +0000

    refactor: wrap each function with async handler

--- mern.io/server/controllers/goalController.js
+++ mern.io/server/controllers/goalController.js
@@ -3,16 +3,16 @@ const asyncHandler = require("express-async-handler");
 // @desc    Get Goals
 // @route   GET /api/goals
 // @access  Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: "Get goals",
   });
-};
+});
 
 // @desc    Set a Goal
 // @route   POST /api/goals
 // @access  Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   // Add an error handler created by ExpressJS, if a field named text does not exist in request body.
   if (!req.body.text) {
     res.status(400);
@@ -20,21 +20,21 @@ const setGoal = (req, res) => {
   }
 
   res.status(200).json({ message: "Set a goal" });
-};
+});
 
 // @desc    Update a Goal
 // @route   PUT /api/goals/:id
 // @access   Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update a goal ${req.params.id}` });
-};
+});
 
 // @desc    Delete a Goal
 // @route   DELETE /api/goals/:id
 // @access  Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete a goal ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoals,
