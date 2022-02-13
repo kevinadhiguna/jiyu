c2662d9dfa838d9f0b448d822ab5b5781be8cd67
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-13 04:47:59 +0000

    refactor: attempt to connect to database

---
+++ mern.io/server/config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const connection = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+
+        console.log(`MongoDB connected : ${conn.connection.host}`);
+    } catch (error) {
+        
+    }
+}
