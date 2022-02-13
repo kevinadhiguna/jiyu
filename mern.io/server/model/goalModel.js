d35899a065ddb2de2809cedb5fc643d55faf6dc0
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-13 05:03:00 +0000

    refactor: create a schema

---
+++ mern.io/server/model/goalModel.js
@@ -0,0 +1,11 @@
+// Model defines any resources that an app has
+
+const mongoose = require("mongoose");
+
+// Define schema
+const goalSchema = mongoose.Schema({
+  text: {
+    type: String,
+    required: [true, "Please add a text value"],
+  },
+});
