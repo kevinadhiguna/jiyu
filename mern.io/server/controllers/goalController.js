375d2d8b1dc2295aec5c1e65ad73e11e52728e91
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-13 05:14:45 +0000

    refactor: create and send back goal

--- mern.io/server/controllers/goalController.js
+++ mern.io/server/controllers/goalController.js
@@ -23,7 +23,11 @@ const setGoal = asyncHandler(async (req, res) => {
     throw new Error("Please add a text field !");
   }
 
-  res.status(200).json({ message: "Set a goal" });
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+
+  res.status(200).json(goal);
 });
 
 // @desc    Update a Goal
