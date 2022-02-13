7beb63d62a4d639b612a7026cf1a22e2a535d363
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-13 04:50:37 +0000

    refactor: add cyan color and underline terminal output

--- mern.io/server/config/db.js
+++ mern.io/server/config/db.js
@@ -1,11 +1,9 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const connection = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
-        console.log(`MongoDB connected : ${conn.connection.host}`);
-    } catch (error) {
-        
-    }
-}
+    console.log(`MongoDB connected : ${conn.connection.host}`.cyan.underline);
+  } catch (error) {}
+};
