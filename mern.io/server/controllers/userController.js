a4c5135b0b0673ae8b98a06e74e05f700f414066
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-14 04:03:14 +0000

    refactor: add error handling and things required for registration

--- mern.io/server/controllers/userController.js
+++ mern.io/server/controllers/userController.js
@@ -1,11 +1,26 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+
+const User = require("../models/userModel");
+
 // @desc    Register a new user
 // @route   POST /api/users
 // @access  Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+  // Things required for registration
+  const { name, email, password } = req.body;
+
+  // Error handling
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields !");
+  }
+
   res.json({
     message: "Register a user",
   });
-};
+});
 
 // @desc    Authenticate a user
 // @route   POST /api/users/login
