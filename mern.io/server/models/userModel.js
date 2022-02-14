39116aac1b136340dc9182a484594d9e27f37254
kevinadhiguna <43397636+kevinadhiguna@users.noreply.github.com>
2022-02-14 09:35:30 +0700

    fix(model): rename userModel to userSchema

--- mern.io/server/models/userModel.js
+++ mern.io/server/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const userModel = mongoose.Schema({
+const userSchema = mongoose.Schema({
   name: {
     type: String,
     required: [true, "Please add a name"],
