cdb6a2697f5c6cf4b7ddbc5a2c50d8d6fa82ef9e
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-14 02:56:40 +0000

    style: format code

--- mern.io/server/models/userModel.js
+++ mern.io/server/models/userModel.js
@@ -1,21 +1,24 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please add a name"],
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Please add an email"],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Please add a password"],
-  },
-}, {
-  timestamp: true,
-});
+  {
+    timestamp: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
