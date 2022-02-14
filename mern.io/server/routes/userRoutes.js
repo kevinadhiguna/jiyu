1171c0c0ebc6ed9058a0ead219b9d0ebd5c81159
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-14 03:50:08 +0000

    refactor: add get user data routes

--- mern.io/server/routes/userRoutes.js
+++ mern.io/server/routes/userRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser, loginUser } = require("../controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require("../controllers/userController");
 
 // Registration
 router.post("/", registerUser);
 
 // Login
 router.post("/login", loginUser);
+
+// Get user data
+router.get("/me", getMe);
