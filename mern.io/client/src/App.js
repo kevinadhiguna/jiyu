c6c4bfec9735400837326253fe2fb176a187e2a6
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-21 06:03:49 +0000

    refactor: import pages and register to routes

--- mern.io/client/src/App.js
+++ mern.io/client/src/App.js
@@ -1,11 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// Import pages
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
   return (
     <>
       <Router>
         <div className="container">
-          <h1>My App</h1>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
         </div>
       </Router>
     </>
