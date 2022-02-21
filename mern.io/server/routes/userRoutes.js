9e211dbb172fd432a9b4e7d15f5ea7e130d3b41a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-21 02:59:13 +0000

    chore: add notes

--- mern.io/server/routes/userRoutes.js
+++ mern.io/server/routes/userRoutes.js
@@ -10,13 +10,17 @@ const {
 // Add authentication middleware (one must login first to visit routes)
 const { protect } = require("../middleware/authMiddleware");
 
+// -- Option 1 --
 // Registration
 router.post("/", registerUser);
-
 // Login
 router.post("/login", loginUser);
-
 // Get user data
 router.get("/me", protect, getMe);
+
+// -- Option 2 --
+// router.route("/").post("registerUser");
+// router.route("/login").post("loginUser");
+// router.route("/me").get(protect, getMe);
 
 module.exports = router;
