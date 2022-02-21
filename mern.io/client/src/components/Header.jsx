197d308d0b1d3d5a3539c06b20b201fffcc8f279
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-21 06:08:16 +0000

    refactor: import Link

--- mern.io/client/src/components/Header.jsx
+++ mern.io/client/src/components/Header.jsx
@@ -1,5 +1,6 @@
 // Import font awesome icons from react-icons 
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Header() {
   return <div>Header</div>;
