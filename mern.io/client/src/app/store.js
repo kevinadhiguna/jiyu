0f240ebd1a63ab8717af159fbeef6b3522cb395b
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-21 05:47:09 +0000

    refactor: remove counter reducer

--- mern.io/client/src/app/store.js
+++ mern.io/client/src/app/store.js
@@ -1,8 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: {},
 });
