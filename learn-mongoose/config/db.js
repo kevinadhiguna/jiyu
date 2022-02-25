794dc926b4e9bda84faba728f6258b25d0269b05
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-25 21:48:50 +0000

    refactor: add fallback

--- learn-mongoose/config/db.js
+++ learn-mongoose/config/db.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  const mongo_uri = process.env.MONGO_URI || "mongodb://localhost:27017";
+
   try {
-    const conn = await mongoose.connect();
+    const conn = await mongoose.connect(mongo_uri);
     console.log(`Connected to MongoDB host : ${conn.connection.host}`);
   } catch (errorConnectionDB) {
     console.error(`Failed to connect to MongoDB : ${errorConnectionDB}`);
