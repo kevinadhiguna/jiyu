f67bdad097b96686d136bac2ab8f072b68ab94a8
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-02-25 02:14:26 +0000

    refactor: call dotenv package

--- learn-mongoose/index.js
+++ learn-mongoose/index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const User = require("./model/userModel");
+require("dotenv").config();
 
-// Connect to MongoDB 
+// Connect to MongoDB
 mongoose.connect("");
