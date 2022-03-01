62b294fc496084fc9f8c3cf01251631d2fc98cac
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-01 04:10:16 +0000

    feat(javascript): generate unique id

---
+++ javascript/create-unique-id/create-unique-id.js
@@ -0,0 +1,9 @@
+const uid = () => {
+  const head = Date.now().toString(36);
+  const tail = Math.random().toString(36).substr(2);
+
+  return head + tail;
+};
+
+const id = uid();
+console.log("Unique ID : ", id);
