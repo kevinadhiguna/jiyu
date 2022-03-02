9d201359389dd1e20f1c34a5f9279a30bc44db56
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-02 03:40:16 +0000

    chore: add output

--- javascript/base64.js
+++ javascript/base64.js
@@ -1,10 +1,10 @@
 const originalString = "I live in Los Angeles";
-console.log("Original String :", originalString, "\n");
+console.log("Original String :", originalString, "\n"); // Original String : I live in Los Angeles 
 
 // Encode using base64
 const encodedString = Buffer.from(originalString, "binary").toString("base64");
-console.log("Encoded string :", encodedString);
+console.log("Encoded string :", encodedString); // Encoded string : SSBsaXZlIGluIExvcyBBbmdlbGVz
 
 // Decode using base64
 const decodedString = Buffer.from(encodedString, "base64").toString("binary");
-console.log("Decoded string :", decodedString);
+console.log("Decoded string :", decodedString); // Decoded string : I live in Los Angeles
