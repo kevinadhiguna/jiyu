3dab684f5134a9e4078e3b3474312e97d16c4d76
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-02 02:52:37 +0000

    style: format code using prettier

--- javascript/networking/networking.js
+++ javascript/networking/networking.js
@@ -1,9 +1,9 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-    res.writeHead(200, { "Content-type": "text/plain" });
-    res.end(`Hi, you came from ${ip}\n`);
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  res.writeHead(200, { "Content-type": "text/plain" });
+  res.end(`Hi, you came from ${ip}\n`);
 });
 
 const hostname = process.argv[2] || null;
@@ -11,9 +11,9 @@ const hostname = process.argv[2] || null;
 const port = process.env.PORT || 8050;
 
 server.listen(port, hostname, (err) => {
-    if (err) {
-        return console.log("Error occured :", err);
-    }
+  if (err) {
+    return console.log("Error occured :", err);
+  }
 
-    console.log("Listening to", hostname);
+  console.log("Listening to", hostname);
 });
