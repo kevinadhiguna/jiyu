47c162d47f44cef2fca7cb965e926960d0351320
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-02 03:37:33 +0000

    chore: add program output

--- javascript/array-copy-within-method.js
+++ javascript/array-copy-within-method.js
@@ -1,11 +1,11 @@
 const array = ["Burnes", "Harper", "Tatiis", "Mickey", "Donald"];
 
-console.log(array.copyWithin());
+console.log(array.copyWithin()); // ["Burnes", "Harper", "Tatiis", "Mickey", "Donald"]
 
-console.log(array.copyWithin(0));
-console.log(array.copyWithin(1));
-console.log(array.copyWithin(2));
-console.log(array.copyWithin(3));
-console.log(array.copyWithin(4));
+console.log(array.copyWithin(0)); // ["Burnes", "Harper", "Tatiis", "Mickey", "Donald"]
+console.log(array.copyWithin(1)); // ["Burnes", "Burnes", "Harper", "Tatiis", "Mickey"]
+console.log(array.copyWithin(2)); // ["Burnes", "Burnes", "Burnes", "Burnes", "Harper"]
+console.log(array.copyWithin(3)); // ["Burnes", "Burnes", "Burnes", "Burnes", "Burnes"]
+console.log(array.copyWithin(4)); // ["Burnes", "Burnes", "Burnes", "Burnes", "Burnes"]
 
-console.log(array.copyWithin(1, 2));
+console.log(array.copyWithin(1, 2)); // ["Burnes", "Burnes", "Burnes", "Burnes", "Burnes"]
