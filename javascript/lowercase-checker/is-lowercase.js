2dc6a93de2727fe73f742ffebec6fd56936b8ae9
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-02 02:46:12 +0000

    chore: add output

--- javascript/lowercase-checker/is-lowercase.js
+++ javascript/lowercase-checker/is-lowercase.js
@@ -4,6 +4,6 @@ const word = "Steven";
 const lowercaseWord = "joe";
 const uppercaseWord = "WILLOCK";
 
-console.log(`Is ${word} lowercase ?`, isLowerCase(word) ? "Yup" : "Nah..");
-console.log(`Is ${lowercaseWord} lowercase ?`, isLowerCase(lowercaseWord) ? "Yes" : "No..");
-console.log(`Is ${uppercaseWord} lowercase ?`, isLowerCase(uppercaseWord) ? "Sure" : "Nope..");
+console.log(`Is ${word} lowercase ?`, isLowerCase(word) ? "Yup" : "Nah.."); // Is Steven lowercase ? Nah..
+console.log(`Is ${lowercaseWord} lowercase ?`, isLowerCase(lowercaseWord) ? "Yes" : "No.."); // Is joe lowercase ? Yes
+console.log(`Is ${uppercaseWord} lowercase ?`, isLowerCase(uppercaseWord) ? "Sure" : "Nope.."); // Is WILLOCK lowercase ? Nope..
