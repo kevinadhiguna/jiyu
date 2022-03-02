d6b4d9dcc0cb41cdeea71800559e91fe5174b3e8
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-02 03:04:07 +0000

    refactor: switch server to app and add notes

--- javascript/networking/networking.js
+++ javascript/networking/networking.js
@@ -2,20 +2,28 @@ const http = require("http");
 // Load environment variables
 require("dotenv").config();
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
   res.writeHead(200, { "Content-type": "text/plain" });
   res.end(`Hi, you came from ${ip}\n`);
 });
 
-// Determine hostname/host and port 
+// Determine hostname/host and port
 const PORT = process.env.PORT || 8050;
 const HOSTNAME = process.argv[2] || null;
 
-server.listen(PORT, HOSTNAME, (err) => {
+// Start the app
+const server = app.listen(PORT, HOSTNAME, (err) => {
   if (err) {
     return console.log("Error occured :", err);
   }
 
-  console.log("Listening to", hostname);
+  console.log("Listening to", HOSTNAME);
+});
+
+// Graceful shutdown
+process.on("SIGTERM", () => {
+  server.close(() => {
+    console.log("Received SIGTERM, process terminated..");
+  });
 });
