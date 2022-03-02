05c3a51060722958b87f4abce5122d777bd516d4
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-02 03:28:56 +0000

    style: format code using prettier

--- javascript/vanillajs-redis/redis-client.js
+++ javascript/vanillajs-redis/redis-client.js
@@ -5,33 +5,33 @@ require("dotenv").config();
 // Initialize Redis client
 let jsRedisClient;
 try {
-    jsRedisClient = createHandyClient({
-        host: process.env.REDIS_HOST || "localhost",
-        port: process.env.REDIS_PORT || 6379,
-        password: process.env.REDIS_PW || "redispassword",
-    });
+  jsRedisClient = createHandyClient({
+    host: process.env.REDIS_HOST || "localhost",
+    port: process.env.REDIS_PORT || 6379,
+    password: process.env.REDIS_PW || "redispassword",
+  });
 } catch (error) {
-    console.error(`Error connecting to Redis : ${error}`);
+  console.error(`Error connecting to Redis : ${error}`);
 }
 
 async function redisSetMerchant(merchantId, merchantBrand, merchantName) {
-    // Set key-value
-    await jsRedisClient.set("merchantId", merchantId);
-    await jsRedisClient.set("merchantBrand", merchantBrand);
-    await jsRedisClient.set("merchantName", merchantName);
+  // Set key-value
+  await jsRedisClient.set("merchantId", merchantId);
+  await jsRedisClient.set("merchantBrand", merchantBrand);
+  await jsRedisClient.set("merchantName", merchantName);
 
-    // Get key-value
-    const redisMerchantId = await jsRedisClient.get("merchantId");
-    const redisMerchantBrand = await jsRedisClient.get("merchantBrand");
-    const redisMerchantName = await jsRedisClient.get("merchantName");
+  // Get key-value
+  const redisMerchantId = await jsRedisClient.get("merchantId");
+  const redisMerchantBrand = await jsRedisClient.get("merchantBrand");
+  const redisMerchantName = await jsRedisClient.get("merchantName");
 
-    // Show values
-    console.log("Merchant ID is :", redisMerchantId);
-    console.log("Merchant Brand is :", redisMerchantBrand);
-    console.log("Merchant Name is :", redisMerchantName);
-
-    // Close Redis connection
-    await jsRedisClient.quit();
+  // Show values
+  console.log("Merchant ID is :", redisMerchantId);
+  console.log("Merchant Brand is :", redisMerchantBrand);
+  console.log("Merchant Name is :", redisMerchantName);
+  
+  // Close Redis connection
+  await jsRedisClient.quit();
 }
 
 const id = uuidv4();
