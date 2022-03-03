2a89d379f70708b8f88034af85f988d2d0d01007
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-03 04:47:28 +0000

    style: format code using prettier

--- javascript/object/are-objects-same.js
+++ javascript/object/are-objects-same.js
@@ -1,16 +1,16 @@
 // This method is rapid but limited. In addition, the order of property matters. For more : https://stackoverflow.com/a/1144249.
 function areObjectsSame(obj1, obj2) {
-    return JSON.stringify(obj1) === JSON.stringify(obj2) ? "same" : "different";
+  return JSON.stringify(obj1) === JSON.stringify(obj2) ? "same" : "different";
 }
 
 const henderson = {
-    id: "5gf44",
-    name: "Liam Henderson",
+  id: "5gf44",
+  name: "Liam Henderson",
 };
 
 const hader = {
-    id: "6gf46",
-    name: "Josh Hader",
+  id: "6gf46",
+  name: "Josh Hader",
 };
 
 console.log("Stringified object 1 :", JSON.stringify(henderson));
@@ -22,9 +22,9 @@ console.log("Stringified object 2 :", JSON.stringify(hader));
 // Stringified object 2 : {"id":"6gf46","name":"Josh Hader"}
 
 console.log(
-    "Object 1 and Object 2 are ",
-    areObjectsSame(henderson, hader),
-    "objects."
+  "Object 1 and Object 2 are ",
+  areObjectsSame(henderson, hader),
+  "objects."
 );
 // OUTPUT
 // Object 1 and Object 2 are  different objects.
