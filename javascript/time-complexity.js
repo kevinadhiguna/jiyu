4dbe069757d72db7b03c97bb9cd66631d235249d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-03 04:38:14 +0000

    style: format code using prettier

--- javascript/time-complexity.js
+++ javascript/time-complexity.js
@@ -2,26 +2,25 @@ let n = [1, 2, 4];
 let m = ["tteokbokki", "jjajangmyeon"];
 
 const compute = (a, b) => {
-    // Time complexity : O(n)
-    for (let k = 0; k < array.length; k++) {
-        console.log(k);
-    }
-    for (let l = 0; l < array.length; l++) {
-        console.log(l);
-    }
-    // Note :
-    // Here comes the algebra. Algorithms or operations that have a linear time complexity can be identified by the fact that the number of operations increases linearly with the size of the input.
-    // That means if an operation has to run 100 times for a list that’s 100 items long, then it has linear time complexity.
+  // Time complexity : O(n)
+  for (let k = 0; k < array.length; k++) {
+    console.log(k);
+  }
+  for (let l = 0; l < array.length; l++) {
+    console.log(l);
+  }
+  // Note :
+  // Here comes the algebra. Algorithms or operations that have a linear time complexity can be identified by the fact that the number of operations increases linearly with the size of the input.
+  // That means if an operation has to run 100 times for a list that’s 100 items long, then it has linear time complexity.
 };
 
 const calculate = (n, m) => {
-    // Time complexity : O(n^2)
-    for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array.length; j++) {
-            console.log(i);
-        }
+  // Time complexity : O(n^2)
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      console.log(i);
     }
-    // Note :
-    // Algorithms or operations that have quadratic time are identified as having to perform a linear time operation for each value in an input, not just for the input itself.
+  }
+  // Note :
+  // Algorithms or operations that have quadratic time are identified as having to perform a linear time operation for each value in an input, not just for the input itself.
 };
-
