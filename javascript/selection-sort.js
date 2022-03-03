e2f59760df5db85a0b531cc9177724f83ff5962e
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-03 04:31:19 +0000

    chore: add program output

--- javascript/selection-sort.js
+++ javascript/selection-sort.js
@@ -1,22 +1,22 @@
 const numbers = [5, 66, 3, 2, 456, 123];
 
 function selectionSort(array) {
-    const arr = array.slice();
-    for (let i = 0; i < arr.length - 1; i++) {
-        let minIndex = i;
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
-        // Swap
-        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+  const arr = array.slice();
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
     }
-    return arr;
+    // Swap
+    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+  }
+  return arr;
 }
 
 const sorted_result = selectionSort(numbers);
 
 console.log("=== Selection Sort ===\n");
-console.log("Before sorted : ", numbers);
-console.log("After sorted : ", sorted_result);
+console.log("Before sorted : ", numbers); // Before sorted :  [ 5, 66, 3, 2, 456, 123 ]
+console.log("After sorted : ", sorted_result); // After sorted :  [ 2, 3, 5, 66, 123, 456 ]
