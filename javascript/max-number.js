abd42f59c306332487b1f2b3c3e2ce1c63fce990
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-03 04:13:09 +0000

    chore: add program output

--- javascript/max-number.js
+++ javascript/max-number.js
@@ -1,20 +1,20 @@
 const numbers = [-4, 56, 0, 1267, 12, 7, 98789, 54323, 92812];
 
 function largestNumber(arr) {
-    let arrayNumber = arr.slice();
-    let largestNumber = arrayNumber[0];
-    // (1) For-loop
-    // for (let i = 0; i < arrayNumber.length; i++) {
-    //     largestNumber < arrayNumber[i]
-    //         ? (largestNumber = arrayNumber[i])
-    //         : null;
-    // }
+  let arrayNumber = arr.slice();
+  let largestNumber = arrayNumber[0];
+  // (1) For-loop
+  // for (let i = 0; i < arrayNumber.length; i++) {
+  //     largestNumber < arrayNumber[i]
+  //         ? (largestNumber = arrayNumber[i])
+  //         : null;
+  // }
 
-    // (2) For-each
-    arrayNumber.forEach((e) => {
-        largestNumber < e ? (largestNumber = e) : null;
-    });
-    return largestNumber;
+  // (2) For-each
+  arrayNumber.forEach((e) => {
+    largestNumber < e ? (largestNumber = e) : null;
+  });
+  return largestNumber;
 }
 
-console.log("The largest number is :", largestNumber(numbers));
+console.log("The largest number is :", largestNumber(numbers)); // The largest number is : 98789
