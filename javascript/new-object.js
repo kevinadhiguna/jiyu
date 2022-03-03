05216d39d32d53db7de375a0e3f8e488dc89e434
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-03 04:03:01 +0000

    refactor: show baseball object and add program output

--- javascript/new-object.js
+++ javascript/new-object.js
@@ -6,10 +6,10 @@ baseball.teamNumbers = 30;
 baseball.league = ["AL", "NL"];
 
 console.log(`
-    Baseball is one of the most popular sports in ${baseball.country}.
-    It has a professional league called ${baseball.leagueName}.
-    The competition is separated to ${baseball.league[0]} and ${baseball.league[1]}.
-    The total teams in ${baseball.leagueName} is ${baseball.teamNumbers}.
+  Baseball is one of the most popular sports in ${baseball.country}.
+  It has a professional league called ${baseball.leagueName}.
+  The competition is separated to ${baseball.league[0]} and ${baseball.league[1]}.
+  The total teams in ${baseball.leagueName} is ${baseball.teamNumbers}.
 `);
 // OUTPUT
 //
@@ -18,3 +18,12 @@ console.log(`
 // The competition is separated to AL and NL.
 // The total teams in MLB is 30.
 //
+
+console.log("baseball object looks like this :", baseball);
+// OUTPUT
+// baseball object looks like this : {
+//   country: 'United States of America',
+//   leagueName: 'MLB',
+//   teamNumbers: 30,
+//   league: [ 'AL', 'NL' ]
+// }
