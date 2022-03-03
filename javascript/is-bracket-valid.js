063eecfc55649dc75aab9e5e2d9023ab29070a93
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-03 04:05:20 +0000

    refactor: replace var with let

--- javascript/is-bracket-valid.js
+++ javascript/is-bracket-valid.js
@@ -1,17 +1,17 @@
 function isBracketValid(s) {
-    var total = 0;
-    for (var i = 0; i < s.length; i++) {
-        s[i] == "(" ? total++ : total--;
-        if (total < 0) {
-            return false;
-        }
+  let total = 0;
+  for (let i = 0; i < s.length; i++) {
+    s[i] == "(" ? total++ : total--;
+    if (total < 0) {
+      return false;
     }
-    return total == 0;
+  }
+  return total == 0;
 }
 
 console.log("\n=== Are these brackets valid ? ===\n");
 console.log(
-    "GitHub repository : https://github.com/kevinadhiguna/jiyu/blob/master/javascript/is-bracket-valid.js\n"
+  "GitHub repository : https://github.com/kevinadhiguna/jiyu/blob/master/javascript/is-bracket-valid.js\n"
 );
 
 console.log("Case '()' :", isBracketValid("()"));
