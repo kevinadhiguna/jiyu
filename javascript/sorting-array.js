ab056a4dbd675ae2c7e81619c7fe4c166d3b9934
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-03 04:32:06 +0000

    chore: add program output

--- javascript/sorting-array.js
+++ javascript/sorting-array.js
@@ -5,5 +5,5 @@ const arr = array.slice();
 const sorted_result = arr.sort((a, b) => a - b);
 
 console.log("=== Sorting an Array ===\n");
-console.log("Before sorted :", array);
-console.log("After sorted :", sorted_result);
+console.log("Before sorted :", array); // Before sorted : [ 17, 9, 3, 12, 11 ]
+console.log("After sorted :", sorted_result); // After sorted : [ 3, 9, 11, 12, 17 ]
