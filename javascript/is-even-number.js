f1941cb920400a2edb9fecca03ad3d5eae583a53
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-03 04:11:12 +0000

    chore: add program output

--- javascript/is-even-number.js
+++ javascript/is-even-number.js
@@ -1,11 +1,11 @@
 const numbers = [5, 16, 7, 19, 198, 1037, 1887];
 
 function isEvenNumber(array) {
-    const evenNumbersArray = [];
-    for (let i = 0; i < array.length; i++) {
-        array[i] % 2 == 0 ? evenNumbersArray.push(array[i]) : null;
-    }
-    return evenNumbersArray;
+  const evenNumbersArray = [];
+  for (let i = 0; i < array.length; i++) {
+    array[i] % 2 == 0 ? evenNumbersArray.push(array[i]) : null;
+  }
+  return evenNumbersArray;
 }
 
 // Execute the function
@@ -13,8 +13,17 @@ const result = isEvenNumber(numbers);
 
 // Show results
 console.log(
-    "Among these numbers :\n",
-    numbers,
-    "\nEven Numbers are :\n",
-    result
+  "Among these numbers :\n",
+  numbers,
+  "\nEven Numbers are :\n",
+  result
 );
+// OUTPUT
+// Among these numbers :
+//  [
+//      5,  16,    7,
+//     19, 198, 1037,
+//   1887
+// ]
+// Even Numbers are :
+//  [ 16, 198 ]
