91691338f2d705dc25bea8f7b612954a75158b52
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-03 04:36:45 +0000

    chore: add program output

--- javascript/this.js
+++ javascript/this.js
@@ -2,16 +2,17 @@
 
 // "this" keyword refers to an object it belongs to.
 const angels_anaheim = {
-    manager: "Joe Maddon",
-    best_player: "Shohei Ohtani",
-    quote: function () {
-        return (
-            "Angels manager is " +
-            this.manager +
-            " and Angels' best player is " +
-            this.best_player
-        );
-    },
+  manager: "Joe Maddon",
+  best_player: "Shohei Ohtani",
+  quote: function () {
+    return (
+      "Angels manager is " +
+      this.manager +
+      " and Angels' best player is " +
+      this.best_player
+    );
+  },
 };
 
 console.log("Currently", angels_anaheim.quote());
+// Currently Angels manager is Joe Maddon and Angels' best player is Shohei Ohtani
