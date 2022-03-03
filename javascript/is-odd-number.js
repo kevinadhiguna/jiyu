b3ff5aa3913959802447d617c82c74b24bb322ff
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-03 04:12:03 +0000

    chore: add program output

--- javascript/is-odd-number.js
+++ javascript/is-odd-number.js
@@ -1,11 +1,11 @@
 const numbers = [5, 16, 7, 19, 198, 1037, 1887];
 
 function isOddNumber(array) {
-    const oddNumbersArray = [];
-    for (let i = 0; i < array.length; i++) {
-        array[i] % 2 != 0 ? oddNumbersArray.push(array[i]) : null;
-    }
-    return oddNumbersArray;
+  const oddNumbersArray = [];
+  for (let i = 0; i < array.length; i++) {
+    array[i] % 2 != 0 ? oddNumbersArray.push(array[i]) : null;
+  }
+  return oddNumbersArray;
 }
 
 // Execute the function
@@ -13,8 +13,17 @@ const result = isOddNumber(numbers);
 
 // Show results
 console.log(
-    "Among these numbers :\n",
-    numbers,
-    "\nodd numbers are :\n",
-    result
+  "Among these numbers :\n",
+  numbers,
+  "\nodd numbers are :\n",
+  result
 );
+// OUTPUT
+// Among these numbers :
+//  [
+//      5,  16,    7,
+//     19, 198, 1037,
+//   1887
+// ]
+// odd numbers are :
+//  [ 5, 7, 19, 1037, 1887 ]
