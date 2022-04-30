8f9f1d2914c3065c60264115aee1cffac494abd2
kevinadhiguna <43397636+kevinadhiguna@users.noreply.github.com>
2022-04-30 16:39:58 +0700

    refactor: add healthcheck in nodejs

---
+++ nodejs/healthcheck.js
@@ -0,0 +1,21 @@
+// Source : https://medium.com/sfl-newsroom/nodejs-health-checks-and-overload-protection-368a132a725e
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.end(`Hello PID: ${process.pid}`);
+});
+
+let status = 200;
+setTimeout(() => {
+    status = 503;
+    console.log('Status changed to 503');
+}, 20000);
+app.get('/check', (req, res) => {
+    console.log(`Health Check Request ${status}`);
+    res.status(status).end();
+});
+
+app.listen(process.env.PORT);
+console.log(`Api Server running on ${process.env.PORT} port, PID: ${process.pid}`);
