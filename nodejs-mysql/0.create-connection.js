1d425bd0c9ce2f8acea9cdc9e8eff734d62a6c64
kevinadhiguna <43397636+kevinadhiguna@users.noreply.github.com>
2022-04-30 09:41:02 +0700

    refactor: log mysql users

--- nodejs-mysql/0.create-connection.js
+++ nodejs-mysql/0.create-connection.js
@@ -20,3 +20,11 @@ mysqlConnection.connect((err) => {
     if (err) throw err;
     console.log("Connected to MySQL !");
 });
+
+mysqlConnection.query(
+    'SELECT * FROM mysql.user',
+    function(error, results, fields) {
+        console.log(results); // results contains rows returned by server
+        console.log(fields); // fields contains extra meta data about results, if available
+    }
+);
