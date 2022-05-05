024fc436c0e59abfa6efc600c430b083871c708d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-05-05 09:14:34 +0000

    chore: add another reference and change description

--- nodejs/os.js
+++ nodejs/os.js
@@ -1,6 +1,8 @@
-// Source : https://levelup.gitconnected.com/the-ultimate-guide-to-get-started-with-node-js-4ce54579ceb7
+// Source : 
+// - https://levelup.gitconnected.com/the-ultimate-guide-to-get-started-with-node-js-4ce54579ceb7
+// - https://nodejs.dev/learn/the-nodejs-os-module
 
-// OS architecture
+// Return the string that identifies the underlying architecture, like arm, x64, arm64.
 os.arch();
 
 // Return information on the CPUs available on your system.
