e46bfc6d510e59488fef3ec733b893fac49d986a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-05-05 09:35:31 +0000

    fix: declare os module

--- nodejs/os.js
+++ nodejs/os.js
@@ -2,11 +2,13 @@
 // - https://levelup.gitconnected.com/the-ultimate-guide-to-get-started-with-node-js-4ce54579ceb7
 // - https://nodejs.dev/learn/the-nodejs-os-module
 
+const os = require("os");
+
 // Return the string that identifies the underlying architecture, like arm, x64, arm64.
-os.arch();
+console.log("OS architecture : ", os.arch());
 
 // Return information on the CPUs available on your system.
-os.cpus();
+console.log("Available CPUs on system : ", os.cpus());
 
 // Return the number of bytes that represent the free memory in the system.
-os.freemem();
+console.log("Free memory in the system : ", os.freemem());
