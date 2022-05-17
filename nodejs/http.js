c906f25fda00592578f2beb5e8849320f860fe06
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-05-17 01:41:04 +0000

    refactor: list all http methods

--- nodejs/http.js
+++ nodejs/http.js
@@ -1,4 +1,7 @@
 const http = require("http");
 
+// Lists all the HTTP methods supported:
+console.log("HTTP methods : ", http.METHODS);
+
 // Lists all the HTTP status codes and their description:
 console.log("HTTP status codes : ", http.STATUS_CODES);
