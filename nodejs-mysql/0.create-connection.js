83fae5bc5b3bc983aeddcc6578584516f20b4a28
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-05-26 02:41:27 +0000

    refactor: set default mysql port to 3306

--- nodejs-mysql/0.create-connection.js
+++ nodejs-mysql/0.create-connection.js
@@ -13,7 +13,7 @@ const mysqlConnection = mysql2.createConnection({
   host: process.env.MYSQL_HOST || "localhost",
   user: process.env.MYSQL_USER || "root",
   password: process.env.MYSQL_PASSWORD || "",
-  port: process.env.MYSQL_PORT,
+  port: process.env.MYSQL_PORT || 3306,
 });
 
 // Connect to MySQL
