d074c96a3a1afa08efca42d331088e1d86d7ce1d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-06-03 03:25:31 +0000

    refactor: add cloudflare dns

--- diagrams/aws-request-flow/aws-request-flow.py
+++ diagrams/aws-request-flow/aws-request-flow.py
@@ -2,9 +2,10 @@
 
 from diagrams import Diagram
 
+from diagrams.saas.cdn import Cloudflare
 from diagrams.aws.compute import EC2
 from diagrams.aws.compute import ECS
 from diagrams.aws.database import RDS
 
 with Diagram("Request Flow", show=True):
-  EC2("Nginx Load Balancer") >> ECS("Web Server") >> RDS("Database")
+  Cloudflare("Cloudflare") >> EC2("Nginx Load Balancer") >> ECS("Web Server") >> RDS("Database")
