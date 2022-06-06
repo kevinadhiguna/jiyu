c13dd5b832ce0d8f49c0bdededb5212c41d63a8d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-06-06 01:55:30 +0000

    refactor: provide mlb al cy young 2021 data

--- reactjs-map-array-of-objects/src/App.js
+++ reactjs-map-array-of-objects/src/App.js
@@ -15,6 +15,28 @@ const al_mvp_2021 = [
   { id: "Hj8Vo", name: "Marcus Semien", age: 31, club: "Toronto Blue Jays" },
 ];
 
+// MLB AL Cy Young 2021 data
+const al_cyyoung_2021 = [
+  {
+    id: "Nz7Be",
+    name: "Robbie Ray",
+    age: 30,
+    club: "Toronto Blue Jays",
+  },
+  {
+    id: "Vo8Vx",
+    name: "Gerrit Cole",
+    age: 31,
+    club: "New York Yankees",
+  },
+  {
+    id: "Tr7Qq",
+    name: "Lance Lynn",
+    age: 35,
+    club: "Chicago WhiteSox",
+  },
+];
+
 const App = () => {
   return (
     <>
