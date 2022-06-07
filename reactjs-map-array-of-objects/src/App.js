76da47c5c7774a492b825b0e52533cc8b28c83c0
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-06-07 01:51:53 +0000

    refactor: import mvp and cy young award data

--- reactjs-map-array-of-objects/src/App.js
+++ reactjs-map-array-of-objects/src/App.js
@@ -1,41 +1,5 @@
-// MLB AL MVP 2021 data
-const al_mvp_2021 = [
-  {
-    id: "Zz1Dh",
-    name: "Shohei Ohtani",
-    age: 27,
-    club: "Los Angeles Angels",
-  },
-  {
-    id: "Qi6Kz",
-    name: "Vladimir Guerrero jr.",
-    age: 23,
-    club: "Toronto Blue Jays",
-  },
-  { id: "Hj8Vo", name: "Marcus Semien", age: 31, club: "Toronto Blue Jays" },
-];
-
-// MLB AL Cy Young 2021 data
-const al_cyyoung_2021 = [
-  {
-    id: "Nz7Be",
-    name: "Robbie Ray",
-    age: 30,
-    club: "Toronto Blue Jays",
-  },
-  {
-    id: "Vo8Vx",
-    name: "Gerrit Cole",
-    age: 31,
-    club: "New York Yankees",
-  },
-  {
-    id: "Tr7Qq",
-    name: "Lance Lynn",
-    age: 35,
-    club: "Chicago WhiteSox",
-  },
-];
+import { al_mvp_2021 } from "./mock-data/mvp-data";
+import { al_cyyoung_2021 } from "./mock-data/cyyoung-data";
 
 const App = () => {
   return (
