16e6f8d83183bb697d5d20f524e514ee97dca68d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-06-07 01:51:07 +0000

    chore: add mvp award data

---
+++ reactjs-map-array-of-objects/src/mock-data/mvp-data.js
@@ -0,0 +1,16 @@
+// MLB AL MVP 2021 data
+export const al_mvp_2021 = [
+  {
+    id: "Zz1Dh",
+    name: "Shohei Ohtani",
+    age: 27,
+    club: "Los Angeles Angels",
+  },
+  {
+    id: "Qi6Kz",
+    name: "Vladimir Guerrero jr.",
+    age: 23,
+    club: "Toronto Blue Jays",
+  },
+  { id: "Hj8Vo", name: "Marcus Semien", age: 31, club: "Toronto Blue Jays" },
+];
