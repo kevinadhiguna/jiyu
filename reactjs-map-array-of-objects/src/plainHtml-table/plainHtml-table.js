3b08170c6ed76f314d144b2184f8fbb30a8687e1
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-06-09 05:16:20 +0000

    refactor: add plain htmlt able to a component

---
+++ reactjs-map-array-of-objects/src/plainHtml-table/plainHtml-table.js
@@ -0,0 +1,46 @@
+import { al_mvp_2021 } from "./mock-data/mvp-data";
+import { al_cyyoung_2021 } from "./mock-data/cyyoung-data";
+
+const PlainHtmlTable = () => {
+  return (
+    <>
+      <h3>MLB - AL MVP 2021</h3>
+      <table>
+        <tr>
+          <th>Player ID</th>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Club</th>
+        </tr>
+        {al_mvp_2021.map((data) => (
+          <tr>
+            <td>{data.id}</td>
+            <td>{data.name}</td>
+            <td>{data.age}</td>
+            <td>{data.club}</td>
+          </tr>
+        ))}
+      </table>
+      <br />
+      <h3>MLB - AL Cy Young 2021</h3>
+      <table>
+        <tr>
+          <th>Player ID</th>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Club</th>
+        </tr>
+        {al_cyyoung_2021.map((data) => (
+          <tr>
+            <td>{data.id}</td>
+            <td>{data.name}</td>
+            <td>{data.age}</td>
+            <td>{data.club}</td>
+          </tr>
+        ))}
+      </table>
+    </>
+  );
+};
+
+export default PlainHtmlTable;
