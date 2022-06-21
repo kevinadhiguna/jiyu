b8ca31fc333fd67ca105e6e7c21b9308a686c2ba
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-06-21 03:36:54 +0000

    refactor: remove bootstrap-nuxt and add nuxt-buefy

--- nuxt2r/nuxt.config.js
+++ nuxt2r/nuxt.config.js
@@ -33,8 +33,8 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
+    // https://go.nuxtjs.dev/buefy
+    'nuxt-buefy',
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
