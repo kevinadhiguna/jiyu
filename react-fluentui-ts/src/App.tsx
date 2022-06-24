bd0e40518ba18e3a394b630eafadb3d05e0f3002
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-06-24 09:17:28 +0000

    refactor: import todolist component

--- react-fluentui-ts/src/App.tsx
+++ react-fluentui-ts/src/App.tsx
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 import { Stack } from "@fluentui/react";
 import "./App.css";
+
 import list from "./mock-data/data";
 
+import { TodoList } from "./components/TodoList";
+
 export const App: React.FunctionComponent = () => {
-  const [todo, setTodo] = useState(list);
+  const [todos, setTodos] = useState(list);
   
   return (
     <>
       <div className="wrapper">
         <Stack horizontalAlign="center">
           <h1>Notes app</h1>
+          <Stack style={{ width: 300 }} gap={25}>
+            <TodoList todos={todos} />
+          </Stack>
         </Stack>
       </div>
     </>
