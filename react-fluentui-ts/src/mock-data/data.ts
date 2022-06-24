a703447ea93bda9fbc1e5295a2b7fa3470e5174f
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-06-24 08:33:32 +0000

    refactor: add mock data

---
+++ react-fluentui-ts/src/mock-data/data.ts
@@ -0,0 +1,18 @@
+import { ItodoList } from "./data.d";
+
+let list: ItodoList[] = [
+  {
+    id: 1,
+    name: "Push to GitHub",
+  },
+  {
+    id: 2,
+    name: "Deploy a back-end service",
+  },
+  {
+    id: 3,
+    name: "Review a pull request",
+  }
+];
+
+export default list;
