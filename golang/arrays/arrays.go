f39899c569160282cdb787a6966a6a01a0c487e7
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-06-30 03:55:45 +0000

    refactor: declare and display arrays

---
+++ golang/arrays/arrays.go
@@ -0,0 +1,11 @@
+// Refer
+package main
+import ("fmt")
+
+func main() {
+	var marks = [7]int{99, 96, 93, 91, 90, 88, 100}
+	anotherMarks := [5]int{100, 99, 91, 93, 97}
+
+	fmt.Println("Marks : ", marks)
+	fmt.Println("Another Marks : ", anotherMarks)
+}
