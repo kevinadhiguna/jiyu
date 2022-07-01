f48e3601b6725258adbdc9620d0a1a2f8e43765f
Kevin <43397636+kevinadhiguna@users.noreply.github.com>
2022-07-01 15:30:08 +0700

    refactor: add an array of string

--- golang/arrays/arrays.go
+++ golang/arrays/arrays.go
@@ -4,7 +4,8 @@ import ("fmt")
 
 func main() {
 	var marks = [7]int{99, 96, 93, 91, 90, 88, 100}
-	anotherMarks := [5]int{100, 99, 91, 93, 97}
+	otherMarks := [5]int{100, 99, 91, 93, 97}
+        var students = [7]string{"john", "doe", "kai", "smith", "patrick", "michael", "noah"}
 
 	fmt.Println("Marks : ", marks)
 	fmt.Println("Another Marks : ", anotherMarks)
