ac3c01c8153acc1f0c8e2ddb410b8c43693026f8
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-07-03 02:36:03 +0000

    refactor: create a slice with make function

--- golang/slices/create-slices.go
+++ golang/slices/create-slices.go
@@ -4,11 +4,20 @@ import ("fmt")
 func main()  {
 	// A normal Array
 	var top5mvp = [5]string{"shohei", "judge", "ramirez", "alvarez", "trout"}
-	fmt.Println("Top 5 MVP : ", top5mvp)
+	fmt.Println("Top 5 MVP (a normal array) : ", top5mvp)
 
 	// A slice
 	top3mvp := top5mvp[0:3]
 	fmt.Println("Top 3 MVP (sliced from the top 5 MVP) : ", top3mvp)
-	fmt.Println("Length    : ", len((top3mvp)))
-	fmt.Println("Capacity  : ", cap((top3mvp)))
+	fmt.Println("Top 3 MVP Length    : ", len((top3mvp)))
+	fmt.Println("Top 3 MVP Capacity  : ", cap((top3mvp)))
+
+	createSliceWithMakeFunc()
+}
+
+func createSliceWithMakeFunc()  {
+	top2mvps := make([]string, 2)
+	fmt.Println("Top 2 MVP (created with make function) : ", top2mvps)
+	fmt.Println("Top 2 MVP Length   : ", len(top2mvps))
+	fmt.Println("Top 2 MVP Capacity : ", cap(top2mvps))
 }
