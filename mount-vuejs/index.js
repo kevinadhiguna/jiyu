8471294f41a784a5ab9c61595c9b0ae19633284a
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2022-07-05 14:10:45 +0700

    refactor: hide unnecessary lines

--- mount-vuejs/index.js
+++ mount-vuejs/index.js
@@ -3,11 +3,12 @@ const app = Vue.createApp({
     return {
       teamName: "Los Angeles Angels",
       websiteUrl: "https://www.angels.com",
-      playerName: "Shohei Ohtani",
-      playerPosition: "DH",
-      homeruns: 46,
-      hasManager: true,
-      managerName: "Joe Maddon",
+      // playerName: "Shohei Ohtani",
+      // playerPosition: "DH",
+      // homeruns: 46,
+      // hasManager: true,
+      showPlayers: true,
+      // managerName: "Joe Maddon",
       rosters: [
         {
           name: "Mike Trout",
@@ -28,29 +29,32 @@ const app = Vue.createApp({
           img: "https://images.daznservices.com/di/library/sporting_news/5e/f6/noah-syndergaard-41619-ftr_1xae9ljnym1vd1usagyhhdj7of.png?t=711309146&quality=100&w=1280&h=720",
         },
       ],
-      x: 0,
-      y: 0,
+      // x: 0,
+      // y: 0,
     };
   },
   methods: {
-    changePlayer(playerName) {
-      this.playerName = playerName;
+    // changePlayer(playerName) {
+    //   this.playerName = playerName;
+    // },
+    toggleShowPlayers() {
+      this.showPlayers = !this.showPlayers;
     },
-    toggleShowManager() {
-      this.hasManager = !this.hasManager;
-    },
-    handleEvent(e, data) {
-      console.log("Event :", e);
-      console.log("Event Type :", e.type);
+    // toggleShowManager() {
+    //   this.hasManager = !this.hasManager;
+    // },
+    // handleEvent(e, data) {
+    //   console.log("Event :", e);
+    //   console.log("Event Type :", e.type);
 
-      if (data) {
-        console.log("Data :", data);
-      }
-    },
-    handleMouseMove(e) {
-      this.x = e.offsetX;
-      this.y = e.offsetY;
-    },
+    //   if (data) {
+    //     console.log("Data :", data);
+    //   }
+    // },
+    // handleMouseMove(e) {
+    //   this.x = e.offsetX;
+    //   this.y = e.offsetY;
+    // },
   },
 });
 
