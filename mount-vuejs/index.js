2e102ba8c1c7e7c1be2372fde75751dd800c7a60
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2022-07-05 14:17:09 +0700

    refactor: add a couple of pitchers

--- mount-vuejs/index.js
+++ mount-vuejs/index.js
@@ -23,10 +23,22 @@ const app = Vue.createApp({
           img: "https://sportshub.cbsistatic.com/i/r/2020/08/24/6f2baabd-df27-45d6-acae-792c338d6d6d/thumbnail/1200x675/d3efc6b52fb5862456f74a74c1f780cc/anthony-rendon.png",
         },
         {
+          name: "Shohei Ohtani",
+          position: "SP/DH",
+          hasBeenMVP: true,
+          img: "https://hczhy4dqcfw396ohi4169w11-wpengine.netdna-ssl.com/wp-content/uploads/2022/07/Ohtani-White-Sox-City-Connect.jpg",
+        },
+        {
           name: "Noah Syndergaard",
           position: "SP",
           hasBeenMVP: false,
           img: "https://images.daznservices.com/di/library/sporting_news/5e/f6/noah-syndergaard-41619-ftr_1xae9ljnym1vd1usagyhhdj7of.png?t=711309146&quality=100&w=1280&h=720",
+        },
+        {
+          name: "Patrick Sandoval",
+          position: "SP",
+          hasBeenMVP: false,
+          img: "https://www.presstelegram.com/wp-content/uploads/2019/09/GettyImages-1172368294-1.jpg?w=1860",
         },
       ],
       // x: 0,
