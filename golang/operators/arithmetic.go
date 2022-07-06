97fc986b1bde4906c43db5ddaa12d5b14fcec489
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-07-06 05:05:23 +0000

    refactor: add arithmrtic operators

---
+++ golang/operators/arithmetic.go
@@ -0,0 +1,33 @@
+// Reference : https://www.w3schools.com/go/go_arithmetic_operators.php
+package main
+import ("fmt")
+
+func main() {
+	var (
+		x int = 100
+		y int = 50
+		i float32 = 200.6
+		j float32 = 156.7
+	)
+
+	fmt.Println("X + Y : ", addition(x, y))
+	fmt.Println("I - J : ", subtraction(i, j))
+	fmt.Println("I * J : ", multiplication(i, j))
+	fmt.Println("X / Y : ", division(x, y))
+}
+
+func addition(number1 int, number2 int) int {
+	return number1 + number2
+}
+
+func subtraction(number1 float32, number2 float32) float32 {
+	return number1 - number2
+}
+
+func multiplication(number1 float32, number2 float32) float32 {
+	return number1 * number2
+}
+
+func division(number1 int, number2 int) int {
+	return number1 / number2
+}
