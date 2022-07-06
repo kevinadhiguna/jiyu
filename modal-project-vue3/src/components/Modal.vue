330d9979e53288623b0a05c32b19e441b3fca616
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2022-07-06 15:43:14 +0700

    refactor: add modal component

---
+++ modal-project-vue3/src/components/Modal.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <p>Modal Content</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: #fff;
+  border-radius: 10px;
+}
+</style>
