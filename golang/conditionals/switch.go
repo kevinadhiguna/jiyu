10e98c615f33e6ab16e111b58684d4ffc8a8ab5b
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-07-09 02:08:28 +0000

    refactor: add a multicase switch

--- golang/conditionals/switch.go
+++ golang/conditionals/switch.go
@@ -15,6 +15,27 @@ func main()  {
 		case "windows":
 			fmt.Println("The operating system created by Microsoft !")
 		default:
-			fmt.Println("Oops, the OS was not available in list. Please check ")
+			fmt.Println("Oops, the OS was not available in  list...")
+	}
+
+	multicaseSwitch()
+}
+
+func multicaseSwitch() {
+	region := "California"
+
+	switch region {
+		case "Tokyo", "Osaka":
+			fmt.Println(region, "is in Japan")
+		case "New York", "San Francisco", "California":
+			fmt.Println(region, "belongs to the U.S.")
+		case "Madrid":
+			fmt.Println(region, "is a city in Spain")
+		default:
+			if (region != "") {
+				fmt.Println(region, "is outside of Japan, U.S., and Spain")
+			} else {
+				fmt.Println("The region is not provided in database...")
+			}
 	}
 }
