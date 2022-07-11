166bf4b78064418abaa75934a3aceaf4435a7f37
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-07-11 01:25:18 +0000

    refactor: add continue statement in for loop

--- golang/loops/for-loops.go
+++ golang/loops/for-loops.go
@@ -4,5 +4,16 @@ import ("fmt")
 func main()  {
 	for i := 0; i < 100; i +=10 {
 		fmt.Println("i = ", i)
-	} 
+	}
+
+	continueStatement()
+}
+
+func continueStatement()  {
+	for j := 0; j < 10; j++ {
+		if j == 7 {
+			continue
+		}
+		fmt.Println("j = ", j)
+	}
 }
