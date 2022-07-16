15597734eedea7c12f9b4239f09d5b1239824dd9
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-07-16 04:19:32 +0000

    fix: add a web socket URL to prevent mixed content error

--- modal-project-vue3/vue.config.js
+++ modal-project-vue3/vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  // Need an extra config to avoid Vue.js (v3) mixed content error when opened from Gitpod in a browser (see https://github.com/gitpod-io/gitpod/issues/11170)
+  devServer: {
+    allowedHosts: "all",
+    client: {
+      // Reference : https://webpack.js.org/configuration/dev-server/#websocketurl
+      webSocketURL: "auto://0.0.0.0:0/ws",
+    },
+  },
 });
