40b9b87c43b481c47cf889d4d00b3de17cfd5293
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-07-18 02:37:19 +0000

    refactor: load a page and create examples

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -11,6 +11,15 @@ type Page struct {
 	Body []byte
 }
 
+func main()  {
+	p1 := &Page{Title: "MLB All Stars 2022", Body: []byte("Come and watch MLB All Stars 2022 in the Dodgers Stadium, Los Angeles !")}
+	p1.save()
+
+	p2, _ := loadPage("MLB All Stars 2022")
+	// fmt.Println(p2.Body) 			// Output : [67 111 109 101 32 97 110 100 32 119 97 116 99 104 32 77 76 66 32 65 108 108 32 83 116 97 114 115 32 50 48 50 50 32 105 110 32 116 104 101 32 68 111 100 103 101 114 115 32 83 116 97 100 105 117 109 44 32 76 111 115 32 65 110 103 101 108 101 115 32 33]
+	fmt.Println(string(p2.Body))  // Output : Come and watch MLB All Stars 2022 in the Dodgers Stadium, Los Angeles !
+}
+
 func (p *Page) save() error  {
 	filename := p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600) // '0600' is the permission 
