dda32458cb0a88d144b0611a1c9ec07c86bca0bf
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-07-18 02:18:46 +0000

    refactor: handle errors when loading a page

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -16,8 +16,18 @@ func (p *Page) save() error  {
 	return os.WriteFile(filename, p.Body, 0600) // '0600' is the permission 
 }
 
-func loadPage(title string) *Page {
+func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
-	body, _ := os.ReadFile(filename)
-	return &Page{Title: title, Body: body}
+	body, err := os.ReadFile(filename)
+	// Return two (in case of an error) :
+	// (1) 'nil' as '*Page'
+	// (2) 'err' as 'error'
+	if err != nil {
+		return nil, err
+	}
+
+	// Return two :
+	// (1) '&Page{Title: title, Body: body}' as '*Page'
+	// (2) 'nil' as 'error' (since this line is executed when no error occurrs)
+	return &Page{Title: title, Body: body}, nil
 }
