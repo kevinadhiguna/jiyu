bcc97e5f994d2b064b56ac88ff415e3aa07eca0f
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-07-22 04:01:22 +0000

    refactor: add edit handler function

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -52,3 +52,17 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	p, _ := loadPage(title)
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
