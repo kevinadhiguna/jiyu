94357593053c6f98a0b3fa086e6fd703e7a32b92
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-09-12 10:29:16 +0700

    refactor: add aws security group resource that only allows HTTP, HTTPS, SSH protocols
    port 80, 443, 22 are open

--- terraform/project.tf
+++ terraform/project.tf
@@ -91,6 +91,46 @@ resource "aws_route_table_association" "a" {
 }
 
 # 6) Create security group to allow port 22, 80, 443
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow web inbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "allow_web"
+  }
+}
 
 # 7) Create a network interface with an IP in the subnet that was created in step 4
 
