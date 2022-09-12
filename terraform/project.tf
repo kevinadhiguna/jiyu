a389b259ca5d0e65a37ed95c51e354ff6e49db6e
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-09-12 10:21:46 +0700

    refactor: add a variable to store cidr block of prod-vpc

--- terraform/project.tf
+++ terraform/project.tf
@@ -4,6 +4,16 @@ provider "aws" {
   secret_key = ""
 }
 
+# -- Variable --
+# The actual values can be stored in a separate file named terraform.tfvars.
+#   Otherwise, pass an argument to tell Terraform the env vars filename.
+#     Example   : "$ terraform apply -var-file <envvars-filename>"
+#     Example 2 : For 'example.tfvars', run "$ terraform apply -var-file example.tfvars" 
+variable "prod-vpc-cidr-block" {
+  description = "prod-vpc cidr_block"
+  type        = string # <- To avoid input like ["10.0.0.0/16"] instead of 10.0.0.0/16. Useful when working on large codebase.
+}
+
 # 1) Create VPC
 
 # 2) Create internet gateway
