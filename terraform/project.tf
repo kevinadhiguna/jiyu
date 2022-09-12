0a2984ac05d684a12dc1a5316951ec747b593aa5
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-09-12 10:20:15 +0700

    chore: set up an example project and list the steps

---
+++ terraform/project.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region     = ""
+  access_key = ""
+  secret_key = ""
+}
+
+# 1) Create VPC
+
+# 2) Create internet gateway
+
+# 3) Create custom route table
+
+# 4) Create a subnet
+
+# 5) Associate subnet with route table
+
+# 6) Create security group to allow port 22, 80, 443
+
+# 7) Create a network interface with an IP in the subnet that was created in step 4
+
+# 8) Assign an elastic IP to the network interface created in step 7
+
+# 9) Create Ubuntu server and install/enable Apache2
