fbd05a48366c299a9920ac262f762fe8e0418a6a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-09-12 10:17:22 +0700

    chore: add valid and invalid output examples

--- terraform/main.tf
+++ terraform/main.tf
@@ -73,3 +73,18 @@ output "subnet-1_cidr_block" {
   # Will display the value of cidr_block in aws_subnet.subnet-1 resource
   value = aws_subnet.subnet-1.cidr_block
 }
+
+# -- Invalid example of Output --
+# (Error) Attribute redefined: The argument "value" was already set at main.tf:69,3-8. Each argument may be set only once.
+# output "subnet-1_cidr_block" {
+#   value = aws_subnet.subnet-1.cidr_block
+#   value = aws_vpc.dev-vpc,cidr_block
+# }
+
+# -- Valid example of Output --
+# output "subnet-1_cidr_block" {
+#   value = aws_subnet.subnet-1.cidr_block
+# }
+# output "dev-vpc_cidr_block" {
+#   value = aws_vpc.dev-vpc.cidr_block
+# }
