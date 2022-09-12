7cb239003ed70ad09f1f374d6f5d0c3864a44ef6
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-09-12 10:24:14 +0700

    refactor: add a custom aws route table resource

--- terraform/project.tf
+++ terraform/project.tf
@@ -34,6 +34,23 @@ resource "aws_internet_gateway" "gw" {
 }
 
 # 3) Create custom route table
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    "Name" = "Prod"
+  }
+}
 
 # 4) Create a subnet
 
