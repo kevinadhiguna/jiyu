d100f169b83d2439c284b6a239e1ec12cddf6a51
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-09-12 10:16:52 +0700

    chore: add subnet cidr block output

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,3 +63,13 @@ resource "aws_subnet" "subnet-1" {
     Name = "dev-subnet"
   }
 }
+
+# "output" in Terraform is just like console.log in JavaScript
+# The value is shown when executing :
+#   1) $ terraform output
+#   2) $ terraform refresh
+#   3) $ terraform apply (not recommended if purpose is just to display the value)
+output "subnet-1_cidr_block" {
+  # Will display the value of cidr_block in aws_subnet.subnet-1 resource
+  value = aws_subnet.subnet-1.cidr_block
+}
