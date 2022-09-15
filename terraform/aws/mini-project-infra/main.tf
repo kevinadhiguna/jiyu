1ff04c382c3ac89472388b3a6f527a09e2cd4d19
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-09-15 09:42:18 +0700

    refactor: separate variables definition into another file

--- terraform/aws/mini-project-infra/main.tf
+++ terraform/aws/mini-project-infra/main.tf
@@ -1,22 +1,3 @@
-# -- Variable --
-# The actual values can be stored in a separate file named terraform.tfvars.
-#   Otherwise, pass an argument to tell Terraform the env vars filename.
-#     Example   : "$ terraform apply -var-file <envvars-filename>"
-#     Example 2 : For 'example.tfvars', run "$ terraform apply -var-file example.tfvars" 
-variable "prod-vpc-cidr-block" {
-  description = "prod-vpc cidr_block"
-  type        = string # <- To avoid input like ["10.0.0.0/16"] instead of 10.0.0.0/16. Useful when working on large codebase.
-}
-
-variable "subnet-values" {
-  description = "Values of subnet properties"
-  type = list(object({
-    cidr_block        = string
-    availability_zone = string
-    tags              = string
-  }))
-}
-
 # 1) Create VPC
 resource "aws_vpc" "prod-vpc" {
   # cidr_block = "10.0.0.0/16" # <- not using Variable
