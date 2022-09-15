a91a6c99749b6514263f00b0cec5906d6c63e224
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-09-15 09:44:57 +0700

    refactor: separate provider definition into another file

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.13.0"
-    }
-  }
-}
-
-provider "docker" {}
-
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
   keep_locally = false
