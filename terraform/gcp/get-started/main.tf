456de3f2f7f305321caeec97e36c1462bc010bdb
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-09-16 13:33:31 +0700

    refactor: add tags to google compute instance

--- terraform/gcp/get-started/main.tf
+++ terraform/gcp/get-started/main.tf
@@ -5,6 +5,7 @@ resource "google_compute_network" "vpc_network" {
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1.micro"
+  tags         = ["web", "dev"]
 
   boot_disk {
     initialize_params {
