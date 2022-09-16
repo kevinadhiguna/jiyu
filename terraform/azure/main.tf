685d9318343d84a66c617cf0e3488a4175bd3d4e
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-09-16 11:04:03 +0700

    refactor: add a virtual network

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -3,3 +3,11 @@ resource "azurerm_resource_group" "rg" {
   name     = "my-terraform-resource-group"
   location = "westus2"
 }
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "my-terraform-resource-group-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg
+}
