5258decde554d69b8aae282f1608ab1a723edaf2
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-09-16 13:32:26 +0700

    refactor: add google compute instance resource

--- terraform/gcp/get-started/main.tf
+++ terraform/gcp/get-started/main.tf
@@ -1,3 +1,14 @@
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1.micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian9"
+    }
+  }
+}
