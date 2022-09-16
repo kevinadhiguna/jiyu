fe8686067c8bf32a2a7e3e589408c3e87de69720
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-09-16 14:00:03 +0700

    refactor: add oci subnet resource

--- terraform/oracle-cloud/get-started/main.tf
+++ terraform/oracle-cloud/get-started/main.tf
@@ -5,3 +5,13 @@ resource "oci_core_vcn" "internal" {
   compartment_id = "Oc6ddhw" # <- just an example
   display_name   = "oci-internal-vcn"
 }
+
+resource "oci_core_subnet" "dev" {
+  vcn_id     = oci_core_vcn.internal.id
+  cidr_block = "172.16.0.0/24"
+  # compartment_id = "<Compartment_OCID>"
+  compartment_id             = "Oc6ddhw" # <- just an example
+  display_name               = "dev-subnet-1"
+  prohibit_public_ip_on_vnic = true
+  dns_label                  = "dev"
+}
