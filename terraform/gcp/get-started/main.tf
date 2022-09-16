faba859ad47fcf8f863c310f8e1b6d5c8b3c74a6
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-09-16 13:36:42 +0700

    refactor: change boot disk to google container optimized os

--- terraform/gcp/get-started/main.tf
+++ terraform/gcp/get-started/main.tf
@@ -9,7 +9,9 @@ resource "google_compute_instance" "vm_instance" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian9"
+      # image = "debian-cloud/debian9"
+      image = "cos-cloud/cos-stable" # <- Change Debian 9 image to Google's Container-Optimized OS (DESTRUCTIVE CHANGE !).
+      # Destructive Change: A destructive change is a change that requires the provider to replace the existing resource rather than updating it.
     }
   }
 
