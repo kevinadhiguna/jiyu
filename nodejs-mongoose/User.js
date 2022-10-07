33d4594841b9d1602bfee247afc8b810d6e54393
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-10-07 20:15:05 +0700

    refactor: add user schema

--- nodejs-mongoose/User.js
+++ nodejs-mongoose/User.js
@@ -5,4 +5,39 @@ const addressSchema = new mongoose.Schema({
   city: String
 });
 
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: {
+    type: Number,
+    min: 1,
+    max: 200,
+    // An odd number will result in : "Error saving account 2 :  User validation failed: age: 25 is not an even number"
+    validate: {
+      validator: v => v % 2 === 0,
+      message: props => `${props.value} is not an even number`
+    }
+  },
+  email: {
+    type: String,
+    minLength: 4,
+    required: true,
+    lowercase: true
+  },
+  createdAt: {
+    type: Date,
+    immutable: true, // <- does not allow changes
+    default: Date.now()
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now()
+  },
+  bestFriend: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "User"
+  },
+  hobbies: [String],
+  address: addressSchema
+});
+
 module.exports = mongoose.model("User", userSchema);
