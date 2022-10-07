3645208c6cae3b6c32357bc98a8f058e73aa9a92
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-10-07 20:19:34 +0700

    refactor: add run function to create accounts

--- nodejs-mongoose/app.js
+++ nodejs-mongoose/app.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const User = require("./User");
+
 require("dotenv").config();
 
 const mongodbURL = process.env.MONGODB_URL || "mongodb://localhost/baseball";
@@ -8,3 +10,65 @@ try {
 } catch (error) {
   console.error("Error connecting to MongoDB : ", error);  
 }
+
+// Create a user (account)
+async function run() {
+  try {
+    // -- CREATE --
+    const account = await User.create({ name: "nianda", age: 22, email: "nianda@google.com" });
+    // The code above does the same thing as the code below :
+    // const account = new User({ name: "Michelle", age: 22, email: "michelle@google.com" });
+    // await account.save();
+
+    // -- UPDATE --
+    // Update age property to 24 then save it
+    account.age = 24;
+    await account.save(); 
+
+    console.info("Account 1 created : ", account);
+  } catch (error) {
+    console.error("Error saving account 1 : ", error.message);    
+  }
+
+  try {
+    const account2 = await User.create({
+      name: "ghina",
+      age: 26,
+      email: "ghina@ballysportsnorth.us",
+      address: {
+        street: "Freeway street",
+        city: "Anaheim"
+      },
+      hobbies: ["Player Interview", "Sport Analysis"],
+    });
+
+    console.info("Account 2 created : ", account2);
+  } catch (error) {
+    console.error("Error saving account 2 : ", error.message);
+  }
+
+  try {
+    const account3 = await User.create({
+      name: "zahra",
+      age: 28,
+      email: "ZahRa@yahoo.com", // <- will be converted to lowercase automatically (according to user model)
+      address: {
+        street: "Atlanta street",
+      },
+      hobbies: ["Baseball", "Pitching"],
+    });
+
+    account3.bestFriend = "633b0479d1b1e78c1bde3e0c";
+    await account3.save();
+
+    console.info("Account 3 created : ", account3);
+  } catch (error) {
+    console.error("Error saving account 3 : ", error.message);
+  }
+}
+
+async function exec() {
+  await run();
+}
+
+exec();
