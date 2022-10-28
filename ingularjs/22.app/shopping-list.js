ab4a05808754707241265036ccc697835927af90
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-10-28 16:00:37 +0700

    refactor: add adding items flow

--- ingularjs/22.app/shopping-list.js
+++ ingularjs/22.app/shopping-list.js
@@ -1,4 +1,17 @@
 const myShoppingList = angular.module("myShoppingList", []);
 myShoppingList.controller("myShoppingListController", function ($scope) {
   $scope.products = ["Milk", "Bread", "Cheese"];
+
+  // Adding items
+  $scope.addItem = function () {
+    $scope.errortext = "";
+
+    if (!$scope.addedProduct) { return; }
+
+    if ($scope.products.indexOf($scope.addedProduct) == -1) {
+      $scope.products.push($scope.addedProduct);
+    } else {
+      $scope.errortext = "The item is already in your shopping list !";
+    }
+  }
 });
