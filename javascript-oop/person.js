57daa9a9f3ba86f05cac32c142f36c73321f907a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-10-31 18:33:11 +0700

    refactor: add age, height, and weight with some functions

--- javascript-oop/person.js
+++ javascript-oop/person.js
@@ -1,17 +1,28 @@
 class Person {
-  name;
-
-  constructor (name) {
+  constructor (name, age, height, weight) {
     this.name = name;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
   }
 
   introduceSelf() {
-    console.log(`Hi, I am ${this.name}`);
+    console.log(`Hi, I am ${this.name}.`);
+  }
+
+  currentAge() {
+    console.log(`I am ${this.age} years old.`);
+  }
+
+  physique() {
+    console.log(`I am ${this.height} cm tall and am ${this.weight} kg.`);
   }
 }
 
-const jack = new Person('jack');
+const jack = new Person('jack', 20, 200, 85);
 jack.introduceSelf(); // Hi, I am jack
+jack.currentAge();
+jack.physique();
 
 const doe = new Person();
 doe.introduceSelf(); // Hi, I am undefined
