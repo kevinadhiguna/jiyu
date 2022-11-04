7b87697bbf8192b81e6092fd24062cf6cd91463f
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-04 17:11:45 +0700

    refactor: calculate perimeter of a shape

--- javascript-oop/shape.js
+++ javascript-oop/shape.js
@@ -4,4 +4,9 @@ class Shape {
     this.sides = sides;
     this.sideLength = sideLength;
   }
+
+  calcPerimeter() {
+    const perimeter = this.sides * this.sideLength; 
+    console.log(`${this.name} perimeter is ${perimeter}`);
+  }
 }
