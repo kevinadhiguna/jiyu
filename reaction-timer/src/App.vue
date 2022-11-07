112c10bedc07506c8d5aca173c957c4eed428796
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-07 10:28:57 +0700

    style: change basic text color

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -15,7 +15,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
