b65a11fcc5d43982a5d050212cb505b3b9427b11
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-08 11:32:38 +0700

    refactor: add start method when button is clicked

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Reaction Timer</h1>
-    <button :disabled="isPlaying">play</button>
+    <button @click="start" :disabled="isPlaying">play</button>
   </div>
 </template>
 
@@ -15,7 +15,15 @@ export default {
     }
   },
   components: {},
-  methods: {}
+  methods: {
+    start() {
+      // The most it could be is 7s (7000ms) and the least it could be is 2s (2000ms)
+      this.delay = 2000 + Math.random() * 5000; // Math.random() gives a value between 0 and 1
+      console.log('this.delay : ', (this.delay / 1000).toFixed(3), 'seconds');
+
+      this.isPlaying = true;
+    }
+  }
 }
 </script>
 
