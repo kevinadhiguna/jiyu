3b056d6b722cf856b0e778e23c5daca527b3fb15
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-08 11:30:39 +0700

    refactor: add a block component

---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="block">
+    Click me
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['delay']
+}
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: #0faf87;
+  color: #fff;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
