9b78ad0e31bc24ee67433a3686b8c1e90c353e00
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-10 10:27:38 +0700

    refactor: create customer objects

--- python-oop/customer.py
+++ python-oop/customer.py
@@ -3,3 +3,12 @@ class Customer:
   def __init__(self, name, membership_type):
     self.name = name
     self.membership_type = membership_type
+
+# Create objects from the Customer class
+customer = Customer('Jack', 'Gold')
+customer2 = Customer('Kate', 'Silver')
+print('customer.name : ', customer.name) # Jack
+print('customer.membership_type : ', customer.membership_type) # Gold
+print('customer2.name : ', customer2.name) # Kate
+print('customer2.membership_type : ', customer2.membership_type) # Silver
+print('type of customer variable : ', type(customer)) # <class '__main__.Customer'>
