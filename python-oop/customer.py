ff1a60daec02b0b10c9d61a1f14a8f8dd151d416
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-10 10:29:21 +0700

    refactor: create customer objects in one line

--- python-oop/customer.py
+++ python-oop/customer.py
@@ -12,3 +12,11 @@ print('customer.membership_type : ', customer.membership_type) # Gold
 print('customer2.name : ', customer2.name) # Kate
 print('customer2.membership_type : ', customer2.membership_type) # Silver
 print('type of customer variable : ', type(customer)) # <class '__main__.Customer'>
+
+# Another way to create objects in one line
+anotherCustomer = [Customer('Evgeny', 'Silver'), Customer('Selena', 'Bronze')]
+print('first anotherCustomer name : ', anotherCustomer[0].name) # Evgeny
+print('first anotherCustomer membership_type : ', anotherCustomer[0].membership_type) # Silver
+print('second anotherCustomer name : ', anotherCustomer[1].name) # Selena
+print('second anotherCustomer membership_type : ', anotherCustomer[1].membership_type) # Bronze
+print('type of anotherCustomer variable : ', type(anotherCustomer)) # <class 'list'>
