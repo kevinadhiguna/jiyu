a906d0deb6e07b16f8bd8aa6829a6ea636464f0e
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-13 16:27:16 +0700

    fix: replace params with property

--- python-oop/customer.py
+++ python-oop/customer.py
@@ -8,7 +8,7 @@ class Customer:
     # One can do something such as cost calculation, invoke API calls, etc. here
     self.membership_type = new_membership_type
 
-  @params
+  @property
   def name(self):
     return self._name
 
