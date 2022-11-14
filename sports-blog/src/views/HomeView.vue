eccfe48e3516107a9207178a4e92efdf81721e77
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-14 20:02:30 +0700

    refactor: add watch and watchEffect functions

--- sports-blog/src/views/HomeView.vue
+++ sports-blog/src/views/HomeView.vue
@@ -12,13 +12,27 @@
 </template>
 
 <script>
-import { computed, ref } from 'vue';
+import { computed, ref, watch, watchEffect } from 'vue';
 
 export default {
   name: 'HomeView',
   setup() {
     const search = ref('');
     const names = ref(['Dasha', 'Zhenya', 'Alina', 'Anastasia', 'Jessica', 'Medlyn', 'Kate']);
+
+    // Triggered :
+    // - everytime the variables (dependency) changes. In this case, it is 'search'.
+    watch(search, () => {
+      console.log('triggered by watch function !');
+    });
+
+    // Triggered :
+    // - at first (component mounted)
+    // - when a variable inside the watchEffect function changes/is affected
+    // In addition, watchEffect() is preferred to watch() since one does not have to list watch list (list of variables that will be the dependency of this function)
+    watchEffect(() => {
+      console.log('search keyword (watchEffect) : ', search.value);
+    });
 
     const matchingNames = computed(() => {
       return names.value.filter(name => name.toLowerCase().includes(search.value.toLowerCase()));
