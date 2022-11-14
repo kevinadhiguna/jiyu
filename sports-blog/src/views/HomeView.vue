7190e4629f78168b83cc9d4fdf6a299b4ad96647
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-14 16:54:59 +0700

    refactor: implement reactive in composition api

--- sports-blog/src/views/HomeView.vue
+++ sports-blog/src/views/HomeView.vue
@@ -1,32 +1,43 @@
 <template>
   <div class="home">
     <h1>Home view</h1>
-    <p>My name is {{ name }} and my age is {{ age }}</p>
-    <div>
-      <button @click="handleClick">Click me</button>
-      <button @click="age++">Add age</button>
-    </div>
-    <input type="text" v-model="name" />
+    <h2>Refs</h2>
+    <p>{{ bloggerOne.name }} - {{ bloggerOne.age }}</p>
+    <button @click="updateBloggerOne">Update Blogger one</button>
+    <h2>Reactive</h2>
+    <p>{{ bloggerTwo.name }} - {{ bloggerTwo.age }} - {{ bloggerThreeName }}</p>
+    <button @click="updateBloggerTwo">Update Blogger two</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
 export default {
   name: 'HomeView',
   setup() {
-    // To make variables reactive to changes, wrap them in 'ref()' method
-    const name = ref('Kai');
-    const age = ref(25);
+    const bloggerOne = ref({ name: 'Kyle', age: 17 });
+    const bloggerTwo = reactive({ name: 'Anastasia', age: 15 });
 
-    const handleClick = () => {
-      // To change the variable values, access '.value' property of each variable ('.value' property is only accessed in setup() method)
-      name.value = 'Doe';
-      age.value = 27;
+    // One of the drawbacks of reactive is it can not be used to a primitive variable
+    let bloggerThreeName = reactive('Emma');
+
+    const updateBloggerOne = () => {
+      bloggerOne.value.name = 'Wright';
+      bloggerOne.value.age = 20;
     }
 
-    return { name, age, handleClick }
+    const updateBloggerTwo = () => {
+      // With reactive, one does not have to access value property to access the variable's value
+      bloggerTwo.name = 'Alina';
+      bloggerTwo.age = 18;
+
+      // None of these work
+      bloggerThreeName = 'Jessica';
+      // bloggerThreeName.value = 'Jessica';
+    }
+
+    return { bloggerOne, bloggerTwo, updateBloggerOne, updateBloggerTwo, bloggerThreeName }
   },
 }
 </script>
