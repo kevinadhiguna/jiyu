0781afd5a6b0f05c30768d2d8c77f425dc9c3ebe
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-14 15:40:08 +0700

    refactor: return a function

--- sports-blog/src/views/HomeView.vue
+++ sports-blog/src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <h1>Home view</h1>
     <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click me</button>
   </div>
 </template>
 
@@ -24,7 +25,11 @@ export default {
     let name = 'Kai';
     let age = 25;
 
-    return { name, age }
+    const handleClick = () => {
+      console.log('Button clicked!');
+    }
+
+    return { name, age, handleClick }
   },
 }
 </script>
