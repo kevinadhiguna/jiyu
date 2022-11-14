48f33d4edb0132adaec236241dfb5420f39341e8
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-14 17:12:44 +0700

    refactor: add names and iterate them one by one

--- sports-blog/src/views/HomeView.vue
+++ sports-blog/src/views/HomeView.vue
@@ -2,21 +2,22 @@
   <div class="home">
     <h1>Home view</h1>
     <h2>Computed</h2>
-    <p>Name : {{ name }}</p>
+    <!-- :key = "name" since all names are unique (no same name) -->
+    <div v-for="name in names" :key="name">
+      <p>{{ name }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 
 export default {
   name: 'HomeView',
   setup() {
-    const name = computed(() => {
-      return 'Kai';
-    });
+    const names = ref(['Dasha', 'Zhenya', 'Alina', 'Anastasia', 'Jessica', 'Medlyn', 'Kate']);
 
-    return { name }
+    return { names }
   },
 }
 </script>
