f90aaa6d51a47944e604d5e266f49f3c822ffa87
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-14 16:04:55 +0700

    refactor: add template refs

--- sports-blog/src/views/HomeView.vue
+++ sports-blog/src/views/HomeView.vue
@@ -1,35 +1,33 @@
 <template>
   <div class="home">
     <h1>Home view</h1>
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { ref } from 'vue';
 
 export default {
   name: 'HomeView',
   setup() {
-    console.log('setup');
 
-    // These are not reacting values, hence the values do not change just like :
-    // data() {
-    //   return {
-    //     name: 'Kai',
-    //     age: 25
-    //   }
-    // }
-    // in Options API
+    // In Composition API, 'this' will get undefined in setup function
+    // console.log(this); // undefined
+
+    // Template refs
+    const p = ref(null);
+
     let name = 'Kai';
     let age = 25;
 
     const handleClick = () => {
-      console.log('Button clicked!');
+      console.log('p : ', p);
+      console.log('p.value : ', p.value); // <p>My name is Kai and my age is 25</p>
     }
 
-    return { name, age, handleClick }
+    return { name, age, handleClick, p }
   },
 }
 </script>
