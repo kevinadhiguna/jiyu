33afcc2fe8b82d4c148a5059947a579d63d9356a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-14 16:29:58 +0700

    refactor: make variables reactive to changes

--- sports-blog/src/views/HomeView.vue
+++ sports-blog/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h1>Home view</h1>
-    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
+    <p>My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">Click me</button>
   </div>
 </template>
@@ -12,31 +12,17 @@ import { ref } from 'vue';
 export default {
   name: 'HomeView',
   setup() {
-
-    // In Composition API, 'this' will get undefined in setup function
-    // console.log(this); // undefined
-
-    // Template refs
-    const p = ref(null);
-    
-    // 'p.value' will be null since Vue has not returned 'p' yet
-    console.log('p.value : ', p.value); // p.value :  null
-
-    let name = 'Kai';
-    let age = 25;
+    // To make variables reactive to changes, wrap them in 'ref()' method
+    const name = ref('Kai');
+    const age = ref(25);
 
     const handleClick = () => {
-      console.log('p : ', p);
-      console.log('p.value : ', p.value); // <p>My name is Kai and my age is 25</p>
-
-      // Add a class named 'test' to the HTML element (<p></p>)
-      p.value.classList.add('test'); // <p class="test">My name is Kai and my age is 25</p>
-
-      // Replace the content of <p></p> to 'Hello, sports bloggers!'
-      p.value.textContent = 'Hello, sports bloggers!';
+      // To change the variable values, access '.value' property of each variable ('.value' property is only accessed in setup() method)
+      name.value = 'Doe';
+      age.value = 27;
     }
 
-    return { name, age, handleClick, p }
+    return { name, age, handleClick }
   },
 }
 </script>
