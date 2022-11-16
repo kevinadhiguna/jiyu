66c10dffc0ae0eed95868de6fe75b6ce1af72668
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-16 15:52:09 +0700

    refactor: display title and body of post

--- sports-blog/src/views/DetailsView.vue
+++ sports-blog/src/views/DetailsView.vue
@@ -1,5 +1,9 @@
 <template>
-  details
+  <div v-if="error">{{ error }}</div>
+  <div v-if="post" class="post">
+    <h3>{{ post.title }}</h3>
+    <p class="pre">{{ post.body }}</p>
+  </div>
 </template>
 
 <script>
