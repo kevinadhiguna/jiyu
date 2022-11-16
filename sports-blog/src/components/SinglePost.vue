a8d72dfc37326fa16efb94bb6e06141683854f2d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-16 14:52:18 +0700

    refactor: add tags of each post

--- sports-blog/src/components/SinglePost.vue
+++ sports-blog/src/components/SinglePost.vue
@@ -3,6 +3,7 @@
     <h3>{{ post.title }}</h3>
     <!-- <p>{{ post.body }}</p> -->
     <p>{{ snippet }}</p>
+    <span v-for="tag in post.tags" :key="tag">#{{ tag }}</span>
   </div>
 </template>
 
