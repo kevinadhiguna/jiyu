1c27fa615dda7c5b08a78b58bfc5ff94955fda31
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-16 13:02:00 +0700

    refactor: add post list component

---
+++ sports-blog/src/components/PostList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="post-list">
+    <div v-for="post in posts" :key="post.id">
+      <SinglePost :post="post" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from '@vue/runtime-core';
+import SinglePost from './SinglePost.vue';
+
+export default {
+  components: { SinglePost },
+  props: ['posts'],
+  setup(props) {
+    console.log('props.posts : ', props.posts);
+
+    // Lifecycle hooks in Composition API
+    onMounted(() => {
+      console.log('Component mounted!');
+    });
+
+    onUnmounted(() => {
+      console.log('Component unmounted!');
+    });
+
+    onUpdated(() => {
+      console.log('Component updated!');
+    });
+  },
+}
+</script>
+
+<style></style>
