332ce7ce382eb86d5bcf1b4e7bfc914e51b44572
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-16 13:03:19 +0700

    refactor: add single post component

---
+++ sports-blog/src/components/SinglePost.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="post">
+    <h3>{{ post.title }}</h3>
+    <!-- <p>{{ post.body }}</p> -->
+    <p>{{ snippet }}</p>
+  </div>
+</template>
+
+<script>
+import { computed } from '@vue/runtime-core'
+export default {
+  props: ['post'],
+  setup(props) {
+    // If post content exceeds 100 chars, then it will add '...' instead of full content of the post
+    const snippet = computed(() => {
+      if (props.post.body.length > 100) {
+        return props.post.body.substring(0, 100) + '...';
+      } else {
+        return props.post.body;
+      }
+    });
+
+    return { snippet }
+  },
+}
+</script>
+
+<style></style>
