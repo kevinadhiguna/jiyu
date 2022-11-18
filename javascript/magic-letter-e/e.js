d7c51f43c9ce9bf9f069578c3397f149f9a1be67
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-18 12:42:45 +0700

    refactor: add e in javascript to add zeros

---
+++ javascript/magic-letter-e/e.js
@@ -0,0 +1,7 @@
+let number = 2e17;
+// 17 zeros
+console.log('number : ', number); // number :  200000000000000000
+
+let num = -3e16;
+// 16 zeros
+console.log('num : ', num); // num :  -30000000000000000
