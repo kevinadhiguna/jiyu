5094c981e7e5ffa0e0ce7728afd62cd3a653baf0
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-18 12:30:29 +0700

    refactor: change rank on component mounted

--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -13,6 +13,15 @@ export default {
       rank: null,
     }
   },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Ninja fingers';
+    } else if (this.score < 400) {
+      this.rank = 'Rapid reflexes';
+    } else {
+      this.rank = 'Snail pace...';
+    }
+  },
 }
 </script>
 
