9c280b8c12701b0e297af720669d821790824a3a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-20 18:03:05 +0700

    refactor: add sign up form component

--- web-form/src/App.vue
+++ web-form/src/App.vue
@@ -1,10 +1,13 @@
 <template>
+  <SignupForm />
 </template>
 
 <script>
+import SignupForm from './components/SignupForm.vue';
 
 export default {
   name: 'App',
+  components: { SignupForm },
 }
 </script>
 
