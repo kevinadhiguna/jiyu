b6ef3d691aa188b063b5ccb02afae77e876a5d9d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-20 18:02:42 +0700

    refactor: initialize app and styling

---
+++ web-form/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
