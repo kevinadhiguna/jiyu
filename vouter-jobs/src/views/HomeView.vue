cce522d3fdc1b5680e2e28e0e7bea285dff4fd2c
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-21 14:18:26 +0700

    refactor: add homeview component

---
+++ vouter-jobs/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Home page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis officia accusantium dolor ea incidunt, soluta ducimus hic vel ad porro. Deleniti voluptatem aliquid iusto, voluptatum omnis doloribus fugiat odio totam!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis officia accusantium dolor ea incidunt, soluta ducimus hic vel ad porro. Deleniti voluptatem aliquid iusto, voluptatum omnis doloribus fugiat odio totam!</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {},
+}
+</script>
