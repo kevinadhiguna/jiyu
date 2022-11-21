6984f96f4438036359ee8a8829c77a33435e198a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-21 14:18:44 +0700

    refactor: add aboutview component

---
+++ vouter-jobs/src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis veniam, alias beatae quam ratione nostrum ducimus iusto ut quos delectus explicabo incidunt quaerat similique fuga maxime eius distinctio, nihil cupiditate.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis veniam, alias beatae quam ratione nostrum ducimus iusto ut quos delectus explicabo incidunt quaerat similique fuga maxime eius distinctio, nihil cupiditate.</p>
+  </div>
+</template>
