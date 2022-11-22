971e3c576bb9e3149925ea4683296ab658de0f9a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-22 12:59:12 +0700

    refactor: add notfound view

---
+++ vouter-jobs/src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>404</h2>
+  <h3>Oops... page nout found</h3>
+  <router-link :to="{ name: 'home' }">
+    <h4 class="back-btn">Back to home</h4>
+  </router-link>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.back-btn {
+  color: #42b983;
+}
+</style>
