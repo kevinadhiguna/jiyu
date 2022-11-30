c2a73cec980b7492ac89ce24eb520e9b1866ef65
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-11-30 11:21:31 +0700

    refactor: implement && instead of if statement

---
+++ javascript/use-AND-instead-of-IF.js
@@ -0,0 +1,11 @@
+let is2022 = true;
+
+if (is2022) {
+  console.log("FIFA World Cup 2022 is this year! (inside IF statement)");
+}
+
+is2022 && console.log("FIFA World Cup 2022 is this year! (using &&)");
+
+// Output:
+// FIFA World Cup 2022 is this year! (inside IF statement)
+// FIFA World Cup 2022 is this year! (using &&)
