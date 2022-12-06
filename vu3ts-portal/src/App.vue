1c694e9d42132ecca80dbcf76f1dddeb073f61fb
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-12-06 13:22:23 +0700

    refactor: add jobs and iterate each of it

--- vu3ts-portal/src/App.vue
+++ vu3ts-portal/src/App.vue
@@ -1,64 +1,31 @@
 <template>
-  <div class="app">
-    <p>Hi, my name is {{ name }} and I am {{ age }}</p>
-    <div v-if="isStudent">
-      I am currently a student
-    </div>
-    <button @click="changeName('zelda')">change name</button>
-    <button @click="changeAge(17)">change age 17 (number)</button>
-    <button @click="changeAge('21')">change age 21 (string)</button>
+  <div class="app" v-for="job in jobs" :key="job.id">
+    <p>ID : {{ job.id }}</p>
+    <p>Title : {{ job.title }}</p>
+    <p>Location : {{ job.location }}</p>
+    <p>Salary : {{ job.salary }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref, toRefs } from 'vue';
+import { defineComponent, ref } from 'vue';
+import Job from './types/Job';
 
 export default defineComponent({
   name: 'App',
   components: {},
-  // -- Composition API --
   setup() {
-    // -- Reactive --
-    // const state = reactive({
-    //   name: 'Leah',
-    //   age: 25 as number | string
-    // });
+    const jobs = ref<Job[]>([
+      { id: 1, title: 'Footballer', location: 'Brazil', salary: 170000 },
+      { id: 2, title: 'Baseballer', location: 'Japan', salary: 150000 },
+      { id: 3, title: 'Figure Skater', location: 'Russia', salary: 130000 },
+      { id: 4, title: 'Interpreter', location: 'US', salary: 103000 },
+      { id: 5, title: 'Fitness Coach', location: 'London', salary: 105000 },
+    ]);
 
-    // // To change value, one can do:
-    // state.name = 'Selena';
-    // state.age = 19;
-    // // state.age = '22'; // <- also works
-
-    // return { ...toRefs(state) }
-
-    // -- Ref --
-    const name = ref('Leah');
-    const age = ref<number | string>(25);
-
-    // To change value:
-    name.value = 'Zoe';
-    age.value = 26;
-
-    return { name, age }
+    return { jobs }
   },
-  // -- Options API --
-  // data() {
-  //   return {
-  //     name: 'vu3ts',
-  //     age: 15 as number | string, // This means age can be changed to string
-  //     isStudent: true,
-  //   }
-  // },
-  methods: {
-    changeName(name: string): string {
-      this.name = name;
-      return name;
-    },
-    changeAge(age: number | string): number | string { // Allowed Parameter: [1] (age: number | string), [2] (age: number), [3] (age: string)
-      this.age = age;
-      return age;
-    }, 
-  },
+  methods: {},
 });
 </script>
 
