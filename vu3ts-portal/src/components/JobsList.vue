da438fb825edc8e5aef89007441b343b700060a1
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-12-07 10:07:02 +0700

    refactor: pass order as props

--- vu3ts-portal/src/components/JobsList.vue
+++ vu3ts-portal/src/components/JobsList.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="job-list">
+    <p>Ordered by {{ order }}</p>
     <ul>
       <li v-for="job in jobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
@@ -17,12 +18,17 @@
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import Job from '../types/Job';
+import OrderTerm from '../types/OrderTerm';
 
 export default defineComponent({
   props: {
     jobs: {
       required: true,
       type: Array as PropType<Job[]>
+    },
+    order: {
+      required: true,
+      type: String as PropType<OrderTerm>
     },
   },
 })
