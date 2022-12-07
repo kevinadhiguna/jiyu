d16b4cd4593069de06011b11ba1b98829a9d231c
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-12-07 09:51:00 +0700

    style: add styling to the component

--- vu3ts-portal/src/components/JobsList.vue
+++ vu3ts-portal/src/components/JobsList.vue
@@ -29,4 +29,33 @@ export default defineComponent({
 </script>
 
 <style scoped>
+  .job-list {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+  .job-list ul {
+    padding: 0;
+  }
+  .job-list li {
+    list-style-type: none;
+    background: #fff;
+    padding: 16px;
+    margin: 16px 0;
+    border-radius: 4px;
+  }
+  .job-list h2 {
+    margin: 0 0 10px;
+    text-transform: capitalize;
+  }
+  .salary {
+    display: flex;
+  }
+  .salary img {
+    width: 30px;
+  }
+  .salary p {
+    color: #17bf66;
+    font-weight: bold;
+    margin: 10px 4px;
+  }
 </style>
