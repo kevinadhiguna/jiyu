274d62769103bbc3044446287fe4a2fc84fec7d9
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-12-07 12:40:56 +0700

    refactor: add title

--- vu3ts-portal/src/App.vue
+++ vu3ts-portal/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app">
     <header>
+      <div class="title">
+        <img src="./assets/images/heart.svg" alt="Vu3ts site logo">
+        <h1>Vu3ts Portal</h1>
+      </div>
       <div class="order">
         <button @click="handleClick('title')">order by title</button>
         <button @click="handleClick('salary')">order by salary</button>
@@ -57,5 +61,16 @@ export default defineComponent({
     border-radius: 4px;
     cursor: pointer;
     font-weight: bold;
+  }
+  header .title {
+    display: flex;
+    justify-content: center;
+  }
+  header img {
+    width: 60px;
+    margin-right: 20px;
+  }
+  header h1 {
+    font-size: 3em;
   }
 </style>
