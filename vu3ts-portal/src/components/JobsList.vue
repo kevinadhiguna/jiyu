b2a90b6454d6f196b3742848b47fbdeb7fa3d51c
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-12-07 09:50:10 +0700

    refactor: add jobslist component and pass props
    also this takes advantages of props validation by putting PropType

---
+++ vu3ts-portal/src/components/JobsList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import Job from '../types/Job';
+
+export default defineComponent({
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>
+    },
+  },
+})
+</script>
+
+<style scoped>
+</style>
