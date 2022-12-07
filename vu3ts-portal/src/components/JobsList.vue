9a32da3153a9644c875f95e818389cf8c0ee93b9
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-12-07 09:50:36 +0700

    refactor: display job information

--- vu3ts-portal/src/components/JobsList.vue
+++ vu3ts-portal/src/components/JobsList.vue
@@ -1,5 +1,17 @@
 <template>
-  <div></div>
+  <div class="job-list">
+    <ul>
+      <li v-for="job in jobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <p>{{ job.salary }} USD</p>
+        </div>
+        <div class="description">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus vitae culpa repellat aliquid cum mollitia nemo dignissimos, magni fuga perferendis.</p>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
