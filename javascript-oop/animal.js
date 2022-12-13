9c52c39589284e776740e5f22afb7402c3402999
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-12-13 09:28:32 +0700

    refactor: add instanceof keyword and alternative of if statement
    one-liner insisde if

--- javascript-oop/animal.js
+++ javascript-oop/animal.js
@@ -8,3 +8,13 @@ class Animal {
 
 const panda = new Animal();
 panda.sleep(); // zzz...
+
+// Check if 'panda' is an object created from the 'Animal' class
+const question = panda instanceof Animal;
+
+// A quick alternative of :
+// if (question) {
+//   // Do something here
+//   console.log("panda is an instance of Animal class")
+// }
+question && console.log("panda is an instance of Animal class");
--- javascript-oop/method.js
+++ javascript-oop/method.js
@@ -11,3 +11,13 @@ class Example {
 const stuff = new Example();
 stuff.somePublicMethod(); // You called a private method, didn't you ?
 // stuff.#somePrivateMethod(); // Error : Property '#somePrivateMethod' is not accessible outside class 'Example' because it has a private identifier.
+
+// Check if 'stuff' is an object created from the 'Example' class
+const question = stuff instanceof Example;
+
+// A quick alternative of :
+// if (question) {
+//   // Do something here
+//   console.log("stuff is an instance of Example class")
+// }
+question && console.log("stuff is an instance of Example class");
