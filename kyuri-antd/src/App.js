004b9e778130f4dd6ca0eec10311c96246569e47
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-01-13 10:07:19 +0700

    style: add color to the qrcode

--- kyuri-antd/src/App.js
+++ kyuri-antd/src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import { QRCode } from 'antd';
+import { QRCode, theme } from 'antd';
+const { useToken } = theme;
 
 function App() {
   const link = 'https://github.com/kevinadhiguna';
   const icon = 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png';
+  const { token } = useToken();
 
   return (
     <div className="App">
       <QRCode
         value={link}
         icon={icon}
+        color={token.colorInfoText}
       />
     </div>
   );
