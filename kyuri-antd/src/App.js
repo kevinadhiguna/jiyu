0d7ee1090dced7723c33b973d193e9285d19c992
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-01-15 10:05:08 +0700

    refactor: add qrcode components

--- kyuri-antd/src/App.js
+++ kyuri-antd/src/App.js
@@ -1,43 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import { QRCode, theme, Button } from 'antd';
-const { useToken } = theme;
+
+import SingleQRcode from './components/singleQRcode';
+import PopoverQRcode from './components/popoverQRcode';
 
 function App() {
-  const link = 'https://github.com/kevinadhiguna';
-  const icon = 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png';
-  const { token } = useToken();
-  const qrcodeName = 'qrcode-github.png';
-
-  // Issue : Still unsuccesful downloading the QR code
-  function downloadQRcode() {
-    const canvas = document.getElementById('qrcode-container')?.querySelector('canvas');
-    
-    if (canvas) {
-      const url = canvas.toDataURL();
-      const a = document.createElement('a');
-      a.download = qrcodeName;
-      a.href = url;
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-    }
-  }
-
   return (
-    <div className="App" id='qrcode-container'>
-      <QRCode
-        value={link}
-        icon={icon}
-        color={token.colorInfoText}
-        style={{
-          marginBottom: 16,
-          backgroundColor: token.colorBgLayout
-        }}
-      />
-      <Button type='primary' onClick={downloadQRcode}>
-        Download
-      </Button>
+    <div className="App">
+      <SingleQRcode />
+      <PopoverQRcode />
     </div>
   );
 }
