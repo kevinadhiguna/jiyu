695d65a1a71a7f3cc2ee93e665449c6b01dbd2e6
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-01-16 13:20:51 +0700

    refactor: add person list component

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Greet } from './components/Greet';
 import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
   const personName = {
@@ -8,10 +9,17 @@ function App() {
     last: "Toichi"
   };
 
+  const nameList = [
+    { first: "Eiryu", last: "Jyubei" },
+    { first: "Noel", last: "Neuer" },
+    { first: "Etosi", last: "Sae" },
+  ];
+
   return (
     <div className="App">
       <Greet name='Alvaro' unreadMessages={5} isLoggedIn={false} />
       <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
