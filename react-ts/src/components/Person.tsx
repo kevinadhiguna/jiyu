440c2c66fde1d3e5319653254ca62fc9ce5af593
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-01-16 13:00:43 +0700

    refactor: add person component and its props

---
+++ react-ts/src/components/Person.tsx
@@ -0,0 +1,16 @@
+// Learn object prop example
+
+type PersonProps = {
+  name: {
+    first: string;
+    last: string;
+  }
+};
+
+export const Person = ({ name }: PersonProps) => {
+  return(
+    <div>
+      <p>{name.first} {name.last}</p>
+    </div>
+  );
+}
