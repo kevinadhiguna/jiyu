de70893cc9ad22e667a300fa4094b35ef51a4393
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-01-16 13:01:08 +0700

    refactor: pass person props value and import perosn component

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
 
 function App() {
+  const personName = {
+    first: "Isagi",
+    last: "Toichi"
+  };
+
   return (
     <div className="App">
       <Greet name='Alvaro' unreadMessages={5} isLoggedIn={false} />
+      <Person name={personName} />
     </div>
   );
 }
