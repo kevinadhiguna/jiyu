fc3ef404eabeff87e256c867a120b352726a1966
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-01-16 13:31:27 +0700

    refactor: include status component

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Greet } from './components/Greet';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
 
 function App() {
   const personName = {
@@ -20,6 +21,7 @@ function App() {
       <Greet name='Alvaro' unreadMessages={5} isLoggedIn={false} />
       <Person name={personName} />
       <PersonList names={nameList} />
+      <Status status='success' />
     </div>
   );
 }
