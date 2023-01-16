7f8842fe8658c457d8d99f9735f30a7835538b51
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-01-16 13:20:15 +0700

    refactor: add personlist component and its props in an array of object

---
+++ react-ts/src/components/PersonList.tsx
@@ -0,0 +1,31 @@
+// Learn an array of object prop example
+
+type PersonListProps = {
+  names: {
+    first: string;
+    last: string;
+  }[]
+};
+
+export const PersonList = ({ names }: PersonListProps) => {
+  return(
+    <div>
+      {/* Hardcode (BAD) */}
+      {/* 
+        <h4>Barow Shohei</h4>
+        <h4>Etoshi Rin</h4>
+        <h4>Nagi Eishiro</h4>
+        <h4>Azira Meguru</h4> 
+      */}
+
+      {/* Dynamic (GOOD) */}
+      {names.map(name => {
+        return(
+          <p key={name.first}>
+            {name.first} {name.last}
+          </p>
+        );
+      })}
+    </div>
+  );
+}
