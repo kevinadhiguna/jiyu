ef549864c5dd85398c88430fa7956a2d22f07a79
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-01-17 16:28:28 +0700

    refactor: implement useReducer hook

---
+++ react-ts/src/state/Counter.tsx
@@ -0,0 +1,35 @@
+import { useReducer } from 'react';
+import { CounterState } from '../types/CounterState.type';
+import { CounterAction } from '../types/CounterAction.type';
+
+const initialState = { count: 0 };
+
+function reducer(state: CounterState, action: CounterAction) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + action.payload };      
+    case 'decrement':
+      return { count: state.count - action.payload };
+    case 'reset':
+      return initialState;
+    default:
+      return state;
+  }
+}
+
+export const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return(
+    <>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: 'increment', payload: 10 })}>
+        Increment 10
+      </button>
+      <button onClick={() => dispatch({ type: 'decrement', payload: 10 })}>
+        Decrement 10
+      </button>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+    </>
+  );
+}
