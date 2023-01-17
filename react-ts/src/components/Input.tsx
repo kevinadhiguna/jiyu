8c36fea3770a74b9519acd67fe3ad503f3b2b92b
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-01-17 08:43:39 +0700

    refactor: add input component and its props

---
+++ react-ts/src/components/Input.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type InputProps = {
+  value: string;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+};
+
+export const Input = ({ value, handleChange }: InputProps) => {
+  return(
+    <input type="text" value={value} onChange={handleChange} />
+  );
+}
