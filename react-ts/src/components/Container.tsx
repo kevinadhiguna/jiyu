b3a2400679843469d11f27ac1197cc7f9c8d0975
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-01-17 08:45:02 +0700

    refactor: add container component and style or css props

---
+++ react-ts/src/components/Container.tsx
@@ -0,0 +1,9 @@
+import { ContainerProps } from '../types/Container.type';
+
+export const Container = ({ styles }: ContainerProps) => {
+  return(
+    <div style={styles}>
+      Text goes here
+    </div>
+  );
+}
