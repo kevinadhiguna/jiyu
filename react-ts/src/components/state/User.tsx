d289cd9dcc43d70bbd354cc514c1e163d7690a8e
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-01-18 08:21:10 +0700

    chore: move user component to components directory

--- react-ts/src/components/state/User.tsx
+++ react-ts/src/components/state/User.tsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
-import { AuthUser } from '../types/AuthUser.type';
+import { useState, useContext } from 'react';
+import { AuthUser } from '../../types/AuthUser.type';
+import { UserContext } from '../context/UserContext';
 
 export const User = () => {
   const [user, setUser] = useState<AuthUser | null>(null);
+  const userContext = useContext(UserContext);
 
   function handleLogin() {
     setUser({
@@ -17,11 +19,13 @@ export const User = () => {
 
   return(
     <div>
+      <p>User component - state</p>
       <button onClick={handleLogin}>Login</button>
       <button onClick={handleLogout}>Logout</button>
+
       {/* optional chaining is after the user object (user.name) since user is possibly null */}
       <p>User name is {user?.name}</p>
-      <p>User email is {user?.email}</p>
+      <p>User email is {user?.email}</p> 
     </div>
   );
 }
