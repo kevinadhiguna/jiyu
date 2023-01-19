806812b2dee732a574bdf2f0579e231d4c3d1619
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-01-19 13:33:28 +0700

    refactor: round, ceil, and floor numbers using math library

---
+++ javascript/Math/math.js
@@ -0,0 +1,7 @@
+console.log('Math.round(14.22273448) :', Math.round(14.22273448)); // 14
+console.log('Math.floor(14.22273448) :', Math.floor(14.22273448)); // 14
+console.log('Math.ceil(14.22273448) :', Math.ceil(14.22273448)); // 15
+
+console.log('Math.round(14.82273448) : ', Math.round(14.82273448)); // 15
+console.log('Math.floor(14.82273448) : ', Math.floor(14.82273448)); // 14
+console.log('Math.ceil(14.82273448) : ', Math.ceil(14.82273448)); // 15
