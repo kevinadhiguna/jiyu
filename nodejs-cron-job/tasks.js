1f28946b6da71bbde519847757039b98b85aa169
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-01-26 14:08:57 +0700

    refactor: add a task that runs every second

---
+++ nodejs-cron-job/tasks.js
@@ -0,0 +1,9 @@
+const tasks = {
+  everySecond: function () {
+    console.log('==========');
+    console.log('A cron job that runs every second');
+    console.log('==========');
+  },
+};
+
+module.exports = tasks;
