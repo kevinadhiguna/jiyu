803d2563f64cc9fa50f573a94a167eba0be9aa04
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-01-29 10:43:36 +0700

    chore: add a cron job interval syntax

--- nodejs-cron-job/app.js
+++ nodejs-cron-job/app.js
@@ -7,6 +7,20 @@ const tasks = require('./tasks');
 
 const app = express();
 
+/* A cron job interval:
+
+ "* * * * * *"
+  | | | | | |
+  | | | | | |
+  | | | | | day of week -> 0 - 7  or names, 0 and 7 refer to sunday
+  | | | | month         -> 1 - 12 or names
+  | | | day of month    -> 1 - 31
+  | | hour              -> 0 - 23
+  | minute              -> 0 - 59
+  second(optional)      -> 0 - 59
+
+*/
+
 cron.addJob('Run a task every second', '* * * * *', tasks.everySecond);
 
 const port = config.port || 5050;
