dca10e655657b6e6f6ea08dc6f312f9587ca6c5b
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-01-30 16:40:17 +0700

    refactor: add a function that is executed every 20 minutes

--- nodejs-cron-job/tasks.js
+++ nodejs-cron-job/tasks.js
@@ -10,6 +10,12 @@ const tasks = {
     console.log('A cron job that runs on 3th and 17th at 23:59 every month');
     console.log('==========');
   },
+
+  every20minutes: function () {
+    console.log('==========');
+    console.log('A cron job that runs every 20 minutes');
+    console.log('==========');
+  },
 };
 
 module.exports = tasks;
