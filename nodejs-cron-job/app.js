e0a66e89185867e14c6889482631a78848da62eb
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-02-09 10:51:33 +0700

    refactor: execute a task at particular period

--- nodejs-cron-job/app.js
+++ nodejs-cron-job/app.js
@@ -26,6 +26,9 @@ cron.addJob('Run a task every second', '* * * * *', tasks.everySecond);
 // Use single integer values to populate a chron expression
 cron.addJob('Run a task on 3rd and 17th at 23:59 every month', '59 23 3,17 * *', tasks.certainDates);
 
+// Use a range of values to populate a chron expression
+cron.addJob('Run a task at :15 from 1pm through 3pm on the 3rd', '15 13-15 3 * * *', tasks.every_minute15th_from1pmThrough3pmOnThe3rd);
+
 // Use multiple values to populate a chron expression
 cron.addJob('Run a task at :11, :33, and :55 during 5am', '11,33,55 5 * * *', tasks.everydayAt_511_533_555AM);
 
