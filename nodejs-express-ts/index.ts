c7dce4b886cca5c3e27d9bb0353f75956fcd580f
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-02-10 16:44:44 +0700

    refactor: add a sample endpoint

--- nodejs-express-ts/index.ts
+++ nodejs-express-ts/index.ts
@@ -4,6 +4,13 @@ import { config } from 'dotenv';
 const app: Express = express();
 config();
 
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    status: 200,
+    message: 'Welcome to API powered by Express.js and TypeScript!',
+  });
+});
+
 // To-do: port shouldn't be a string(?)
 const port: string | number = process.env.PORT || 3303;
 
