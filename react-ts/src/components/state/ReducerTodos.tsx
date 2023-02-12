53b243820d08d44c4432975f16f0f3cddc00959b
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-02-12 12:18:32 +0700

    refactor: add intial todos

---
+++ react-ts/src/components/state/ReducerTodos.tsx
@@ -0,0 +1,14 @@
+import { ITodos } from '../types/ReducerTodos.type';
+
+const initialTodos: ITodos = [
+  {
+    id: 1,
+    title: "Apply to a scholarship program",
+    complete: false,
+  },
+  {
+    id: 2,
+    title: "Get a letter of acceptance in the XYZ college",
+    complete: false,
+  },
+];
