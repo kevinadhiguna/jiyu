30a6f060d62d994cd6c6f70f5cc430b36edb72b5
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-02-13 16:11:05 +0700

    refactor: create reducer input component

--- react-ts/src/components/state/ReducerInput.tsx
+++ react-ts/src/components/state/ReducerInput.tsx
@@ -1,3 +1,4 @@
+import React, { useReducer } from 'react';
 import { IPlayer, IAction } from '../interfaces/ReducerInput.interface';
 
 const player: IPlayer = {
@@ -23,4 +24,28 @@ function reducer(state: IPlayer, action: IAction) {
       throw Error(`Unknown action type : ${action.type}`);
     }
   }
+}
+
+export function ReducerInput() {
+  const [state, dispatch] = useReducer(reducer, player);
+
+  function handleNameChange(e: React.ChangeEvent<HTMLInputElement>) {
+    dispatch({ type: "change_name", nextName: e.target.value });
+  }
+
+  function handleIncrementRank() {
+    dispatch({ type: "increment_rank", nextName: state.name });
+  }
+
+  return(
+    <>
+      <input type="text" value={state.name} onChange={handleNameChange} />
+      <button onClick={handleIncrementRank}>+ rank</button>
+      { 
+        state.name && state.rank 
+          ? <p>Hi {state.name}, your rank is {state.rank}</p> 
+          : <p>Welcome guest!</p>
+      }
+    </>
+  );
 }
