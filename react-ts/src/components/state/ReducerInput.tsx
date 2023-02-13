f32f85a65cc0fd62cd64dab9b05bee8511244b8f
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-02-13 16:10:27 +0700

    refactor: add reducer function

--- react-ts/src/components/state/ReducerInput.tsx
+++ react-ts/src/components/state/ReducerInput.tsx
@@ -1,6 +1,26 @@
-import { IPlayer } from '../interfaces/ReducerInput.interface';
+import { IPlayer, IAction } from '../interfaces/ReducerInput.interface';
 
 const player: IPlayer = {
   name: "Kai Doe",
   rank: 1,
 };
+
+function reducer(state: IPlayer, action: IAction) {
+  switch (action.type) {
+    case "increment_rank": {
+      return {
+        name: state.name,
+        rank: state.rank + 1,
+      };
+    }
+    case "change_name":{
+      return {
+        name: action.nextName,
+        rank: state.rank,
+      };
+    }
+    default: {
+      throw Error(`Unknown action type : ${action.type}`);
+    }
+  }
+}
