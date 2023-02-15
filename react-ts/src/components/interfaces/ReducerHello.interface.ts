79a1194a155fe1dec555c5475e1658c943b029e8
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-02-15 18:35:50 +0700

    refactor: add a workaround for action interface

--- react-ts/src/components/interfaces/ReducerHello.interface.ts
+++ react-ts/src/components/interfaces/ReducerHello.interface.ts
@@ -10,3 +10,13 @@ interface ISetContextAction {
 interface IClearContextAction {
   type: 'CLEAR_CONTEXT';
 }
+
+// To-do: How to merge interfaces that share a same property? (in this case, 'type' property)
+// interface IAction extends ISetContextAction, IClearContextAction {}
+
+// -- Some temporary workarounds --
+// (1)
+export interface IAction {
+  type: 'SET_CONTEXT' | 'CLEAR_CONTEXT';
+  payload?: string;
+}
