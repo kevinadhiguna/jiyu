a055337ee27c1d3753291a1795b937fe82c35757
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-02-18 09:57:53 +0700

    refactor: exclude unnecessary workarounds

--- react-ts/src/components/interfaces/ReducerHello.interface.ts
+++ react-ts/src/components/interfaces/ReducerHello.interface.ts
@@ -15,19 +15,20 @@ interface IClearContextAction {
 // interface IAction extends ISetContextAction, IClearContextAction {}
 
 // -- Some temporary workarounds --
-// (1)
-export interface IAction {
-  type: 'SET_CONTEXT' | 'CLEAR_CONTEXT';
-  payload?: string;
-}
+// (1) (This could be abysmal as payload is set to optional even though type is 'SET_CONTEXT'. Better clear this off!)
+// export interface IAction {
+//   type: 'SET_CONTEXT' | 'CLEAR_CONTEXT';
+//   payload?: string;
+// }
 
-// (2)
-type ActionType = 
+// (2) (These are possibly the way to go as it meets requirements)
+export type ActionType = 
   | { type: 'SET_CONTEXT', payload: string }
   | { type: 'CLEAR_CONTEXT' };
+export type ActionType2 = ISetContextAction | IClearContextAction;
 
-// (3) (But this forces one to include 'payload' property)
+// (3) (But this forces one to include 'payload' property even though type is 'CLEAR_CONTEXT'. Not really the best option)
 // Reference: https://stackoverflow.com/a/75458439/14418391
-export interface IAction2 extends Omit<ISetContextAction, 'type'>, Omit<IClearContextAction, 'type'> {
-  type: 'SET_CONTEXT' | 'CLEAR_CONTEXT';
-}
+// export interface IAction2 extends Omit<ISetContextAction, 'type'>, Omit<IClearContextAction, 'type'> {
+//   type: 'SET_CONTEXT' | 'CLEAR_CONTEXT';
+// }
