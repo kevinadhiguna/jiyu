cc59f3f6545a8596696baa0681e35bafead4d7e4
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-02-19 22:56:17 +0700

    refactor: handle name and address change

--- react-ts/src/components/state/StateEffectForm.tsx
+++ react-ts/src/components/state/StateEffectForm.tsx
@@ -1,15 +1,23 @@
-import { useState } from 'react';
+import { useState, ChangeEvent } from 'react';
 import { Form, Input, Button } from 'antd';
 
 export function StateEffectForm() {
   const [name, setName] = useState<string>('');
   const [address, setAddress] = useState<string>('');
 
+  function handleNameChange(e: ChangeEvent<HTMLInputElement>) {
+    setName(e.target.value);
+  }
+
+  function handleAddressChange(e: ChangeEvent<HTMLInputElement>) {
+    setAddress(e.target.value);
+  }
+
   return(
     <Form>
       <Form.Item>
-        <Input placeholder='Name'>Name</Input>
-        <Input placeholder='Address'>Address</Input>
+        <Input placeholder='Name' onChange={handleNameChange}>Name</Input>
+        <Input placeholder='Address' onChange={handleAddressChange}>Address</Input>
         <Button htmlType='submit' type='primary'>Submit</Button>
       </Form.Item>
     </Form>
