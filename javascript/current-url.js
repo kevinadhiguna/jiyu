7207b4ff41ad60301574a51fa8ca1e6d3a5aacba
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-03-02 07:40:22 +0700

    refactor: retrieve current url

---
+++ javascript/current-url.js
@@ -0,0 +1,9 @@
+// Please only execute this in a browser which has 'window' API
+function getCurrentURL() {
+  return window.location.href;
+}
+
+console.log('Current URL :', getCurrentURL());
+
+// Reference:
+// - https://morioh.com/p/a76bc7d72226
