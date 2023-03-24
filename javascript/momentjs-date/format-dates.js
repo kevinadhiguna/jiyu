5e4b9206170538c52785e38e38996e2566513829
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-03-24 21:33:38 +0700

    refactor: add new date format

--- javascript/momentjs-date/format-dates.js
+++ javascript/momentjs-date/format-dates.js
@@ -1,20 +1,7 @@
 const moment = require("moment");
 
-console.log("moment().format() :", moment().format());
-// OUTPUT :
-// moment().format() : 2022-03-02T02:46:33+00:00
-
-console.log(
-  "moment().format('MMMM Do YYYY') :",
-  moment().format("MMMM Do YYYY")
-);
-// OUTPUT :
-// moment().format('MMMM Do YYYY') : March 2nd 2022
-
-console.log("moment().format('MMM Do YYY') :", moment().format("MMM Do YY"));
-// OUTPUT :
-// moment().format('MMM Do YYY') : Mar 2nd 22
-
-console.log("moment().format('dddd') :", moment().format("dddd"));
-// OUTPUT :
-// moment().format('dddd') : Wednesday
+console.log("moment().format() :", moment().format()); // 2022-03-02T02:46:33+00:00
+console.log("moment().format('MMMM Do YYYY') :", moment().format("MMMM Do YYYY")); // March 2nd 2022
+console.log("moment().format('MMM Do YYY') :", moment().format("MMM Do YY")); // Mar 2nd 22
+console.log("moment().format('dddd') :", moment().format("dddd")); // Wednesday
+console.log("moment().format('DD/MM/YYYY') :", moment().format('DD/MM/YYYY')); // 02/03/2022
