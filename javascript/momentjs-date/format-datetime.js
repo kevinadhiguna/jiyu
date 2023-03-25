23274b12babad23798857a451e8a88f347e956af
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-03-25 21:13:28 +0700

    refactor: format datetime

---
+++ javascript/momentjs-date/format-datetime.js
@@ -0,0 +1,5 @@
+const moment = require("moment");
+
+console.log("moment().format('h:mma') :", moment().format('h:mma')); // 1:17pm
+
+// Reference: https://thecodebarbarian.com/formatting-javascript-dates-with-moment-js.html
