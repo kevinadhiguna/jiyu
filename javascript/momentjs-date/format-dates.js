916bc7c1bf574ce81b01067f2d23d13b6efd308f
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-03-28 09:50:18 +0700

    refactor: format date correctly and incorrectly using momentjs

--- javascript/momentjs-date/format-dates.js
+++ javascript/momentjs-date/format-dates.js
@@ -5,3 +5,11 @@ console.log("moment().format('MMMM Do YYYY') :", moment().format("MMMM Do YYYY")
 console.log("moment().format('MMM Do YYY') :", moment().format("MMM Do YY")); // Mar 2nd 22
 console.log("moment().format('dddd') :", moment().format("dddd")); // Wednesday
 console.log("moment().format('DD/MM/YYYY') :", moment().format('DD/MM/YYYY')); // 02/03/2022
+
+const ddMMyyyy_date_1 = "21/03/2022";
+
+// ❌ Invalid date
+console.log("moment(new Date(ddMMyyyy_date_1)).format('DD/MM') :", moment(new Date(ddMMyyyy_date_1)).format('DD/MM')); // <- Invalid date
+console.log("moment(new Date(ddMMyyyy_date_1)).format('DD/MM') :", moment(new Date(ddMMyyyy_date_1), 'DD/MM/YYYY').format('DD/MM')); // <- Invalid date
+// ✅ Correct way to format date (learn more: https://stackoverflow.com/a/35863673)
+console.log("moment(ddMMyyyy_date_1, 'DD/MM/YYYY').format('DD/MM') :", moment(ddMMyyyy_date_1, 'DD/MM/YYYY').format('DD/MM')); // 21/03
