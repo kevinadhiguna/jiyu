cdf31f8dcaed6d4b0961e3285bd989fd6c02a919
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-03-29 13:09:17 +0700

    refactor: add navbar

--- nuxt3r-merch/pages/index.vue
+++ nuxt3r-merch/pages/index.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Nuxt3r Merch app</NuxtLink>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">About</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/products">Products</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
     <h2>Home</h2>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus aliquam maiores illum autem quo beatae vitae suscipit rem aspernatur? Recusandae?</p>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate ea aliquam aliquid. Alias fugiat eligendi optio illum id nesciunt laboriosam.</p>
