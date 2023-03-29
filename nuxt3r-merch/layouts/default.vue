def3d61e428121aca93f26de163008da05818fed
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-03-29 13:09:31 +0700

    refactor: add default layout

---
+++ nuxt3r-merch/layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Nuxt3r Merch app</NuxtLink>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">About</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/products">Products</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <!-- Display page content -->
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .router-link-exact-active {
+    color: #12b488;
+  }
+</style>
