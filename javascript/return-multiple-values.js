c77de09c5f83b46e4b5e80e2c77537b15bbf77fe
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-03-29 09:28:59 +0700

    refactor: return multiple values from a function

--- javascript/return-multiple-values.js
+++ javascript/return-multiple-values.js
@@ -7,6 +7,16 @@ function getEmail() {
 }
 
 let [email1, email2] = getEmail();
-console.log('[email1, email2] :', [email1, email2]);
+console.log('[ email1, email2 ] :', [ email1, email2 ]);
+
+function getAirplanes() {
+  let airplane1 = 'Emirates';
+  let airplane2 = 'All Nippon Airlanes';
+
+  return { airplane1, airplane2 };
+}
+
+let { airplane1, airplane2 } = getAirplanes();
+console.log('{ airplane1, airplane2 } :', { airplane1, airplane2 });
 
 // reference: https://www.javascripttutorial.net/javascript-return-multiple-values/
