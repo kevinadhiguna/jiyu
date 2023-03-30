ad6a603ca7c6f1123dc8f09c3c070da1520a5ede
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-03-30 09:09:19 +0700

    refactor: add products layout

---
+++ nuxt3r-merch/layouts/products.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/products">Nuxt3r Merch app - Products</NuxtLink>
+      </nav>
+    </header>
+
+    <!-- Display page content -->
+    <div>
+      <slot />
+    </div>
+
+    <footer>
+      <ul>
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about">About</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/products">Products</NuxtLink>
+        </li>
+      </ul>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+  .router-link-exact-active {
+    color: #12b488;
+  }
+</style>
