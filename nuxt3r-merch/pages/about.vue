e97f9ab79371ed66221fdd93263716d06d38c916
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-03-31 09:02:30 +0700

    refactor: add about page

---
+++ nuxt3r-merch/pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus aliquam maiores illum autem quo beatae vitae suscipit rem aspernatur? Recusandae?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate ea aliquam aliquid. Alias fugiat eligendi optio illum id nesciunt laboriosam.</p>
+  </div>
+</template>
+
+<script setup>
+  
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+</style>
