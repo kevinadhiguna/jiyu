04e15e09db15f5bd1d317025f59bd9ae6a19ec3d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-04-01 12:53:50 +0700

    feat: add an initial page to products route

---
+++ nuxt3r-merch/pages/products/[id].vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>Product ID: {{ id }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat repellendus dicta deserunt? Neque eaque asperiores temporibus, hic animi sit eius!</p>
+  </div>
+</template>
+
+<script setup>
+  // The param inside the '{}' should be equal with the filename ('[].vue')
+  const { id } = useRoute().params;
+
+  definePageMeta({
+    layout: 'products'
+  });
+</script>
+
+<style scoped>
+
+</style>
---
+++ nuxt3r-merch/pages/products/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Products</p>
+  </div>
+</template>
+
+<script setup>
+  definePageMeta({
+    layout: 'products'
+  });
+</script>
+
+<style scoped>
+
+</style>
