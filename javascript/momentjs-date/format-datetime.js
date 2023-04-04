2aed0deff4ee8445109e9739f6f4f2e9d690da66
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-04-04 12:50:17 +0700

    refactor: compare date formatting using momentjs and native

--- javascript/momentjs-date/format-datetime.js
+++ javascript/momentjs-date/format-datetime.js
@@ -1,5 +1,44 @@
 const moment = require("moment");
 
-console.log("moment().format('h:mma') :", moment().format('h:mma')); // 1:17pm
+console.log("moment().format('h:mma') :", moment().format("h:mma")); // 1:17pm
 
-// Reference: https://thecodebarbarian.com/formatting-javascript-dates-with-moment-js.html
+// -- Comparing Moment.js & Native --
+
+// (1)
+const moment1 = moment().format("dddd, MMMM Do YYYY, h:mm:ss A");
+console.log(`moment().format("dddd, MMMM Do YYYY, h:mm:ss A") :`, moment1); // Wednesday, January 11th 2023, 01:17:16 PM
+
+const native1 = new Intl.DateTimeFormat("en-US", {
+  dateStyle: "full",
+  timeStyle: "medium",
+}).format(new Date());
+console.log(
+  `new Intl.DateTimeFormat("en-US", {
+  dateStyle: "full",
+  timeStyle: "medium",
+}).format(new Date()) :`,
+  native1
+); // Wednesday, January 11th 2023, 01:17:16 PM
+
+// (2)
+let moment2 = moment().format("ddd, hA");
+console.log(`moment().format("ddd, hA") :`, moment2); // Wed, 01 PM
+
+let native2 = new Intl.DateTimeFormat("en-US", {
+  weekday: "short",
+  hour: "numeric",
+}).format(new Date());
+console.log(
+  `new Intl.DateTimeFormat("en-US", {
+  weekday: "short",
+  hour: "numeric",
+}).format(new Date()) :`,
+  native2
+); // Wed, 01 PM
+
+/**
+ * References :
+ *
+ * - https://thecodebarbarian.com/formatting-javascript-dates-with-moment-js.html
+ * - https://github.com/you-dont-need/You-Dont-Need-Momentjs#format
+ */
