3d8af621f201696210a2f6ac58b9a07b1b267a23
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-04-05 10:02:27 +0700

    refactor: find hypotenuse

--- javascript/Math/math.js
+++ javascript/Math/math.js
@@ -1,7 +1,9 @@
-console.log('Math.round(14.22273448) :', Math.round(14.22273448)); // 14
-console.log('Math.floor(14.22273448) :', Math.floor(14.22273448)); // 14
-console.log('Math.ceil(14.22273448) :', Math.ceil(14.22273448)); // 15
+console.log("Math.round(14.22273448) :", Math.round(14.22273448)); // 14
+console.log("Math.floor(14.22273448) :", Math.floor(14.22273448)); // 14
+console.log("Math.ceil(14.22273448) :", Math.ceil(14.22273448)); // 15
 
-console.log('Math.round(14.82273448) : ', Math.round(14.82273448)); // 15
-console.log('Math.floor(14.82273448) : ', Math.floor(14.82273448)); // 14
-console.log('Math.ceil(14.82273448) : ', Math.ceil(14.82273448)); // 15
+console.log("Math.round(14.82273448) : ", Math.round(14.82273448)); // 15
+console.log("Math.floor(14.82273448) : ", Math.floor(14.82273448)); // 14
+console.log("Math.ceil(14.82273448) : ", Math.ceil(14.82273448)); // 15
+
+console.log("Math.hypot(3, 4) :", Math.hypot(3, 4)); // 5
