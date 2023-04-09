b83ff49c5924ebce81665d27000545b90865a2eb
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-04-09 12:54:00 +0700

    chore: change file name and add some essential note

--- javascript/console/console.js
+++
@@ -1,4 +0,0 @@
-console.table([
-  { AL: "Angels", NL: "Dodgers" },
-  { AL: "Yankees", NL: "Mets", NPB: "Tokyo Yakult Swallows" },
-]);
---
+++ javascript/console/table.js
@@ -0,0 +1,12 @@
+console.table([
+  { AL: "Angels", NL: "Dodgers" },
+  { AL: "Yankees", NL: "Mets", NPB: "Tokyo Yakult Swallows" },
+]);
+
+// -- NOTES --
+// const teams = [
+//   { AL: "Angels", NL: "Dodgers" },
+//   { AL: "Yankees", NL: "Mets", NPB: "Tokyo Yakult Swallows" },
+// ];
+
+// console.table("teams :", teams); // <- DO NOT WORK!
