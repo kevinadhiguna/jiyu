83a92c0a40f9209010ac1aef61d458ccf0bad2ec
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-04-13 08:11:47 +0700

    refactor: display absolute values

--- javascript/Math/math.js
+++ javascript/Math/math.js
@@ -6,4 +6,7 @@ console.log("Math.round(14.82273448) : ", Math.round(14.82273448)); // 15
 console.log("Math.floor(14.82273448) : ", Math.floor(14.82273448)); // 14
 console.log("Math.ceil(14.82273448) : ", Math.ceil(14.82273448)); // 15
 
+console.log("Math.abs(-9189) :", Math.abs(-9189)); // 9189
+console.log("Math.abs(5832949) :", Math.abs(5832949)); // 5832949
+
 console.log("Math.hypot(3, 4) :", Math.hypot(3, 4)); // 5
