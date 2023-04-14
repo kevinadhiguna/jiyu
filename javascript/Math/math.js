a388714126231f7235568e84229dbb387e8a138d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-04-14 14:45:11 +0700

    refactor: prompt random value between 0 and 1

--- javascript/Math/math.js
+++ javascript/Math/math.js
@@ -9,4 +9,6 @@ console.log("Math.ceil(14.82273448) : ", Math.ceil(14.82273448)); // 15
 console.log("Math.abs(-9189) :", Math.abs(-9189)); // 9189
 console.log("Math.abs(5832949) :", Math.abs(5832949)); // 5832949
 
+console.log("Math.random() :", Math.random()); // Any random floating numbers between 0 and 1 (example: 0.5281335966084748)
+
 console.log("Math.hypot(3, 4) :", Math.hypot(3, 4)); // 5
