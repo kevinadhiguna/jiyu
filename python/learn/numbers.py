65e6781dc90749f0a73d4d339eb027ef961c9f29
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-05-11 09:00:47 +0700

    refactor: add numbers in python

---
+++ python/learn/numbers.py
@@ -0,0 +1,20 @@
+# 3 numeric types in Python : int, float, complex
+
+# (1) int
+var_int1 = 1
+var_int2 = 9548437234
+var_int3 = -684938432
+
+# (2) float
+var_float1 = 10.5
+var_float2 = 3.42
+var_float3 = -5.34
+
+# (3) complex
+var_complex1 = 1j
+var_complex2 = 4+7j
+var_complex3 = -753j
+
+print("type of", var_int1, ": ", type(var_int1))
+print("type of", var_float2, ": ", type(var_float2))
+print("type of", var_complex3, ": ", type(var_complex3))
