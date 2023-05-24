b20e407323e7956abb2867d5f174d885d490d92a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-05-24 11:18:41 +0700

    refactor: get character in a string

---
+++ python/learn/strings-are-arrays.py
@@ -0,0 +1,19 @@
+# Strings are arrays
+city = "Los Angeles"
+print(city[0] + city[1] + city[2]) # Los
+
+for char in city:
+  print(char)
+# L
+# o
+# s
+ 
+# A
+# n
+# g
+# e
+# l
+# e
+# s
+
+print("String length :", len(city))
