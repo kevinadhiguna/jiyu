0a3e2a9c30ee139ef90db7cded1a89136b66ae40
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-05-27 08:08:06 +0700

    modify a string

---
+++ python/learn/modify-string.py
@@ -0,0 +1,12 @@
+alWestChampion = "Los Angeles Angels"
+print("alWestChampion : {}".format(alWestChampion))
+
+alWestChampion_uppercase = alWestChampion.upper()
+print("alWestChampion_uppercase : {}".format(alWestChampion_uppercase))
+
+alWestChampion_lowercase = alWestChampion.lower()
+print("alWestChampion_lowercase : {}".format(alWestChampion_lowercase))
+
+# Remove whitespace (whitespace: a space before and/or after the actual text)
+alWestChampion_strip = alWestChampion.strip()
+print("alWestChampion_strip : {}".format(alWestChampion_strip))
