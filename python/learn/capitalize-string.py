d4878c0215e6609376ab9196e93f339815d44690
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-05-29 07:41:52 +0700

    refactor: capitalize a string

---
+++ python/learn/capitalize-string.py
@@ -0,0 +1,5 @@
+sentence = "kubo has been pretty solid this season and is a prominent part of Real Sociedad offense"
+capitalized = sentence.capitalize()
+
+print(capitalized)
+# Kubo has been pretty solid this season and is a prominent part of real sociedad offense
