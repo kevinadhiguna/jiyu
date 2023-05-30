c11480b52b3c43415a324f6b6164aed15b0ad68d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-05-30 08:40:47 +0700

    refactor: access list items

---
+++ python/learn/access-list-items.py
@@ -0,0 +1,5 @@
+cleanup = ["Trout", "Ohtani", "Judge"]
+
+print("First item of {}".format(cleanup), ":", cleanup[0])  # Trout
+print("Second item of {}".format(cleanup), ":", cleanup[1]) # Ohtani
+print("Last item of {}".format(cleanup), ":", cleanup[-1])  # Judge
