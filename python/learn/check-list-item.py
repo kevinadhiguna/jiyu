b8957c7aa0aec8e8999e2a186faac76eec48e59b
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-06-01 07:27:39 +0700

    refactor: check a list item

---
+++ python/learn/check-list-item.py
@@ -0,0 +1,8 @@
+leadoffs = ["Betts", "Arraez", "Springer", "Moniak", "Altuve"]
+
+if "Julio" in leadoffs:
+  print("Yes, Julio ia a leadoff man")
+else:
+  print("Julio is not a leadoff man")
+
+# Julio is not a leadoff man
