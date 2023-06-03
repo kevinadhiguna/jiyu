73fb8dbfbaa79b95b6ecaada3f339dd2c8c1f998
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-06-03 08:27:44 +0700

    refactor: floor division

---
+++ python/learn/floor-division.py
@@ -0,0 +1,11 @@
+num1 = 573
+num2 = 5
+result = num1 // num2
+
+print("(Floor division) {}".format(num1), "//", "{}".format(num2), ": {}".format(result))
+# (Floor division) 573 // 5 : 114
+
+# Notes:
+# 570 / 5 = 114
+# 570 // 5 = 114
+# 573 // 5 = 114
