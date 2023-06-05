1c3a3f2806df78d49642ee5e4fcce9cca10370aa
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-06-05 10:48:00 +0700

    refactor: add lists

---
+++ python/learn/lists.py
@@ -0,0 +1,10 @@
+reliefers = ["Cano", "Bickford", "Moore"]
+
+print("Length of {}".format(reliefers), "is", len(reliefers)) # 3
+print("Type of {}".format(reliefers), "is", type(reliefers)) # <class 'list'>
+
+# Another way to create a list
+closers = list(("Bautista", "Graterol", "Estevez", "Jansen")) # <- Must be in double-round brackets
+
+print("Length of {}".format(closers), "is", len(closers)) # 4
+print("Type of {}".format(closers), "is", type(closers)) # <class 'list'>
