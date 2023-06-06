4827fda680a94734418ed7f7c6975c24e5bf4e03
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-06-06 09:27:26 +0700

    refactor: add arithmetic operators

---
+++ python/learn/arithmetic-operators.py
@@ -0,0 +1,40 @@
+num1 = 100
+
+num1 += 5
+print("num1 += 5 -> {}".format(num1))
+
+num1 -= 5
+print("num1 -= 5 -> {}".format(num1))
+
+num1 *= 5
+print("num1 *= 5 -> {}".format(num1))
+
+num1 /= 5
+print("num1 /= 5 -> {}".format(num1))
+
+num1 %= 5
+print("num1 %= 5 -> {}".format(num1))
+
+num1 //= 5
+print("num1 //= 5 -> {}".format(num1))
+
+num1 **= 5
+print("num1 **= 5 -> {}".format(num1))
+
+num1 &= 5
+print("num1 &= 5 -> {}".format(num1))
+
+num1 &= 5
+print("num1 &= 5 -> {}".format(num1))
+
+num1 |= 5
+print("num1 |= 5 -> {}".format(num1))
+
+num1 ^= 5
+print("num1 ^= 5 -> {}".format(num1))
+
+num1 >>= 5
+print("num1 >>= 5 -> {}".format(num1))
+
+num1 <<= 5
+print("num1 <<= 5 -> {}".format(num1))
