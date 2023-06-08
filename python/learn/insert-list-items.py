a9e2793f115ad7e2b68222cf9df81cbdef5ad404
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-06-08 10:21:53 +0700

    refactor: insert list items

---
+++ python/learn/insert-list-items.py
@@ -0,0 +1,4 @@
+bottom_lineup = ["Neto", "Vargas", "Matijevic", "Riley", "Higashioka"]
+
+bottom_lineup.insert(1, "Estrada")
+print("bottom of a lineup: {}".format(bottom_lineup)) # ['Neto', 'Estrada', 'Vargas', 'Matijevic', 'Riley', 'Higashioka']
