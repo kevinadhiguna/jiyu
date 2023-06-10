34a7c9834da4845b2a5e51116ad446b0dd166878
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-06-10 07:45:27 +0700

    refactor: loop list items using while loop

---
+++ python/learn/loop-list-items-2.py
@@ -0,0 +1,13 @@
+closers = ["Bautista", "Hendriks", "Estevez"]
+
+# Loop list items using a while loop
+i = 0
+
+while i < len(closers):
+  print("Closer-{}".format(i+1), ": {}".format(closers[i]))
+  i = i + 1
+
+# Output:
+# Closer-1 : Bautista
+# Closer-2 : Hendriks
+# Closer-3 : Estevez
