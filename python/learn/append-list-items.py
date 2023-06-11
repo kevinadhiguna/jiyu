fbf5238c59907a0323bf0e6ceee4e990f18f0209
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-06-11 07:17:30 +0700

    refactor: append list items

---
+++ python/learn/append-list-items.py
@@ -0,0 +1,4 @@
+bottom_lineup = ["Neto", "Vargas", "Matijevic", "Riley", "Higashioka"]
+
+bottom_lineup.append("Heyward")
+print("bottom of a lineup: {}".format(bottom_lineup)) # ['Neto', 'Vargas', 'Matijevic', 'Riley', 'Higashioka', 'Heyward']
