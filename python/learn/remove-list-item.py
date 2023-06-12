85b7353cd26daa92663d44a76563951814b959ed
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-06-12 20:35:37 +0700

    refactor: remove a list item

---
+++ python/learn/remove-list-item.py
@@ -0,0 +1,6 @@
+top_lineup = ["Stanton", "Olson", "Freeman", "Rustchman", "Bichette"]
+print("Top of lineup (BEFORE): {}".format(top_lineup)) # ['Stanton', 'Olson', 'Freeman', 'Rustchman', 'Bichette']
+
+# Remove a specific item
+top_lineup.remove("Rustchman")
+print("Top of lineup (AFTER) : {}".format(top_lineup)) # ['Stanton', 'Olson', 'Freeman', 'Bichette']
