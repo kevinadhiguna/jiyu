6e7bb69ab300f597ea8d0cc2021cdada44aea1a6
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-06-15 07:54:06 +0700

    refactor: add list comprehension

---
+++ python/learn/list-comprehension.py
@@ -0,0 +1,10 @@
+managers = ["Dana Brown", "Perry Minasian", "Andrew Friedman"]
+GMs = []
+
+for manager in managers:
+  GMs.append(manager)
+
+print("GMs : {}".format(GMs))
+
+# Output:
+# GMs : ['Dana Brown', 'Perry Minasian', 'Andrew Friedman']
