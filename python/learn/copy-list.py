c0ca43d092b24b9c97305aaa8a0b3a947fd21d54
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-06-17 08:37:23 +0700

    refactor: copy a list

---
+++ python/learn/copy-list.py
@@ -0,0 +1,6 @@
+al_rookies = ["neto", "jung", "yoshida"]
+
+al_rookies_list = al_rookies.copy()
+
+print("AL rookies : {}".format(al_rookies)) # ["neto", "jung", "yoshida"]
+print("AL rookies list : {}".format(al_rookies_list)) # ["neto", "jung", "yoshida"]
