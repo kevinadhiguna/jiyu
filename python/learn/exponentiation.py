8898ad541ee09160b3d1b0c767b4e90942da11d3
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-06-20 07:59:41 +0700

    refactor: add exponentiation

---
+++ python/learn/exponentiation.py
@@ -0,0 +1,6 @@
+num1 = 5
+num2 = 2
+exponentiationResult = num1 ** num2
+
+print("(Exponentiation) {}".format(num1), "**", "{}".format(num2), ": {}".format(exponentiationResult))
+# (Exponentiation) 5 ** 2 : 25
