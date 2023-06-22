4fe3c12d6f732399c7519ff66d5aeb17d17dd6cb
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-06-22 09:59:54 +0700

    refactor: join lists

---
+++ python/learn/join-lists-2.py
@@ -0,0 +1,8 @@
+al_east = ["orioles", "yankees" , "blue_jays", "redsox", "rays"]
+prominent_players = ["mullins", "judge", "vladdy", "devers", "franco"]
+
+for prominent_player in prominent_players:
+  al_east.append(prominent_player)
+
+print("AL East and each of their star : {}".format(al_east)) 
+# ['orioles', 'yankees', 'blue_jays', 'redsox', 'rays', 'mullins', 'judge', 'vladdy', 'devers', 'franco']
