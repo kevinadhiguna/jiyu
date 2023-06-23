e0f8b9d5907596639e3d2a81e356e23084a57f4c
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-06-23 11:56:48 +0700

    refactor: sort lists

---
+++ python/learn/sort-lists.py
@@ -0,0 +1,13 @@
+# --- Sort a list alphanumerically (ascending, by default) ---
+
+first_basemen = ["Freddie Freeman", "Matt Olson", "Vladimir Guerrero Jr.", "Jared Walsh", "Pete Alonso"]
+first_basemen.sort()
+
+print("First Basemen (sorted) : {}".format(first_basemen))
+# ['Freddie Freeman', 'Jared Walsh', 'Matt Olson', 'Pete Alonso', 'Vladimir Guerrero Jr.']
+
+first_basemen_homers = [31, 32, 27, 35, 37]
+first_basemen_homers.sort()
+
+print("Homers by First Basemen (sorted) : {}".format(first_basemen_homers))
+# [27, 31, 32, 35, 37]
