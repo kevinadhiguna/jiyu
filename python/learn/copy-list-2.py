f62437258ff3f64e46a55a9ead31c520f045c91d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-06-27 08:40:17 +0700

    refactor: copy a list

---
+++ python/learn/copy-list-2.py
@@ -0,0 +1,7 @@
+nl_rookies = ["carroll", "miller", "mcclain"]
+
+# Another way to copy a list
+nl_rookies_list = list(nl_rookies)
+
+print("NL rookies : {}".format(nl_rookies)) # ['carroll', 'miller', 'mcclain']
+print("NL rookies list : {}".format(nl_rookies_list)) # ['carroll', 'miller', 'mcclain']
