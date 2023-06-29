eb7fd9d409aaedad145a166e46046aef8c3954d3
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-06-29 07:34:44 +0700

    refactor: add list comprehension

---
+++ python/learn/list-comprehension-4.py
@@ -0,0 +1,5 @@
+players = ["Bichette", "Franco", "Semien", "Neto", "Vargas"]
+
+shortstop = [player.upper() for player in players]
+
+print("shortstops : {}".format(shortstop)) # ['BICHETTE', 'FRANCO', 'SEMIEN', 'NETO', 'VARGAS']
