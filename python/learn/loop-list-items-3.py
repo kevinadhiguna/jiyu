308e6977691c671e88274b260270e979d8a61002
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-06-30 09:15:40 +0700

    refactor: loop list items

---
+++ python/learn/loop-list-items-3.py
@@ -0,0 +1,9 @@
+starters = ["Ohtani", "Miller", "Kershaw"]
+
+# Looping using list comprehension
+[print("Starter: {}".format(starter)) for starter in starters]
+
+# Output:
+# Starter: Ohtani
+# Starter: Miller
+# Starter: Kershaw
