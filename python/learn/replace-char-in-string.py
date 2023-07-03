61035ac7ca320161d820537ff5bfef14143dbf53
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-07-03 11:58:47 +0700

    refactor: replace a word in a string

---
+++ python/learn/replace-char-in-string.py
@@ -0,0 +1,6 @@
+programming_language = "JavaScript"
+
+another_programming_language = programming_language.replace("Java", "Coffee")
+
+print("programming_language: {}".format(programming_language)) # JavaScript
+print("another_programming_language: {}".format(another_programming_language)) # CoffeeScript
