831a75564700ee76f82a7f5839943d2194e029e0
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-07-07 09:38:34 +0700

    refactor: loop items in a tuple

---
+++ python/learn/tuple-loop.py
@@ -0,0 +1,11 @@
+homerunDerbyParticipants = ("julio", "juan", "kyle", "mookie", "pete")
+
+# Loop through a loop
+for participant in homerunDerbyParticipants:
+  print("Participant : {}".format(participant))
+
+# Participant : julio
+# Participant : juan
+# Participant : kyle
+# Participant : mookie
+# Participant : pete
