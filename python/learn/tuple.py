48de1455434cc3532ca389689fda9c36d9dd8c73
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-07-09 17:07:02 +0700

    refactor: add a tuple

---
+++ python/learn/tuple.py
@@ -0,0 +1,5 @@
+# A tuple allows duplicates
+players = ("ted_williams", "babe_ruth", "ted_williams")
+
+print("Players : {}".format(players)) # ('ted_williams', 'babe_ruth', 'ted_williams')
+print("Type of players tuple : {}".format(type(players))) # <class 'tuple'>
