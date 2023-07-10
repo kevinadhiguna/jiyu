9c9813132870d64790f511631bc0b6e107aaa840
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-07-10 09:02:09 +0700

    refactor: unpack a tuple

---
+++ python/learn/tuple-unpack.py
@@ -0,0 +1,7 @@
+teams = ("brighton", "liverpool", "arsenal")
+
+(seagulls, liverpudlian, gooners) = teams
+
+print("seagulls : {}".format(seagulls))           # brighton
+print("liverpudlian : {}".format(liverpudlian))   # liverpool
+print("gooners : {}".format(gooners))             # arsenal
