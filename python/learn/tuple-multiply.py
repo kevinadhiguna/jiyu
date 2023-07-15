93e3ebccc23dd384762d13531dfd3af48102deea
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-07-15 09:03:20 +0700

    refactor: multiple tuple's items

---
+++ python/learn/tuple-multiply.py
@@ -0,0 +1,5 @@
+braves = ("riley", "acuna", "olson")
+
+bravesMultiplies = braves * 2
+
+print("Braves team (multiplied) : {}".format(bravesMultiplies)) # ('riley', 'acuna', 'olson', 'riley', 'acuna', 'olson')
