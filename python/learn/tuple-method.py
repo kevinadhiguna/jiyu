6d9f849c8c486f506ca4623e8a4b1b24f33ed7ed
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-07-16 09:54:18 +0700

    refactor: use some tuple methods

---
+++ python/learn/tuple-method.py
@@ -0,0 +1,7 @@
+phillies = ("harper", "turner", "schwarber", "harper", "stott", "marsh", "harper", "bohm")
+
+# Method-1: count()
+print("Occurence of 'harper' in {}".format(phillies), " : {}".format(phillies.count("harper")), "\n") # 3
+
+# Method-2: index()
+print("Index of 'schwarber' in {}".format(phillies), " : {}".format(phillies.index('schwarber'))) # 2
