ac3fdb35168cae76df3b4eba6fc097a920c1b0c4
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-07-17 08:14:55 +0700

    refactor: loop tuple items

---
+++ python/learn/tuple-loop-2.py
@@ -0,0 +1,15 @@
+americanLeagueAllStars = ("ohoppe", "ohtani", "trout", "judge", "yordan", "bo", "altuve", "vladdy", "rendon")
+
+# Loop through a For loop
+for i in range(len(americanLeagueAllStars)):
+  print("American League AllStar - {}".format(i+1), " : {}".format(americanLeagueAllStars[i]))
+
+# American League AllStar - 1  : ohoppe
+# American League AllStar - 2  : ohtani
+# American League AllStar - 3  : trout
+# American League AllStar - 4  : judge
+# American League AllStar - 5  : yordan
+# American League AllStar - 6  : bo
+# American League AllStar - 7  : altuve
+# American League AllStar - 8  : vladdy
+# American League AllStar - 9  : rendon
