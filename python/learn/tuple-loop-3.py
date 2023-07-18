781937a0d581caa9cbee7ae7f71a5c3f10440f62
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-07-18 08:43:09 +0700

    refactor: loop tuple items

---
+++ python/learn/tuple-loop-3.py
@@ -0,0 +1,18 @@
+nationalLeagueAllStars = ("betts", "freeman", "machado", "tatis", "turner", "stroman", "murphy", "harper", "carroll")
+
+i = 0
+
+# Loop through a While loop
+while i < len(nationalLeagueAllStars):
+  print("National League All Star - {}".format(i+1), " : {}".format(nationalLeagueAllStars[i]))
+  i = i+1
+
+# National League All Star - 1  : betts
+# National League All Star - 2  : freeman
+# National League All Star - 3  : machado
+# National League All Star - 4  : tatis
+# National League All Star - 5  : turner
+# National League All Star - 6  : stroman
+# National League All Star - 7  : murphy
+# National League All Star - 8  : harper
+# National League All Star - 9  : carroll
