dde9078baabb9d6865323e64762512e4d0639c91
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-07-19 13:37:32 +0700

    refactor: join two tuples into one

---
+++ python/learn/tuple-join.py
@@ -0,0 +1,6 @@
+dodgers = ("betts", "freeman", "smith")
+angels = ("ohtani", "trout", "ohoppe")
+
+losAngelesTeam = dodgers + angels
+
+print("Los Angeles team : {}".format(losAngelesTeam)) # ('betts', 'freeman', 'smith', 'ohtani', 'trout', 'ohoppe')
