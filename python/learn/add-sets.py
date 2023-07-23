e20f714c29d2c75ce24286fd38bbd9b62f6020a5
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-07-23 08:21:26 +0700

    refactor: add sets

---
+++ python/learn/add-sets.py
@@ -0,0 +1,6 @@
+fielders = {"Judge", "Bader", "Rizzo"}
+pitchers = {"Cole", "Rodon", "Schimdt"}
+
+fielders.update(pitchers)
+
+print("Yankees : {}".format(fielders)) # {'Cole', 'Rizzo', 'Rodon', 'Judge', 'Bader', 'Schimdt'}
