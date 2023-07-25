65713d630fde1fc3d26b947b0bf287180e4e7dc1
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-07-25 10:02:34 +0700

    refactor: add a set item

---
+++ python/learn/add-set-item.py
@@ -0,0 +1,11 @@
+padres = {"Machado", "Tatis Jr.", "Soto"}
+
+# Add an item to a set
+padres.add("Sanchez")
+
+for padre in padres:
+  print("Padres : {}".format(padre))
+# Padres : Tatis Jr.
+# Padres : Soto
+# Padres : Sanchez
+# Padres : Machado
