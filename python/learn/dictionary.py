02a1b4f16411bf78030988b8f9de63b511a94afa
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-07-27 11:32:23 +0700

    refactor: add a dictionary

---
+++ python/learn/dictionary.py
@@ -0,0 +1,11 @@
+# No duplicates is allowed in a dictionary!
+padres = {
+  "gm": "AJ Preller",
+  "star": "Fernando Tatis Jr.",
+  "home": "San Diego"
+}
+
+print("Padres : {}".format(padres)) # {'gm': 'AJ Preller', 'star': 'Fernando Tatis Jr.', 'home': 'San Diego'}
+
+print("length of padres (dictionary) : {}".format(len(padres))) # 3
+print("type of padres : {}".format(type(padres))) #  <class 'dict'>
