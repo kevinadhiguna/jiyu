6d703e665b09eab3624ad90087ce38bea48c9e90
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-07-29 08:48:01 +0700

    refactor: copy a dictionary

---
+++ python/learn/copy-dictionary.py
@@ -0,0 +1,10 @@
+cardinals = {
+  "league": "NL",
+  "city": "St. Louis"
+}
+
+print("Cardinals : {}".format(cardinals)) # {'league': 'NL', 'city': 'St. Louis'}
+
+eagles = cardinals.copy()
+
+print("Eagles : {}".format(eagles)) # {'league': 'NL', 'city': 'St. Louis'}
