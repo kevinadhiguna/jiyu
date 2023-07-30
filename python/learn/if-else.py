c654f47a557e09fc8248d42a904720442d6976fe
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-07-30 12:43:52 +0700

    refactor: add if-else

---
+++ python/learn/if-else.py
@@ -0,0 +1,12 @@
+number1 = 100
+number2 = 150
+
+if number1 > number2:
+  print("Number 1 is bigger than Number 2")
+elif number1 < number2:
+  print("Number 2 is bigger than Number 1")
+else:
+  print("Number 1 is equal with Number 2")
+
+# === Output ===
+# Number 2 is bigger than Number 1
