a3ece8b61113d8b70b35d9eb014da719ad14d812
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-07-31 10:46:19 +0700

    refactor: clear a dictionary

---
+++ python/learn/clear-dictionary.py
@@ -0,0 +1,11 @@
+cardinals = {
+  "league": "NL",
+  "city": "St. Louis"
+}
+
+print("Cardinals (BEFORE) : {}".format(cardinals)) # {'league': 'NL', 'city': 'St. Louis'}
+
+# Empty the dictionary
+cardinals.clear()
+
+print("Cardinals (AFTER) : {}".format(cardinals)) # {}
