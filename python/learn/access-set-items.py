a7f0d984fcfa0dd36caa80280c4b07da4f7a1fa0
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-08-01 11:11:35 +0700

    refactor: access set items

---
+++ python/learn/access-set-items.py
@@ -0,0 +1,10 @@
+padres = {"Machado", "Tatis Jr.", "Soto"}
+
+for padre in padres:
+  print("Padres : {}".format(padre))
+# Padres : Tatis Jr.
+# Padres : Machado
+# Padres : Soto
+
+# Check if a specific item exists in a set
+print("Is Soto a Padre ? {}".format("Soto" in padres)) # True
