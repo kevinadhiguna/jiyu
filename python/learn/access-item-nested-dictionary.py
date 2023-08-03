557af7f3bdd0c53101340589d2d516414889c2de
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-08-03 08:55:51 +0700

    refactor: access an item of a nested dictionary

---
+++ python/learn/access-item-nested-dictionary.py
@@ -0,0 +1,12 @@
+mariners = {
+  "pitchers": {
+    "starter": "Luis Castillo",
+    "closer": "Paul Sewald"
+  },
+  "fielders": {
+    "infielder": "JP Crawford",
+    "outfielder": "Julio Rodriguez"
+  }
+}
+
+print("Starting pitcher of Seattle Mariners : {}".format(mariners["pitchers"]["starter"])) # Luis Castillo
