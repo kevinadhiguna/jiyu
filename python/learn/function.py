d5a17c0edf1f56a548900be01e5121206d594525
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-08-04 09:21:27 +0700

    refactor: add a function

---
+++ python/learn/function.py
@@ -0,0 +1,5 @@
+def summonName(firstName, lastName):
+  # print("Full name : {}".format(firstName), "{}".format(lastName)) # <- same as below
+  print("Full name :", firstName, lastName)
+
+summonName("Nick", "Seager") # Full name : Nick Seager
