0bc5ac9805b2c9fe3fb2ad400bba5e81410df6c1
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-08-05 16:22:56 +0700

    refactor: get keys of a dictionary

---
+++ python/learn/get-keys-of-dictionary.py
@@ -0,0 +1,9 @@
+dodgers = {
+  "league": "NL",
+  "gm": "Andrew Friedman",
+}
+
+keys = dodgers.keys()
+
+print("Keys of Dodgers dictionary : {}".format(keys))
+# Keys of Dodgers dictionary : dict_keys(['league', 'gm'])
