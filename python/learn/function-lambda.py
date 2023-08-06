57a1d59cda8ba83fe0ec4321233291dc4a200615
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-08-06 07:26:28 +0700

    refactor: add a lambda function

---
+++ python/learn/function-lambda.py
@@ -0,0 +1,11 @@
+x = lambda a : a * 4
+
+result = x(3)
+print("Result :", result) # 12
+
+# === A look-alike JavaScript code ===
+# const x = (a) => a * 4;
+#
+# const result = x(3)
+# console.log("Result :", result)  // 12
+#
