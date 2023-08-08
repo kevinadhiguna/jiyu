2f80201eb25a20077be3b65d9dd1feae0e008cfc
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-08-08 08:22:10 +0700

    refactor: add an or statement in if

---
+++ python/learn/if-or.py
@@ -0,0 +1,6 @@
+pitcherWar = 6
+hitterWar = 12
+defensiveWar = -0.8
+
+if pitcherWar > defensiveWar or hitterWar < defensiveWar:
+  print("Either pitcher WAR or hitter WAR is greater than defensive WAR")
