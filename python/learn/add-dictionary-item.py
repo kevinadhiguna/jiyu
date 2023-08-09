5fad93b267e882fceedba4b92ac5dd67306bf43d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-08-09 16:06:08 +0700

    refactor: add a dictionary item

---
+++ python/learn/add-dictionary-item.py
@@ -0,0 +1,10 @@
+cardinals = {
+  "league": "NL",
+  "city": "St. Louis"
+}
+
+print("Cardinals (BEFORE) : {}".format(cardinals)) # {'league': 'NL', 'city': 'St. Louis'}
+
+cardinals["starPlayer"] = "Nolan Arenado"
+
+print("Cardinals (AFTER) : {}".format(cardinals)) # {'league': 'NL', 'city': 'St. Louis', 'starPlayer': 'Nolan Arenado'}
