582185dbedb048203546e73ae81a3c1fc338d4b8
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-08-10 09:45:33 +0800

    refactor: get items of dictionary

---
+++ python/learn/get-items-of-dictionary.py
@@ -0,0 +1,9 @@
+dodgers = {
+  "league": "NL",
+  "gm": "Andrew Friedman"
+}
+
+items = dodgers.items()
+
+print("Items of Dodgers dictionary : {}".format(items))
+# Items of Dodgers dictionary : dict_items([('league', 'NL'), ('gm', 'Andrew Friedman')])
