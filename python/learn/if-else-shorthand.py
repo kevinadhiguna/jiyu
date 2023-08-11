969aec6319711e8fa170fd97c4905cfc9159c8ff
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-08-11 11:08:19 +0800

    refactor: add if-else shorthand

---
+++ python/learn/if-else-shorthand.py
@@ -0,0 +1,15 @@
+mark1 = 89
+mark2 = 97
+
+print("Mark 2 is higher than Mark 1") if mark2 > mark1 else print("Mark 2 is not higher than Mark 1")
+
+# == Output ===
+# Mark 2 is higher than Mark 1
+
+number1 = 134
+number2 = 97
+
+print("Number 2 is bigger than Number 1") if number2 > number1 else print("Number 2 is not bigger than Number 1")
+
+# === Output ===
+# Number 2 is not higher than Number 1
