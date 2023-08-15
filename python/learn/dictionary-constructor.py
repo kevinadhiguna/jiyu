4df5a0071c9d4fbfbc404b27a4be7e012f7cd8fc
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-08-15 08:23:27 +0700

    refactor: construct a dictionary

---
+++ python/learn/dictionary-constructor.py
@@ -0,0 +1,6 @@
+rays = dict(league = "AL", gm = "Peter Bendix")
+
+print("Rays (dictionary) : {}".format(rays)) # {'league': 'AL', 'gm': 'Peter Bendix'}
+
+print("Rays' length (dictionary) : {}".format(len(rays))) # 2
+print("Type of Rays : {}".format(type(rays))) # <class 'dict'>
