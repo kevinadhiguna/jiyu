5add426226810f984708024c0b9d3d48dbc25c07
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-08-16 09:11:46 +0700

    refactor: change keys in a dictionary

---
+++ python/learn/check-key-in-dictionary.py
@@ -0,0 +1,22 @@
+dodgers = {
+  "league": "NL",
+  "gm": "Andrew Friedman"
+}
+
+### -- Check 'league' key -- ###
+if "league" in dodgers:
+  print("'league' key is available in the Dodgers dictionary")
+
+# 'league' key is available in the Dodgers dictionary
+
+### ------------------------ ###
+
+### -- Check 'trainingLocation' key -- ###
+if "trainingLocation" in dodgers:
+  print("the Dodgers dictionary reveals training location")
+else:
+  print("The Dodgers training location is not uncovered in the Dodgers dictionary")
+
+# The Dodgers training location is not uncovered in the Dodgers dictionary
+
+### ---------------------------------- ###
