4db6cc159782bc8d04e719f29d4a6a71499c6c28
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-08-17 08:35:04 +0700

    refactor: get values of a dictionary

---
+++ python/learn/get-values-of-dictionary.py
@@ -0,0 +1,9 @@
+dodgers = {
+  "league": "NL",
+  "gm": "Andrew Friedman"
+}
+
+values = dodgers.values()
+
+print("Values of Dodgers dictionary : {}".format(values))
+# Values of Dodgers dictionary : dict_values(['NL', 'Andrew Friedman'])
