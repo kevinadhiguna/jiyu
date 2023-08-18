7829f0729729751faf99b8c4085bb6a5c80c2c2a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-08-18 12:51:13 +0700

    refactor: include pass statement in if

---
+++ python/learn/if-pass-statement.py
@@ -0,0 +1,9 @@
+student1 = "De La Cruz"
+student2 = "Davis"
+
+if len(student1) > len(student2):
+  pass
+else:
+  print("Name of student 1 is longer than student 2")
+
+# Nothing is displayed cuz 'pass' is inside the if block
