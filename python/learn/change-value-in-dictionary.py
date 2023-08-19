6d41eaa6745d869e19c88c88b7b69771f5866c89
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-08-19 12:18:40 +0700

    refactor: change values in a dictionary

---
+++ python/learn/change-value-in-dictionary.py
@@ -0,0 +1,10 @@
+cardinals = {
+  "league": "NL",
+  "city": "St. Louis"
+}
+
+print("Cardinals['city'] (BEFORE) : {}".format(cardinals["city"])) # St. Louis
+
+cardinals["city"] = "Denver"
+
+print("Cardinals['city'] (AFTER) : {}".format(cardinals["city"])) # Denver
