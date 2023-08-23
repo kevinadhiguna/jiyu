dd4222cc73a6394604340dc25e9835255d6adc65
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-08-23 08:55:34 +0700

    refactor: add another shorthand of if-else

---
+++ python/learn/if-else-shorthand-2.py
@@ -0,0 +1,4 @@
+fastballVelocity = 103
+cutterVelocity = 92
+
+print("A fastball is much faster than a cutter") if fastballVelocity > cutterVelocity else print("=") if fastballVelocity == cutter_velocity else print("A cutter is faster than a fastball")
