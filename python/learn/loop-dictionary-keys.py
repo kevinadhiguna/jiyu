23e1460288fe7bdc7afe3d499558f8334bb5efa1
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-08-24 10:11:00 +0700

    refactor: loop a dictionary's keys

---
+++ python/learn/loop-dictionary-keys.py
@@ -0,0 +1,10 @@
+cardinals = {
+  "league": "NL",
+  "city": "St. Louis"
+}
+
+for key in cardinals.keys():
+  print("Key of dictionary : {}".format(key))
+
+# Key of dictionary : league
+# Key of dictionary : city
