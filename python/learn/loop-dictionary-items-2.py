737e3677161337bef6f387dce98c713cd92a60c0
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-08-25 08:06:26 +0700

    refactor: loop dictionary items

---
+++ python/learn/loop-dictionary-items-2.py
@@ -0,0 +1,13 @@
+cardinals = {
+  "league": "NL",
+  "city": "St. Louis"
+}
+
+for key, value in cardinals.items():
+  print("Key of Cardinals : {}".format(key))
+  print("Value of Cardinals : {}".format(value))
+
+# Key of Cardinals : league
+# Value of Cardinals : NL
+# Key of Cardinals : city
+# Value of Cardinals : St. Louis
