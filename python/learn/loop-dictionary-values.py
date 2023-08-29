8a08230a3be01da63433c6d490b7b1494d95792a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-08-29 12:42:57 +0700

    refactor: loop dictionary values

---
+++ python/learn/loop-dictionary-values.py
@@ -0,0 +1,10 @@
+cardinals = {
+  "league": "NL",
+  "city": "St. Louis"
+}
+
+for value in cardinals.values():
+  print("Value of dictionary : {}".format(value))
+
+# Value of dictionary : NL
+# Value of dictionary : St. Louis
