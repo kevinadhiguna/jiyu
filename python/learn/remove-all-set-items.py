9dfb08322660be66549471c73ac8c45121aff2ed
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-08-30 07:30:56 +0700

    refactor: remove all set items

---
+++ python/learn/remove-all-set-items.py
@@ -0,0 +1,7 @@
+tigers = {"Skubal", "Miggy", "Tolkerson"}
+
+print("Tigers (BEFORE) : {}".format(tigers)) # {'Tolkerson', 'Miggy', 'Skubal'}
+
+tigers.clear()
+
+print("Tigers (AFTER) : {}".format(tigers)) # set()
