c190cd048a0ba55061a6888ae57ff0164d4ce641
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-09-03 08:49:19 +0700

    refactor: break a while loop

---
+++ python/learn/loop-while-break.py
@@ -0,0 +1,15 @@
+i = 15
+
+while i != 18:
+  print("i : {}".format(i))
+
+  if i == 16:
+    print("i has reached 16, thereby the while loop breaks")
+    break
+  
+  i += 1
+
+# === Output ===
+# i : 15
+# i : 16
+# i has reached 16, thereby the while loop breaks
