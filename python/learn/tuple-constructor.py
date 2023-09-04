9ce233381d0fcd8d591eb9093bda6e75a58031b1
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-09-04 10:22:58 +0700

    refactor: add a tuple constructor

---
+++ python/learn/tuple-constructor.py
@@ -0,0 +1,4 @@
+buildings = (("apartment", "house", "mansion"))
+
+print("Buildings : {}".format(buildings)) # ('apartment', 'house', 'mansion')
+print("Type of Buildings : {}".format(type(buildings))) # <class 'tuple'>
