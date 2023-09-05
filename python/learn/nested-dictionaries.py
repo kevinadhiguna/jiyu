8b3716d008de355c2671bbec2afc8769139836c5
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-09-05 15:48:39 +0700

    refactor: add nested dictionaries

---
+++ python/learn/nested-dictionaries.py
@@ -0,0 +1,18 @@
+mariners = {
+  "pitchers": {
+    "starter": "Luis Castillo",
+    "closer": "Paul Sewald"
+  },
+  "fielders": {
+    "infielder": "JP Crawford",
+    "outfielder": "Julio Rodriguez"
+  }
+}
+
+print("Mariners : {}".format(mariners))
+
+# === Output === #
+# {
+#   'pitchers': {'starter': 'Luis Castillo', 'closer': 'Paul Sewald'}, 
+#   'fielders': {'infielder': 'JP Crawford', 'outfielder': 'Julio Rodriguez'}
+# }
