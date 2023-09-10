f23495df3da5b2ec0644b6169f376e531fcb0733
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-09-10 09:44:38 +0700

    refactor: access tuple items

---
+++ python/learn/tuple-item-access.py
@@ -0,0 +1,12 @@
+racing = ("honda", "ducati", "yamaha")
+
+print("racing[0] : {}".format(racing[0])) # honda
+print("racing[1] : {}".format(racing[1])) # ducati
+print("racing[2] : {}".format(racing[2])) # yamaha
+
+# ------------------------------------------------
+
+# Negative indexing (starts from the last one) :
+print("racing[-1] : {}".format(racing[-1])) # yamaha
+print("racing[-2] : {}".format(racing[-2])) # ducati
+print("racing[-3] : {}".format(racing[-3])) # honda
