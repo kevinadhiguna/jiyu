d4c200b528ddf07a061849172ca4cbf94c18133d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-09-19 13:31:46 +0700

    refactor: pass a list as an argument

---
+++ python/learn/function-passing-list.py
@@ -0,0 +1,14 @@
+def players(athletes):
+  for player in athletes:
+    print("Player :", player)
+
+laa = ["Ohtani", "Trout", "Glaus", "Guerrero", "Ryan"]
+
+players(laa)
+
+# === Output ===
+# Player : Ohtani
+# Player : Trout
+# Player : Glaus
+# Player : Guerrero
+# Player : Ryan
