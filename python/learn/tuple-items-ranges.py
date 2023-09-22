df4803c13a1ba6de7fba3fa229a596b9d27b06d0
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-09-22 11:36:43 +0700

    refactor: examine a tuple's items ranges

---
+++ python/learn/tuple-items-ranges.py
@@ -0,0 +1,6 @@
+laptops = ("hp", "asus", "apple", "dell", "toshiba")
+
+print("laptops[2:4] : {}".format(laptops[2:4])) # ('apple', 'dell')
+print("laptops[3:] : {}".format(laptops[3:])) # ('dell', 'toshiba')
+
+print("laptops[-4:-2] : {}".format(laptops[-4:-2])) # ('asus', 'apple')
