c219aa87f1444e49a94babfeff568b3a55373a2b
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-10-07 21:25:42 +0700

    refactor: add a lambda function

---
+++ python/learn/functions/function-lambda-2.py
@@ -0,0 +1,23 @@
+x = lambda i, j : i * j
+
+result = x(7,3)
+print("Result :", result) # 21
+
+# === A look-alike JavaScript code ===
+# const x = (i, j) => i * j;
+#
+# const result = x(7, 3)
+# console.log("Result :", result)  // 21
+#
+
+y = lambda a,b,c : a + b - c
+
+calc = y(5, 6, 1)
+print("Calc :", calc) # 10
+
+# === A look-alike JavaScript code ===
+# const y = (a, b, c) => a + b - c;
+#
+# const calc = y(5, 6, 1)
+# console.log("Calc :", calc) // 10
+#
