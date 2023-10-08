6894714fe8185be50c837ed25cff501c661e0f99
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-10-08 15:49:26 +0700

    refactor: add an array program

---
+++ python/learn/arrays/array.py
@@ -0,0 +1,12 @@
+airplanes = ["Delta Airways", "JAL", "British Airways"]
+
+print(airplanes[0]) # Delta Airways
+
+for airplane in airplanes:
+  print(airplane)
+
+# === Output ===
+# Delta Airways
+# JAL
+# British Airways
+#
