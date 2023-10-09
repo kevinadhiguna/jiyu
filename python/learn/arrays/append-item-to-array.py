b3a4c60c5f628b9e799259469b17b2ad9919b75b
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-10-09 09:58:54 +0700

    refactor: append an item to an array

---
+++ python/learn/arrays/append-item-to-array.py
@@ -0,0 +1,7 @@
+airplanes = ["Delta Airways", "JAL", "British Airways"]
+
+print("airplanes (BEFORE) : {}".format(airplanes)) # ['Delta Airways', 'JAL', 'British Airways']
+
+airplanes.append("ANA")
+
+print("airplanes (AFTER) : {}".format(airplanes)) # ['Delta Airways', 'JAL', 'British Airways', 'ANA']
