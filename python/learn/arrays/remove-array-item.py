6147483766bb175157e9825dd3b496d52526c2bf
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-10-10 10:44:43 +0700

    refactor: remove an array item

---
+++ python/learn/arrays/remove-array-item.py
@@ -0,0 +1,7 @@
+airplanes = ["Delta Airways", "JAL", "British Airways"]
+
+print("airplanes (BEFORE) : {}".format(airplanes)) # ['Delta Airways', 'JAL', 'British Airways']
+
+airplanes.remove("JAL")
+
+print("airplanes (AFTER) : {}".format(airplanes)) # ['Delta Airways', 'British Airways']
