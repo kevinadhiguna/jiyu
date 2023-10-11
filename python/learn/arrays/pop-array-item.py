e26905af8e4143362086ae4f1437edf4bc285cd1
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-10-11 07:12:51 +0700

    refactor: pop an array item

---
+++ python/learn/arrays/pop-array-item.py
@@ -0,0 +1,7 @@
+airplanes = ["Delta Airways", "JAL", "British Airways"]
+
+print("airplanes (BEFORE) : {}".format(airplanes)) # ['Delta Airways', 'JAL', 'British Airways']
+
+airplanes.pop(0)
+
+print("airplanes (AFTER) : {}".format(airplanes)) # ['JAL', 'British Airways']
