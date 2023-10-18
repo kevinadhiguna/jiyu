93f9d0b896a4d9b119ec93bae4ec3886f4ae776c
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-10-18 11:04:19 +0700

    refactor: add a pass statemtn to a class

---
+++ python/learn/classes/class-pass-statement.py
@@ -0,0 +1,9 @@
+class Dodgers:
+  # To avoid getting an error, put a pass statement in a class 
+  #   when you do not want to add any other properties or methods to the class 
+  #     rather than leaving it blank.
+  pass
+
+club = Dodgers()
+
+print("Club :", club) # <__main__.Dodgers object at 0x7facedfaf8e0>
