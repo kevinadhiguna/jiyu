22921df6e4b568761f86b9710ef6e7177ab6344e
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-10-21 08:10:56 +0700

    refactor: add a child class

---
+++ python/learn/classes/class-inheritance.py
@@ -0,0 +1,25 @@
+class Dodgers:
+  def __init__(self, headcoach, ws_win, gm):
+    self.headcoach = headcoach
+    self.ws_win = ws_win
+    self.gm = gm
+  
+  def printCoachAndGM(self):
+    print(self.headcoach, "-", self.gm)
+
+club = Dodgers(
+  "Dave Roberts", 
+  2020, 
+  "Andrew Friedman"
+)
+club.printCoachAndGM() # Dave Roberts - Andrew Friedman
+
+class OklahomaCity(Dodgers):
+  pass
+
+club2 = OklahomaCity(
+  "Dave Roberts", 
+  2020, 
+  "Andrew Friedman"
+)
+club2.printCoachAndGM() # Dave Roberts - Andrew Friedman
