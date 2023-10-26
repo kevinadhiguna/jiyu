174a4b59970170c25b7e98be74bc8f6df9932973
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-10-26 09:51:52 +0700

    refactor: add iter function to a string

---
+++ python/learn/string/string-iter-function.py
@@ -0,0 +1,11 @@
+sports = "baseball"
+sports_word = iter(sports)
+
+print(next(sports_word)) # b
+print(next(sports_word)) # a
+print(next(sports_word)) # s
+print(next(sports_word)) # e
+print(next(sports_word)) # b
+print(next(sports_word)) # a
+print(next(sports_word)) # l
+print(next(sports_word)) # l
