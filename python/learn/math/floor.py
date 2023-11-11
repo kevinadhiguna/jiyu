814096e6792386d3a7d0eef45961edd4d97a8b2a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-11-11 09:59:20 +0700

    refactor: add a floor function from math library

---
+++ python/learn/math/floor.py
@@ -0,0 +1,8 @@
+import math
+
+number1 = 6.67998888
+
+answer = math.floor(number1)
+
+print("A number downwards to", number1, "nearest integer :", answer)
+# A number downwards to 6.67998888 nearest integer : 6
