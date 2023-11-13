631a48bb5cb05cec40cb7e29ee06f89f477539ca
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-11-13 09:26:20 +0700

    refactor: calculate the area of a circle

---
+++ python/learn/math/area-of-circle.py
@@ -0,0 +1,8 @@
+import math
+
+radius = 5
+
+area = math.pi * radius * radius
+
+print("Radius of the circle :", radius) # Radius of the circle : 5
+print("Area of the circle :", area) # Area of the circle : 78.53981633974483
