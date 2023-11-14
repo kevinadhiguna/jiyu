63c2fd0c0a82a88fedcc9fb4f919a501c3e7f855
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-11-14 08:11:31 +0700

    refactor: add a ceil function

---
+++ python/learn/math/ceil.py
@@ -0,0 +1,8 @@
+import math
+
+number1 = 6.67998888
+
+answer = math.ceil(number1)
+
+print("A number upwards to", number1, "nearest integer :", answer)
+# A number upwards to 6.67998888 nearest integer : 7
