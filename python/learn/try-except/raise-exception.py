2383045183b1d2b46baf37f1f1915dd9ac0de5b5
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-11-22 23:53:49 +0700

    refactor: raise an exception

---
+++ python/learn/try-except/raise-exception.py
@@ -0,0 +1,11 @@
+x = 77
+
+if x < 80:
+  raise Exception("Sorry, you must score more than 80 to pass the exam")
+
+# == Output == #
+# Traceback (most recent call last):
+#   File "/home/user/raise-exception.py", line 4, in <module>
+#     raise Exception("Sorry, you must score more than 80 to pass the exam")
+# Exception: Sorry, you must score more than 80 to pass the exam
+# ============
