87db7339d4dba17fa7c6ca5ca741e0155c368722
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-11-24 09:27:15 +0700

    refactor: add else to exception

---
+++ python/learn/try-except/else.py
@@ -0,0 +1,12 @@
+# In case no error occurred:
+try:
+  print("Hello!")
+except:
+  print("An exception occurred")
+else:
+  print("Nothing went wrong")
+
+# == Output == #
+# Hello!
+# Nothing went wrong
+# ============
