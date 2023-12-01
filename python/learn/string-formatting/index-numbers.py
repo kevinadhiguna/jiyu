ae1e23b7d4599a148e996923eddd0cf76e4e744c
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-01 13:31:23 +0700

    refactor: add index numbers to string formatting

---
+++ python/learn/string-formatting/index-numbers.py
@@ -0,0 +1,11 @@
+contract = 511000000
+player = "Shohei Ohtani"
+years = 10
+club = "Los Angeles Dodgers"
+
+news = "{0} has signed with the {1} for {2} years ${3:.2f}"
+print(news.format(player, club, years, contract))
+
+# == Output == #
+# Shohei Ohtani has signed with the Los Angeles Dodgers for 10 years $511000000.00
+# ============ #
