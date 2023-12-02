609c98703e76e2390ea969cfb87d65cfee8e6b64
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-02 07:53:51 +0700

    refactor(security): attempt to prevent clickjacking

--- nodejs-express-ts/index.ts
+++ nodejs-express-ts/index.ts
@@ -4,6 +4,13 @@ import { config } from 'dotenv';
 const app: Express = express();
 config();
 
+// Set X-Frame-Options header to 'same-origin' for all responses (clickjacking defense)
+app.use((req: Request, res: Response, next) => {
+  res.setHeader('X-Frame-Options', 'same-origin');
+  next();
+});
+// For more: https://cheatsheetseries.owasp.org/cheatsheets/Clickjacking_Defense_Cheat_Sheet.html#defending-with-x-frame-options-response-headers
+
 app.get('/', (req: Request, res: Response) => {
   res.json({
     status: 200,
