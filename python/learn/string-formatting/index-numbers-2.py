5714cbff34d086b0fae8709ecbf482da8f1b00db
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-03 20:13:39 +0700

    refactor: add index numbers to string formatting

---
+++ python/learn/string-formatting/index-numbers-2.py
@@ -0,0 +1,10 @@
+player = "Kyle DeGrom"
+position = "Starting Pitcher"
+age = 26
+
+news = "{0} is {2}. {0} is {1}."
+print(news.format(player, age, position))
+
+# == Output == #
+# Kyle DeGrom is Starting Pitcher. Kyle DeGrom is 26.
+# ============ #
