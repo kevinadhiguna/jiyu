4c6ddcd70f8bf7e94857454e22ba8c8579aaa722
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-04 12:46:51 +0700

    refactor: add named indices to string formatting

---
+++ python/learn/string-formatting/named-indices.py
@@ -0,0 +1,15 @@
+news = """
+{country1} has been attacking and comitting genocides in {country2}.
+All country leaders should call for permanent ceasefire imminently!
+Stop violence, Stop Genocide, and FREE {country2}!
+"""
+
+print(news.format(country1 = "Israel", country2 = "Palestine"))
+
+# == Output == #
+# 
+# Israel has been attacking and comitting genocides in Palestine.
+# All country leaders should call for permanent ceasefire imminently!
+# Stop violence, Stop Genocide, and FREE Palestine!
+# 
+# ============ #
