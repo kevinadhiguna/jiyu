df21b046a69989bbd4ae9ba408712a64a1a84a2a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-05 09:46:36 +0700

    refactor: add multi values to string formatting

---
+++ python/learn/string-formatting/multi-values.py
@@ -0,0 +1,12 @@
+player = "Takefusa Kubo"
+club1 = "Real Sociedad"
+club2 = "Real Madrid"
+transferFee = 78.5173
+
+news = "{} has been transferred from {} to {} for a transfer fee of ${:.2f} million"
+print(news.format(player, club1, club2, transferFee))
+
+# == Output == #
+# Takefusa Kubo has been transferred from Real Sociedad to Real Madrid 
+#   for a transfer fee of $78.52 million
+# ============ #
