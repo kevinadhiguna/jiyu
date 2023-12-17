1d16aafeecd63c985594751b413640a1fddd5cad
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-17 11:19:28 +0700

    refactor: add array indexing

---
+++ python/learn/numpy/np-array-indexing.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr = np.array([1,2,3])
+print("arr:", arr) # [1 2 3]
+
+print("arr[1]:", arr[1]) # 2
+print("arr[2]:", arr[2]) # 3
+print("arr[1] + arr[2]:", arr[1] + arr[2]) # 5
