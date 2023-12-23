d3d3cb9e542d6fca45a9b27c4c72d7e4c73d71d4
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-23 13:00:12 +0700

    refactor: define an array by mentioning its dimension

---
+++ python/learn/numpy/np-define-array-dimension.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+dimension = 5
+arr = np.array([1, 2, 3, 4, 5], ndmin=dimension) # Easier to create an n-dimension array
+
+print("Arr (",dimension,"):", arr) # [[[[[1 2 3 4 5]]]]]
