3ba3d63f3abe1bef9cb3bc3078cad7a8820d3566
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-02-07 13:07:59 +0700

    refactor: add a usage of an anonymous variable

---
+++ python/learn/HIDDEN-features/anonymous-variables/anonymous-variable-1.py
@@ -0,0 +1,7 @@
+# Normal iteration
+for i in range(10):
+    print(f'This is iteration-{i+1}')
+
+# Iteration using an anonymous variable
+for _ in range(10):
+    print(f'Anonymous Variable: This is iteration-{_+1}')
