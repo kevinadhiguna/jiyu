d11622c5533880f7386d4e5aa9d04325cd7ca729
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-02-12 08:52:41 +0700

    refactor: add an anonymous variables usage

---
+++ python/learn/HIDDEN-features/anonymous-variables/anonymous-variable-3.py
@@ -0,0 +1,13 @@
+items = [101, 297, 741]
+
+# Normal usage
+x, y, z = items
+print('x:', x) # 101
+print('y:', y) # 297
+print('z:', z) # 741
+
+# Using an anonymous variable
+i, _, k = items
+print('i:', i) # 101
+print('_:', _) # 297
+print('k:', k) # 741
