002e6eab52dccf9000e8d0422c832b0ba1200d86
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-02-13 15:01:18 +0700

    refactor: add an anonymous variables usage

---
+++ python/learn/HIDDEN-features/anonymous-variables/anonymous-variable-4.py
@@ -0,0 +1,5 @@
+combos = [["Isagi", "Kaiser"], ["Rin", "Sidou"], ["Nagi", "Reo"]]
+
+second_players = [b for _, b in combos]
+
+print(f'Second Players in the list: {second_players}') # ['Kaiser', 'Sidou', 'Reo']
