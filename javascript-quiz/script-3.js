d4a52a9e0b05410058c7da5c061c445d6781f5ca
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-02-27 09:55:37 +0700

    refactor: add closure

---
+++ javascript-quiz/script-3.js
@@ -0,0 +1,19 @@
+// Closure
+function randomFunction() {
+    let a = 5;
+
+    function inner() {
+        return a;
+    }
+
+    return inner;
+}
+
+let get_inner_func = randomFunction();
+
+// console.log(get_inner_func); // [Function: inner]
+console.log(get_inner_func()); // 5
+console.log(get_inner_func()); // 5
+console.log(get_inner_func()); // 5
+
+// Reference: GeeksforGeeks - How to explain closures in JavaScript and when to use it?
