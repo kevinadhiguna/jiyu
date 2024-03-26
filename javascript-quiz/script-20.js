bb74818eb8348757c3106da28d8847e4dfdf8f04
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-03-26 09:35:45 +0700

    refactor: display output of a variable in a for loop

---
+++ javascript-quiz/script-20.js
@@ -0,0 +1,13 @@
+let number = 10;
+
+for (let i = 0; i < number; i++) {
+    console.log('number :', number);
+    number--;
+}
+
+// Output:
+// number : 10
+// number : 9
+// number : 8
+// number : 7
+// number : 6
