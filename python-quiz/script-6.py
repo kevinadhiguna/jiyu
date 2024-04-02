661262b4133062359e29ad0b57aa8057f57d3e53
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-04-02 09:51:44 +0700

    refactor: print an id attribute of an object

---
+++ python-quiz/script-6.py
@@ -0,0 +1,8 @@
+class student:
+    def __init__(self, studentId):
+        id = 5401
+        self.id = str(studentId)
+        id = 1011
+
+max = student(5411)
+print("Max's student ID:", max.id) # 5411
