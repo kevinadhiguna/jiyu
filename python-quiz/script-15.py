3c7eb3de1a655bf1e9b6d4d8b3dea818b4fd85f3
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-04-15 07:36:11 +0700

    refactor: slice a string

---
+++ python-quiz/script-15.py
@@ -0,0 +1,9 @@
+name = "amy stephanie"
+
+print("name[::] :", name[::]) # amy stephanie
+print("name[-1::] :", name[-1::]) # e
+print("name[1::] :", name[1::]) # my stephanie
+print("name[:-1:] :", name[:-1:]) # amy stephani
+print("name[:1:] :", name[:1:]) # a
+print("name[::1] :", name[::1]) # amy stephanie
+print("name[::-1] :", name[::-1]) # einahpets yma
