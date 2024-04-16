abd3174134a7847536f54ec76c6d1d8e5c81cd9b
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-04-16 21:38:33 +0700

    refactor: display an object's values and their sum

---
+++ python-quiz/script-16.py
@@ -0,0 +1,8 @@
+contracts = {
+    'Derrs': 365000000,
+    'Konradni': 700000000,
+    'Friedman': 168000000,
+}
+
+print("Contract values: ", contracts.values())
+print("Total contract value: ", sum(contracts.values()))
