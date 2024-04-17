836dd6a5910f3855d6bb33e8eccd8e4703d56aab
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-04-17 23:08:30 +0700

    refactor: check if a character is present in a string

---
+++ python-quiz/script-17.py
@@ -0,0 +1,6 @@
+name = set('wontani')
+
+print("'a' in name :", 'a' in name) # True
+
+# Additional material (Ternary operator)
+print(f"Is there an 'a' in '{name}' ?", "Yes" if 'a' in name else "No")
