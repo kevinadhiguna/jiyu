cdd7e3625bee118a853f4fc29fa3e50b864ce43e
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-04-19 22:47:20 +0700

    refactor: display a number from a 2d array

---
+++ python-quiz/script-19.py
@@ -0,0 +1,17 @@
+numbers = [
+    [101, 102, 103],
+    [111, 112, 113],
+    [121, 122, 123],
+]
+
+print("numbers[0][0] :", numbers[0][0]) # 101
+print("numbers[0][1] :", numbers[0][1]) # 102
+print("numbers[0][2] :", numbers[0][2]) # 103
+
+print("numbers[1][0] :", numbers[1][0]) # 111
+print("numbers[1][1] :", numbers[1][1]) # 112
+print("numbers[1][2] :", numbers[1][2]) # 113
+
+print("numbers[2][0] :", numbers[2][0]) # 121
+print("numbers[2][1] :", numbers[2][1]) # 122
+print("numbers[2][2] :", numbers[2][2]) # 123
