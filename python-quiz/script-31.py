a643402b1c42e53aac47ed809da26cca36104aeb
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-09-12 11:15:49 -0700

    refactor: examine length of tuples

---
+++ python-quiz/script-31.py
@@ -0,0 +1,7 @@
+variable = [None] * 9
+variable2 = [None] * 1000
+variable3 = [None] * -1
+
+print("len(variable):", len(variable)) # 9
+print("len(variable2):", len(variable2)) # 1000
+print("len(variable3):", len(variable3)) # 0
