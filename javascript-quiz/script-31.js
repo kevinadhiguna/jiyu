119910f038eb5f5a2a4c869899f5a425b263fa71
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-09-27 12:31:21 -0700

    refactor: decode a uri

--- javascript-quiz/script-31.js
+++ javascript-quiz/script-31.js
@@ -4,3 +4,9 @@ const encodedUri = encodeURI(uri);
 console.log('Encoded URI :', encodedUri); // https://applytojob.org/?ref=%E3%83%A1%E3%83%BC%E3%83%AB
 
 // To-do: decode the URI
+try {
+    const decodedURI = decodeURI(encodedUri);
+    console.log("Decoded URI :", decodedURI); // https://applytojob.org/?ref=メール
+} catch (error) {
+    console.log("Something went wrong :", error);
+}
