bfb05049448191585ba3eca8e9f2f1906886eac1
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-09-28 10:09:29 -0700

    refactor: convert cm to ft

---
+++ javascript/us-units-to-intl/cm-to-ft.js
@@ -0,0 +1,6 @@
+const cmTall = 190; // 190 cm
+const feetTall = cmTall / 30.84;
+
+console.log(`If you're ${cmTall} cm tall, you're ${feetTall} feet`); // If you're 190 cm tall, you're 6.160830090791181 feet
+
+// To-do: round the result
