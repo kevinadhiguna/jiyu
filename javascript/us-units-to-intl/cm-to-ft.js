4ff1d513c939fec693b556ed32684f14328bc778
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-09-29 11:32:40 -0700

    refactor: round the number

--- javascript/us-units-to-intl/cm-to-ft.js
+++ javascript/us-units-to-intl/cm-to-ft.js
@@ -1,6 +1,14 @@
 const cmTall = 190; // 190 cm
 const feetTall = cmTall / 30.84;
 
-console.log(`If you're ${cmTall} cm tall, you're ${feetTall} feet`); // If you're 190 cm tall, you're 6.160830090791181 feet
+console.log(`[Precise] If you're ${cmTall} cm tall, you're ${feetTall} feet`);
+// Output: [Precise] If you're 190 cm tall, you're 6.160830090791181 feet
 
-// To-do: round the result
+function roundNumber(number) {
+    // round to 2 decimal places
+    return Math.round(number * 100) / 100;
+}
+
+let roundedFeet = roundNumber(feetTall);
+console.log(`[Rounded] If you're ${cmTall} cm tall, you're ${roundedFeet} feet`);
+// Output: [Rounded] If you're 190 cm tall, you're 6.16 feet
