edfc52c4a69db60ac2855c9b58a9dbbf2f618d15
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-10-04 21:09:12 -0700

    refactor: round the result

--- javascript/us-units-to-intl/mile-to-km.js
+++ javascript/us-units-to-intl/mile-to-km.js
@@ -3,3 +3,12 @@ const km = miles * 1.60934;
 
 console.log(`[Precise] ${miles} miles is equal to ${km} km`);
 // Output: [Precise] 100 miles is equal to 160.934 km
+
+function roundNumber(number) {
+    // round to 2 decimal places
+    return Math.round(number * 100) / 100;
+}
+
+const roundedKm = roundNumber(km);
+console.log(`[Rounded] ${miles} miles is equal to ${roundedKm} km`);
+// Output: [Rounded] 100 miles is equal to 160.93 km
