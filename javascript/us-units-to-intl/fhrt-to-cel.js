3d24e157a3017667e393297df1a833e9c2e6a226
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-10-09 09:09:32 -0700

    refactor: convert fahrenheit to celcius

---
+++ javascript/us-units-to-intl/fhrt-to-cel.js
@@ -0,0 +1,7 @@
+const fahrenheit = 73.1;
+const celcius = 5 * (fahrenheit - 32) / 9;
+
+console.log(`[Precise] ${fahrenheit} F is equal to ${celcius} celcius`);
+// Output: [Precise] 73.1 F is equal to 22.83333333333333 celcius
+
+// To-do: round the number
