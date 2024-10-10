798f9e2ebbb2c5f83dcb2b7b93896790ed8a4cf5
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-10-10 14:42:05 -0700

    refactor: round the number

--- javascript/us-units-to-intl/cel-to-fhrt.js
+++ javascript/us-units-to-intl/cel-to-fhrt.js
@@ -1,6 +1,13 @@
-const celcius = 33;
+const celcius = 33.8;
 const fahrenheit = 9 * (celcius + 32) / 5;
 
-console.log(`[Precise] ${celcius} celcius is equal to ${fahrenheit} fahrenheit`);
+console.log(`[Precise] ${celcius} C is equal to ${fahrenheit} F`);
+// Output: [Precise] 33.8 C is equal to 118.43999999999998 F
 
-// To-do: round the number
+function roundNumber(number) {
+    return Math.round(number * 100) / 100;
+}
+
+const roundedFahrenheit = roundNumber(fahrenheit);
+console.log(`[Rounded] ${celcius} C is equal to ${roundedFahrenheit} F`);
+// Output: [Rounded] 33.8 C is equal to 118.44 F
