c3b8198cc769808f7a60e00853703fd7f454525e
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-10-11 10:03:11 -0700

    refactor: round the number

--- javascript/us-units-to-intl/floz-to-ml.js
+++ javascript/us-units-to-intl/floz-to-ml.js
@@ -3,3 +3,13 @@ const ml = floz * 29.5735;
 
 console.log(`[Precise] ${floz} fl oz is equal to ${ml} ml`);
 // Output: [Precise] 20 fl oz is equal to 591.47 ml
+
+function roundNumber(number) {
+    // Round to 2 decimal places
+    return Math.round(number * 100) / 100;
+}
+
+const roundedMl = roundNumber(ml);
+
+console.log(`[Rounded] ${floz} fl oz is equal to ${roundedMl} ml`);
+// Output: [Rounded] 20 fl oz is equal to 591.47 ml
