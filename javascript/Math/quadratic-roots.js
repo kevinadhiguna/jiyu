fab0c5d63c16ebf7dbc4e3143184ae7cf1014f66
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-10-17 07:36:06 -0700

    fix: check if d is non-negative before trying to find solutions

--- javascript/Math/quadratic-roots.js
+++ javascript/Math/quadratic-roots.js
@@ -1,4 +1,4 @@
-// Quadratic equation: ax**2 + bx + c
+// Quadratic equation: a(x**2) + bx + c
 
 const a = 1;
 const b = 6;
@@ -6,18 +6,19 @@ const c = 9;
 
 const d = (b ** 2) - (4 * a * c);
 
-// Solutions:
-// To-do: check if d is non-negative or not before finding solutions
-const n1 = (-b - Math.sqrt(d)) / (2 * a);
-const n2 = (-b + Math.sqrt(d)) / (2 * a);
-
-let displayA;
-
-if (a = 1) {
-    console.log(`[Solution] x^2 + (${b})x + (${c}) : n1 = ${n1} & n2 = ${n2}`);
+// Check if d is non-negative or not before finding solutions
+if (d < 0) {
+    console.log("No real solutions exist");
 } else {
-    console.log(`[Solution] ${a}x^2 + (${b})x + (${c}) : n1 = ${n1} & n2 = ${n2}`);
-}
+    const n1 = (-b - Math.sqrt(d)) / (2 * a);
+    const n2 = (-b + Math.sqrt(d)) / (2 * a);
 
-// Output: [Solution] x^2 + (6)x + (9) : n1 = -3 & n2 = -3
-// (Wrong) To-do: fix this [Should be n1=3 , n2=3]
+    if (a === 1) {
+        console.log(`[Solution] x^2 + (${b})x + (${c}) : n1 = ${n1} & n2 = ${n2}`);
+    } else {
+        console.log(`[Solution] ${a}x^2 + (${b})x + (${c}) : n1 = ${n1} & n2 = ${n2}`);
+    }
+
+    // Output: [Solution] x^2 + (6)x + (9) : n1 = -3 & n2 = -3
+    // (Wrong) To-do: fix this [Should be n1=3 , n2=3]
+}
