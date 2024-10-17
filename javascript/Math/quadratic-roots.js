256331feb9dd74afc4f84483b1ba116d2dd998cc
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-10-17 07:31:27 -0700

    fix: add keywords to variables

--- javascript/Math/quadratic-roots.js
+++ javascript/Math/quadratic-roots.js
@@ -1,14 +1,15 @@
 // Quadratic equation: ax**2 + bx + c
 
-a = 1;
-b = 6;
-c = 9;
+const a = 1;
+const b = 6;
+const c = 9;
 
-d = (b ** 2) - (4 * a * c);
+const d = (b ** 2) - (4 * a * c);
 
 // Solutions:
-n1 = (-b - Math.sqrt(d)) / (2 * a);
-n2 = (-b + Math.sqrt(d)) / (2 * a);
+// To-do: check if d is non-negative or not before finding solutions
+const n1 = (-b - Math.sqrt(d)) / (2 * a);
+const n2 = (-b + Math.sqrt(d)) / (2 * a);
 
 let displayA;
 
@@ -19,4 +20,4 @@ if (a = 1) {
 }
 
 // Output: [Solution] x^2 + (6)x + (9) : n1 = -3 & n2 = -3
-// (Wrong) To-do: fix this
+// (Wrong) To-do: fix this [Should be n1=3 , n2=3]
