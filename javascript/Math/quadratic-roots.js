394c0a33430b66b8a9c7e5cbfde3ee321ba8d98b
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-10-18 08:12:37 -0700

    chore: add some sample input

--- javascript/Math/quadratic-roots.js
+++ javascript/Math/quadratic-roots.js
@@ -1,8 +1,22 @@
 // Quadratic equation: a(x**2) + bx + c
 
-const a = 1;
-const b = 6;
-const c = 9;
+// [1]
+// const a = 1;
+// const b = 6;
+// const c = 9;
+// Output: [Solution] x^2 + (6)x + (9) : n1 = -3 & n2 = -3
+
+// [2]
+// const a = 1;
+// const b = -5;
+// const c = 6;
+// Output: [Solution] x^2 + (-5)x + (6) : n1 = 2 & n2 = 3]
+
+// [3]
+const a = 2;
+const b = 8;
+const c = 6;
+// Output: [Solution] 2x^2 + (8)x + (6) : n1 = -3 & n2 = -1
 
 const d = (b ** 2) - (4 * a * c);
 
@@ -18,7 +32,4 @@ if (d < 0) {
     } else {
         console.log(`[Solution] ${a}x^2 + (${b})x + (${c}) : n1 = ${n1} & n2 = ${n2}`);
     }
-
-    // Output: [Solution] x^2 + (6)x + (9) : n1 = -3 & n2 = -3
-    // (Wrong) To-do: fix this [Should be n1=3 , n2=3]
 }
