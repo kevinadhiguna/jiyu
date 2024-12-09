5bd160d30d1d23c68a6492a2e70e2a61859714fe
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-12-08 19:34:42 -0800

    refactor: add user

--- learn-mongoose/model/transactionModel.js
+++ learn-mongoose/model/transactionModel.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const transactionSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
     amount: {
         type: Number,
         required: true,
