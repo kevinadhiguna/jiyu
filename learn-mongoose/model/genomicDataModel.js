be0a2b750c9b477009239d69de22275dbf996a74
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-12-20 08:48:43 -0800

    refactor: add timestamps

--- learn-mongoose/model/genomicDataModel.js
+++ learn-mongoose/model/genomicDataModel.js
@@ -19,6 +19,8 @@ const genomicDataSchema = new mongoose.Schema({
     comments: {
         type: String,
     },
+}, {
+    timestamps: true,
 });
 
 const genomicData = mongoose.model('genomicData', genomicDataSchema);
