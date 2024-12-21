cfd516106d4ba5a8c120c70db7fe58bb083b7bc0
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-12-21 11:24:09 -0800

    refactor: add inventory model

---
+++ learn-mongoose/model/inventoryModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+// A model to track inventory in a warehouse or manufacturing system
+const inventorySchema = new mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+    },
+});
+
+const inventory = mongoose.model('inventory', inventorySchema);
+module.exports = inventory;
