43e17ef7095929b7826f2856c9b4dd85438ed3ba
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-12-30 15:26:07 -0800

    refactor: add products

--- learn-mongoose/model/orderModel.js
+++ learn-mongoose/model/orderModel.js
@@ -7,6 +7,21 @@ const orderSchema = new mongoose.Schema({
         ref: 'User',
         required: true,
     },
+    products: [{
+        productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+    }],
     totalPrice: {
         type: Number,
         required: true,
