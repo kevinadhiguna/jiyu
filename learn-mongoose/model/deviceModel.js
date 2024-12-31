68f70eae6bea43787e4c5d7df089570a25d1fc73
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-12-31 08:07:24 -0800

    refactor: add device model

---
+++ learn-mongoose/model/deviceModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+// A model for storing device information, including status and data collected (in IoT devices, for example)
+const deviceSchema = new mongoose.Schema({
+    deviceId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+});
+
+const device = mongoose.model('device', deviceSchema);
+module.exports = device;
