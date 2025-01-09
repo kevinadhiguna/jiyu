74b3ced0afd78a537b560985a30192aa74dbb03f
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2025-01-09 14:25:14 -0800

    refactor: add a stimestamp

--- learn-mongoose/model/teamModel.js
+++ learn-mongoose/model/teamModel.js
@@ -19,6 +19,8 @@ const teamSchema = new mongoose.Schema({
         losses: { type: Number, default: 0 },
         draw: { type: Number, default: 0 },
     },
+}, {
+    timestamp: true,
 });
 
 const team = mongoose.model('team', teamSchema);
