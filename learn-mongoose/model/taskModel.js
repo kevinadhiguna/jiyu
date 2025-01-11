c9715c623332b3377cb1450da424a539e9c27280
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2025-01-11 07:13:43 -0800

    refactor: add description

--- learn-mongoose/model/taskModel.js
+++ learn-mongoose/model/taskModel.js
@@ -5,6 +5,9 @@ const taskSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    description: {
+        type: String,
+    },
 });
 
 const task = new mongoose.model('task', taskSchema);
