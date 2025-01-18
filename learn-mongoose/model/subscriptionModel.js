baf51f33eb6f74e91f0bb274bdee941c8f0fcfb7
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2025-01-18 09:19:18 -0800

    refactor: add user id

--- learn-mongoose/model/subscriptionModel.js
+++ learn-mongoose/model/subscriptionModel.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 // A model for managing user subscriptions in services such as Netflix, Spotify, or subscription-based websites
 const subscriptionModel = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
     plan: {
         type: String,
         enum: ['basic', 'family', 'premium'],
