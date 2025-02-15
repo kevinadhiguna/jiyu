6398311e99259dc1a075a1781e1cce1266220b9a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2025-02-15 09:36:55 -0800

    refactor: add API key

--- learn-mongoose/model/APIkeyModel.js
+++ learn-mongoose/model/APIkeyModel.js
@@ -6,6 +6,14 @@ const apiKeySchema = new mongoose.Schema({
         ref: 'User',
         required: true,
     },
+    // API keys are randomly generated and secured
+    key: {
+        type: String,
+        required: true,
+        unique: true,
+        // Generate a random API key
+        default: () => crypto.randomBytes(32).toString('hex'),
+    },
 });
 
 const apiKey = mongoose.Model('apiKey', apiKeySchema);
