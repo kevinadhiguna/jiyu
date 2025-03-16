74e25a245a2450bf9ad9822f086456ca9fae377e
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2025-03-16 08:05:18 -0700

    refactor: add ingredients

--- learn-mongoose/model/recipeModel.js
+++ learn-mongoose/model/recipeModel.js
@@ -5,6 +5,10 @@ const recipeSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    ingredients: [{
+        type: String,
+        required: true,
+    }],
 });
 
 const recipe = mongoose.model('recipe', recipeSchema);
