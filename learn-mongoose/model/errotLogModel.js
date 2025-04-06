fc0e9ea4067994166f214ddd462b73f930b18373
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2025-04-06 16:09:37 -0700

    refactor: add error log model

---
+++ learn-mongoose/model/errotLogModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const errorLogSchema = new mongoose.Schema({
+    message: {
+        type: String,
+        required: true,
+    },
+});
+
+const errorLog = mongoose.model('ErrorLog', errorLogSchema);
