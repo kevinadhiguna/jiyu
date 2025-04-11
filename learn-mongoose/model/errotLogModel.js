8eaef1c87d1e081d56557f0149b3d3d6815c699a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2025-04-11 11:18:19 -0700

    refactor: add userId

--- learn-mongoose/model/errotLogModel.js
+++ learn-mongoose/model/errotLogModel.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const errorLogSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
     message: {
         type: String,
         required: true,
