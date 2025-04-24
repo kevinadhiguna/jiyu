5cffa7151d1c8a3ccc5d13e4967d5649acb431cc
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2025-04-24 11:48:39 -0700

    refactor: add comment content

--- learn-mongoose/model/commentModel.js
+++ learn-mongoose/model/commentModel.js
@@ -6,6 +6,12 @@ const commentSchema = new mongoose.Schema({
         ref: 'User',
         required: true,
     },
+    content: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: [500, 'Comment can not exceed 500 characters'],
+    },
 });
 
 const comment = mongoose.model('comment', commentSchema);
